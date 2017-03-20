14dca2e2993e4b18e23d77bcb2f678de073aa66e
Louie <CALOUIE@BCGOV>
2017-03-20 12:03:20 -0700

    Reverted back to Django 1.8

--- gwells/forms.py
+++ gwells/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-#from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from .models import Well
 
 class SearchForm(forms.Form):
@@ -8,7 +7,5 @@ class SearchForm(forms.Form):
     def process(self):
         query = self.cleaned_data.get('q')
         wellresults = None
-        #wellresults = Well.objects.annotate(
-		#    search=SearchVector('street_name')
-		#).filter(search=query)
+
         return wellresults
