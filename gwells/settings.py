8e08634c6c980d46e6e74b6cd05333e131259be3
Louie <CALOUIE@BCGOV>
2017-03-20 11:31:06 -0700

    Removed debug toolbar for now

--- gwells/settings.py
+++ gwells/settings.py
@@ -42,14 +42,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    #'debug_toolbar',
     'welcome',
 	'django.contrib.postgres',
     'gwells',
 )
 
 MIDDLEWARE = (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
