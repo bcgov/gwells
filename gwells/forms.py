3821a0ca83c1c4946885a698e3e46dfc9bfa92ac
Louie <CALOUIE@BCGOV>
2017-03-24 10:52:18 -0700

    Changed models in preparation for search results.

--- gwells/forms.py
+++ gwells/forms.py
@@ -1,11 +1,20 @@
 from django import forms
+#from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from .models import Well
 
 class SearchForm(forms.Form):
     q = forms.CharField(label='', min_length=2, max_length=100, required=False, widget=forms.TextInput(attrs={'size':40}))
 
     def process(self):
-        query = self.cleaned_data.get('q')
         wellresults = None
-
+        
+        query = self.cleaned_data.get('q')
+        
+        if (len(query) > 1):
+            wellresults = Well.objects.all().filter(address_line__icontains=query).order_by('-id')
+            
+            #wellresults = Well.objects.annotate(
+            #    search=SearchVector('address_line')
+            #    ).filter(search=query)
+        
         return wellresults
