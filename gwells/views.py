558c94dab96bcee6e196ca01c9bf924a33b68536
Louie <CALOUIE@BCGOV>
2017-04-10 13:46:48 -0700

    Added more tests.

--- gwells/views.py
+++ gwells/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 #from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
+#from django.utils import timezone
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import WellYieldUnit, Well
 from .forms import SearchForm
