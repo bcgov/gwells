fdcb55459043d5ee695b50c9d2b7d92dbfaa965b
Louie <CALOUIE@BCGOV>
2017-04-13 14:36:55 -0700

    Adding more search criteria will restrict the results further. Specifying address and lot will show results with that partial address and lot.

--- gwells/search.py
+++ gwells/search.py
@@ -9,7 +9,7 @@ class Search():
         Search for wells
 
         :param well: the identification plate number or well tag number
-        :param addr: part of the street address of the well
+        :param addr: part of the street address or site area of the well
         :param legal: part of the lot number, legal plan, legal district lot or pid
         :param owner: part of the owner's full name
         :returns: QuerySet of Well objects or None if no matching records found.
@@ -20,22 +20,18 @@ class Search():
         q_list = []
 
         if well:
-            q_list.append(Q(identification_plate_number=well))
-            q_list.append(Q(well_tag_number=well))
+            q_list.append(Q(identification_plate_number=well) | Q(well_tag_number=well))
 
         if addr:
-            q_list.append(Q(street_address__icontains=addr))
+            q_list.append(Q(street_address__icontains=addr) | Q(site_area__icontains=addr))
 
         if legal:
-            q_list.append(Q(lot_number__icontains=legal))
-            q_list.append(Q(legal_plan__icontains=legal))
-            q_list.append(Q(legal_district_lot__icontains=legal))
-            q_list.append(Q(pid__icontains=legal))
+            q_list.append(Q(lot_number__icontains=legal) | Q(legal_plan__icontains=legal) | Q(legal_district_lot__icontains=legal) | Q(pid__icontains=legal))
 
         if owner:
             q_list.append(Q(well_owner__full_name__icontains=owner))
 
         if len(q_list) > 0:
-            well_results = Well.objects.filter(reduce(operator.or_, q_list)).order_by('well_tag_number', 'id')
+            well_results = Well.objects.filter(reduce(operator.and_, q_list)).order_by('well_tag_number', 'id')
                 
         return well_results
