0f78fb9497c10cf5b945fca5cf388d0a2788762b
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2017-04-27 10:10:43 -0700

    Corrected relatives paths in documentation

--- database/scripts/load-search-ready-data.sql
+++ database/scripts/load-search-ready-data.sql
@@ -1,5 +1,5 @@
-\copy gwells_well_yield_unit FROM 'well_yield_unit.csv' HEADER DELIMITER ',' CSV
-\copy gwells_province_state FROM 'province_state.csv' HEADER DELIMITER ',' CSV
-\copy gwells_land_district FROM 'land_district.csv' HEADER DELIMITER ',' CSV
-\copy gwells_well_owner FROM 'well_owner.csv' HEADER DELIMITER ',' CSV
-\copy gwells_well FROM 'well.csv' HEADER DELIMITER ',' CSV
+\copy gwells_well_yield_unit FROM './database/scripts/well_yield_unit.csv' HEADER DELIMITER ',' CSV
+\copy gwells_province_state  FROM './database/scripts/province_state.csv'  HEADER DELIMITER ',' CSV
+\copy gwells_land_district   FROM './database/scripts/land_district.csv'   HEADER DELIMITER ',' CSV
+\copy gwells_well_owner      FROM './database/scripts/well_owner.csv'      HEADER DELIMITER ',' CSV
+\copy gwells_well            FROM './database/scripts/well.csv'            HEADER DELIMITER ',' CSV
