20bd945a2478333461347d9238e71fd3d9689c82
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2017-04-27 10:54:17 -0700

    Correct SQL Syntax errors.

--- database/scripts/load-search-ready-data.sql
+++ database/scripts/load-search-ready-data.sql
@@ -1,5 +1,5 @@
-\copy gwells_well_yield_unit FROM './database/scripts/well_yield_unit.csv' HEADER DELIMITER ',' CSV
-\copy gwells_province_state  FROM './database/scripts/province_state.csv'  HEADER DELIMITER ',' CSV
-\copy gwells_land_district   FROM './database/scripts/land_district.csv'   HEADER DELIMITER ',' CSV
-\copy gwells_well_owner      FROM './database/scripts/well_owner.csv'      HEADER DELIMITER ',' CSV
-\copy gwells_well            FROM './database/scripts/well.csv'            HEADER DELIMITER ',' CSV
+\copy gwells_well_yield_unit FROM './postgres/well_yield_unit.csv' HEADER DELIMITER ',' CSV
+\copy gwells_province_state  FROM './postgres/province_state.csv'  HEADER DELIMITER ',' CSV
+\copy gwells_land_district   FROM './postgres/land_district.csv'   HEADER DELIMITER ',' CSV
+\copy gwells_well_owner      FROM './postgres/well_owner.csv'      HEADER DELIMITER ',' CSV
+\copy gwells_well            FROM './postgres/well.csv'            HEADER DELIMITER ',' CSV
