1bae5c5dd8551e27e9fb9a0c87e362627253eaf6
Louie <CALOUIE@BCGOV>
2017-05-31 15:43:50 -0700

    Changed tests for new models.

--- gwells/test_search.py
+++ gwells/test_search.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import WellOwner, ProvinceState, WellActivity, WellActivityType, ClassOfWell, SubclassOfWell, DrillingCompany, Driller
+from .models import ProvinceState, Well, WellActivityType, WellClass, DrillingCompany, Driller
 from .search import Search
 import datetime
 
@@ -10,54 +10,41 @@ class SearchTestCase(TestCase):
         prov = ProvinceState.objects.create(sort_order=1)
         prov.save()
 
-        activity_type = WellActivityType.objects.create(sort_order=1)
-        activity_type.save()
+        #activity_type = WellActivityType.objects.create(sort_order=1)
+        #activity_type.save()
 
-        class_of_well = ClassOfWell.objects.create(sort_order=1)
-        class_of_well.save()
+        well_class = WellClass.objects.create(sort_order=1)
+        well_class.save()
 
-        c = DrillingCompany.objects.create()
-        c.save()
+        #c = DrillingCompany.objects.create()
+        #c.save()
 
-        d = Driller.objects.create(drilling_company=c)
+        #d = Driller.objects.create(drilling_company=c)
 
 
-        w = WellActivity.objects.create(well_activity_type=activity_type, class_of_well=class_of_well, driller_responsible=d, activity_start_date=datetime.date.today(), activity_end_date = datetime.date.today())
+        w = Well.objects.create(well_class=well_class, owner_province_state=prov)
         w.identification_plate_number = 123
         w.well_tag_number = 456
         w.street_address = '123 Main St.'
         w.legal_plan = '7789'
+        w.owner_full_name = 'John Smith'
         w.save()
 
-        w2 = WellActivity.objects.create(well_activity_type=activity_type, class_of_well=class_of_well, driller_responsible=d, activity_start_date=datetime.date.today(), activity_end_date = datetime.date.today())
+        w2 = Well.objects.create(well_class=well_class, owner_province_state=prov)
         w2.identification_plate_number = 3456332
         w2.well_tag_number = 890
         w2.street_address = '555 Government Street'
         w2.legal_district_lot = '789'
+        w2.owner_full_name = 'Jane Doe'
         w2.save()
 
-        w3 = WellActivity.objects.create(well_activity_type=activity_type, class_of_well=class_of_well, driller_responsible=d, activity_start_date=datetime.date.today(), activity_end_date = datetime.date.today())
+        w3 = Well.objects.create(well_class=well_class, owner_province_state=prov)
         w3.identification_plate_number = 55555
         w3.well_tag_number = 123
         w3.street_address = '200 Main Street'
         w3.pid = 54321
+        w3.owner_full_name = 'Joe Smith'
         w3.save()
-
-        owner = WellOwner.objects.create(province_state=prov, well_activity=w)
-        owner.full_name = 'John Smith'
-        owner.save()
-
-        owner = WellOwner.objects.create(province_state=prov, well_activity=w)
-        owner.full_name = 'John Doe'
-        owner.save()
-
-        owner = WellOwner.objects.create(province_state=prov, well_activity=w2)
-        owner.full_name = 'Jane Doe'
-        owner.save()
-
-        owner = WellOwner.objects.create(province_state=prov, well_activity=w3)
-        owner.full_name = 'Joe Smith'
-        owner.save()
 
 
 
