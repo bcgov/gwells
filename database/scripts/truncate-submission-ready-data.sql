0ed16e85a4027c971482e2c20c14deaaf3d0db18
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2017-06-04 23:58:56 -0700

    fixes to syntax errors and column orders

--- database/scripts/truncate-submission-ready-data.sql
+++ database/scripts/truncate-submission-ready-data.sql
@@ -1,3 +1,5 @@
+/* Delete all rows from Submission Ready tables */
+
 truncate gwells_activity_submission restart identity cascade;
 truncate gwells_driller             restart identity cascade;
 truncate gwells_drilling_company    restart identity cascade;
@@ -10,3 +12,9 @@ truncate gwells_well_activity_type  restart identity cascade;
 truncate gwells_well_class          restart identity cascade;
 truncate gwells_well_subclass       restart identity cascade;
 truncate gwells_well_yield_unit     restart identity cascade;
+
+/* Delete all rows from temporary transformation tables */
+truncate xform_gwells_land_district    restart identity cascade;
+truncate xform_gwells_well             restart identity cascade;
+truncate xform_gwells_driller          restart identity cascade;
+truncate xform_gwells_drilling_company restart identity cascade;
