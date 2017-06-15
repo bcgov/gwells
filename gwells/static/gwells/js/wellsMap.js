f41789ef9808631b8a4523280481a7420fde695f
Seth Greylyn <seth.greylyn@gmail.com>
2017-06-15 10:20:09 -0700

    Basic ESRI Leaflet integration.

--- gwells/static/gwells/js/wellsMap.js
+++ gwells/static/gwells/js/wellsMap.js
@@ -3,9 +3,10 @@ var wMap = (function() {
     // TODO: Incorporate esri-leaflet, get layer list loading.
     var createMap = function(mapNodeId) {
         var myMap = L.map(mapNodeId).setView([48.4284, -123.3656], 13);
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(myMap);                    
+        L.esri.tiledMapLayer({
+        url: 'http://maps.gov.bc.ca/arcserver/rest/services/province/roads_wm/MapServer',
+        maxZoom: 15
+        }).addTo(myMap);                   
     }
     return {
         createMap: createMap
