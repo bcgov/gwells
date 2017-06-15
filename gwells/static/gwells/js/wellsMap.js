9fbf7b042922986975d881d9ece4b42cb9037e84
Seth Greylyn <seth.greylyn@gmail.com>
2017-06-14 18:09:27 -0700

    Module refinement

--- gwells/static/gwells/js/wellsMap.js
+++ gwells/static/gwells/js/wellsMap.js
@@ -1,12 +1,13 @@
-
+/** First go at a wellsMap module, which provides core map functionality to the GWELLS application. Depends on Leaflet. */
 var wMap = (function() {
-
-    return {
-        createMap: function(mapNodeId) {
-            var myMap = L.map(mapNodeId).setView([48.4284, -123.3656], 13);
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(myMap);                    
-        }
+    // TODO: Incorporate esri-leaflet, get layer list loading.
+    var createMap = function(mapNodeId) {
+        var myMap = L.map(mapNodeId).setView([48.4284, -123.3656], 13);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(myMap);                    
     }
+    return {
+        createMap: createMap
+    };
 })();
