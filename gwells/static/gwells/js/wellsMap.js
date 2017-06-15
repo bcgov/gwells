288d41b95692f5b1fa5b5dd8ebf0212f9a28b723
Seth Greylyn <seth.greylyn@gmail.com>
2017-06-14 17:59:00 -0700

    wellsMap module

--- gwells/static/gwells/js/wellsMap.js
+++ gwells/static/gwells/js/wellsMap.js
@@ -1,14 +1,12 @@
-/** First go at a wellsMap module, which provides core map functionality to the GWELLS application. Depends on Leaflet. */
+
 var wMap = (function() {
-    // TODO: Incorporate esri-leaflet, get layer list loading.
-    var createMap = function(mapNodeId) {
-        var myMap = L.map(mapNodeId).setView([48.4284, -123.3656], 13);
-        L.esri.tiledMapLayer({
-        url: 'http://maps.gov.bc.ca/arcgis/rest/services/province/web_mercator_cache/MapServer/',
-        maxZoom: 15
-        }).addTo(myMap);                   
-    }
+
     return {
-        createMap: createMap
-    };
+        createMap: function(mapNodeId) {
+            var myMap = L.map(mapNodeId).setView([48.4284, -123.3656], 13);
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(myMap);                    
+        }
+    }
 })();
