b7e2036f9c59296aa1b8248e63a9c86e7ce9b72a
sgreylyn <seth.greylyn@gmail.com>
2017-06-20 13:37:23 -0700

    WellsMap refinement on ActivityCreation page.

--- gwells/static/gwells/js/wellsMap.js
+++ gwells/static/gwells/js/wellsMap.js
@@ -4,24 +4,43 @@ function WellsMap () {
 
     // Options for creating a wellsMap
     var wellsMapOptions = {
+        // Initial centre of the map
         initLatLong: [48.4284, -123.3656],
-        initZoom: 13,
+        // Minimum zoom level of the map (i.e., how far it can be zoomed out)
+        minZoom: 4,
+        // Bounding lats and longs of the map; corresponds to the lat/long extremes of BC. TODO: Refine?
+        mapBounds: {
+            top:  60.0223,
+            bottom: 48.2045556,
+            left: -139.0736706,
+            right: -114.0338224,
+            padding: 5 // Margin beyond extremes to pad the bounds with, as a percentage of the total bounding box.
+        },
+        // ESRI layers associated with the map
         esriLayers: [
-            /*{
+            /*{ // This is an alternate basemap.
                 url: 'http://maps.gov.bc.ca/arcgis/rest/services/province/web_mercator_cache/MapServer/'
             },*/
             {
                 url: 'http://maps.gov.bc.ca/arcserver/rest/services/Province/roads_wm/MapServer'
             }
         ],
+        // WMS layers associated with the map
         wmsLayers: [
             {
+                rootUrl: 'https://openmaps.gov.bc.ca/geo/pub/WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW/ows?',
+                format: 'image/png',
+                layers: 'pub:WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW',
+                styles: 'PMBC_Parcel_Fabric_Cadastre_Outlined', // TODO: Verify style
+                transparent: true
+            }/*,
+            {   // This is the DataBC Wells layer as viewed in iMap. Its data is updated daily, but not continuously, so it is likely not suitable for the GWELLS application.
                 rootUrl: 'https://openmaps.gov.bc.ca/geo/pub/WHSE_WATER_MANAGEMENT.GW_WATER_WELLS_WRBC_SVW/ows?',
                 format: 'image/png',
                 layers: 'pub:WHSE_WATER_MANAGEMENT.GW_WATER_WELLS_WRBC_SVW',
                 styles: 'Water_Wells_All',
                 transparent: true
-            }
+            } */
         ]
     };
 
@@ -127,10 +146,27 @@ function WellsMap () {
             _leafletMap.remove();
             _leafletMap = null;
         }
+
         // Basic initialisation.
-        var initLatLong = wellsMapOptions.initLatLong || [48.4284, -123.3656];
-        var initZoom = wellsMapOptions.initZoom || 13;
-        _leafletMap = L.map(mapNodeId).setView(initLatLong, initZoom);
+        var initLatLong = wellsMapOptions.initLatLong || [53.7267, -127.6476]; // Fallback default to geographic centre of BC.
+        var minZoom = wellsMapOptions.minZoom || 4;  // Fallback default to whole-province view in small map box.
+        var bounds = wellsMapOptions.mapBounds || void 0; // If no bounds provided in options, don't fit to a bounding box.
+        // TODO: Break maxBounds generation into its own private method.
+        var maxBounds = void 0;
+        if (_exists(bounds.top) && _exists(bounds.bottom) && _exists(bounds.left) && _exists(bounds.right)) {
+            maxBounds = L.latLngBounds([L.latLng(bounds.top, bounds.left), L.latLng(bounds.bottom, bounds.right)]);
+            if (bounds.padding) {
+                maxBounds.pad(bounds.padding);
+            }
+        }
+        _leafletMap = L.map(mapNodeId, {
+            minZoom: minZoom,
+            maxBounds: maxBounds,
+            maxBoundsViscosity: 1.0
+        });
+        if (_exists(maxBounds)) {
+            _leafletMap.fitBounds(maxBounds);
+        }
         _loadEsriLayers(_leafletMap);
         _loadWmsLayers(_leafletMap);
 
