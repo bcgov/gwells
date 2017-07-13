1e08b9de3c60e5c2ba6e49ff840618a750586fc7
Louie <CALOUIE@BCGOV>
2017-07-13 12:06:36 -0700

    Added readme and sample pages for functional tests.

---
+++ functional-tests/src/test/groovy/pages/app/SearchPage.groovy
@@ -0,0 +1,9 @@
+package pages.app
+
+import geb.Page
+
+class SearchPage extends Page {
+    static at = { title == "Groundwater Wells Search - Province of British Columbia" }
+
+    static url = "/search"
+}
--- functional-tests/src/test/groovy/pages/app/SettingsPage.groovy
+++
@@ -1,9 +0,0 @@
-package pages.app
-
-import geb.Page
-
-class SettingsPage extends Page {
-    static at = { title == "Transportation Fuels Reporting System - Settings" }
-
-    static url = "settings"
-}
