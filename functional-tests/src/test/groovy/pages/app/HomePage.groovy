2c12f880ab24e9f5e1273206f79bb3331acd0bef
GeorgeWalker <george.walker@quartech.com>
2017-07-18 11:17:40 -0700

    Updated page definitions

--- functional-tests/src/test/groovy/pages/app/HomePage.groovy
+++ functional-tests/src/test/groovy/pages/app/HomePage.groovy
@@ -3,7 +3,7 @@ package pages.app
 import geb.Page
 
 class DashboardPage extends Page {
-    static at = { title == "Groundwater Wells - Province of British Columbia" }
+    static at = { title == "Groundwater Wells  - Province of British Columbia" }
 
     static url = "/"
 }
