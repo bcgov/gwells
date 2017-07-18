60de5addbd7cadb18bc69ced775a8c22a99295c5
GeorgeWalker <george.walker@quartech.com>
2017-07-18 11:08:43 -0700

    Updated BDD tests

--- functional-tests/src/test/groovy/pages/app/HomePage.groovy
+++ functional-tests/src/test/groovy/pages/app/HomePage.groovy
@@ -3,7 +3,7 @@ package pages.app
 import geb.Page
 
 class DashboardPage extends Page {
-    static at = { title == "Transportation Fuels Reporting System - Dashboard" }
+    static at = { title == "Groundwater Wells Search - Province of British Columbia" }
 
     static url = "/"
 }
