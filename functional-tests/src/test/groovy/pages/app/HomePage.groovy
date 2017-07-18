8914cbaadc4c57cda11ed39175687250ed4f70ba
GeorgeWalker <george.walker@quartech.com>
2017-07-18 11:18:22 -0700

    Fix page name

--- functional-tests/src/test/groovy/pages/app/HomePage.groovy
+++ functional-tests/src/test/groovy/pages/app/HomePage.groovy
@@ -2,7 +2,7 @@ package pages.app
 
 import geb.Page
 
-class DashboardPage extends Page {
+class HomePage extends Page {
     static at = { title == "Groundwater Wells  - Province of British Columbia" }
 
     static url = "/"
