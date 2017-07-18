1b5738e37d2f1c0c369ecea7df62d27d9578ea6a
GeorgeWalker <george.walker@quartech.com>
2017-07-18 11:17:40 -0700

    Updated page definitions

--- functional-tests/src/test/groovy/pages/app/SearchPage.groovy
+++ functional-tests/src/test/groovy/pages/app/SearchPage.groovy
@@ -2,7 +2,7 @@ package pages.app
 
 import geb.Page
 
-class NotificationsPage extends Page {
+class SearchPage extends Page {
     static at = { title == "Groundwater Wells Search - Province of British Columbia" }
 
     static url = "/search"
