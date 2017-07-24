9abdb2b772efe56d49206676a4d6e6b34c3daeec
calouie <calvin.louie@gov.bc.ca>
2017-07-24 15:14:13 -0700

    Added validation to check that wall thickness is required if casing is not open hole.

--- gwells/forms.py
+++ gwells/forms.py
@@ -646,22 +646,19 @@ class CasingForm(forms.ModelForm):
             errors.append('To must be greater than or equal to From.')
 
         try:
-            if casing_type != CasingType.objects.get(code='OPEN') and not casing_material:
+            open_casing_type = CasingType.objects.get(code='OPEN')
+        except Exception as e:
+            errors.append('Configuration error: Open Hole Casing Type does not exist, please contact the administrator.')
+
+        if open_casing_type:
+            if casing_type != open_casing_type and not casing_material:
                 self.add_error('casing_material', 'This field is required.')
-        except Exception as e:
-            errors.append('Configuration error: Open Hole Casing Type does not exist, please contact the administrator.')
 
-        try:
-            if casing_type != CasingType.objects.get(code='OPEN') and not wall_thickness:
+            if casing_type != open_casing_type and not wall_thickness:
                 self.add_error('wall_thickness', 'This field is required.')
-        except Exception as e:
-            errors.append('Configuration error: Open Hole Casing Type does not exist, please contact the administrator.')
 
-        try:
-            if casing_type == CasingType.objects.get(code='OPEN') and casing_material:
+            if casing_type == open_casing_type and casing_material:
                 self.add_error('casing_material', 'Open Hole cannot have a casing material.')
-        except Exception as e:
-            errors.append('Configuration error: Open Hole Casing Type does not exist, please contact the administrator.')
 
 
         if len(errors) > 0:
