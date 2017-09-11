bcd520a332dd92e47ad98f8cf74c41569cfa4bf0
John Doe <john@sample.com>
2017-09-11 09:30:19 -0700

    Updates to Navunit Test Suite

--- navunit/src/test/groovy/pages/app/ActivitySubmissionPage.groovy
+++ navunit/src/test/groovy/pages/app/ActivitySubmissionPage.groovy
@@ -5,5 +5,5 @@ import geb.Page
 class ActivitySubmissionPage extends Page {
     static at = { title == "Groundwater Wells Activity Submission - Province of British Columbia" }
 
-    static url = "submission/create"
+    static url = "/submission/create"
 }
