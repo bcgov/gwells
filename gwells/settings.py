af2d14a3ecd088329530129b4328bef143eb7912
Roland Stens <roland.stens@gmail.com>
2017-10-16 13:26:37 -0700

    static file pointer updated

--- gwells/settings.py
+++ gwells/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/groundwater-wells/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
