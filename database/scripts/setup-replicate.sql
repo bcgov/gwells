eefa8711c40cf495f42a3cf9351773e195ec2c06
Fieran Mason <fieranmason@gmail.com>
2017-10-27 16:13:28 -0700

    fixing typo

--- database/scripts/setup-replicate.sql
+++ database/scripts/setup-replicate.sql
@@ -7,7 +7,7 @@ CREATE OR REPLACE FUNCTION setup_replicate() RETURNS void AS $$
 BEGIN
 	raise notice 'Starting setup_replicate() procedure...';
 	PERFORM public.clear_tables();
-	PERFORM public.create_xform_well_well_ETL_table();
+	PERFORM public.create_xform_well_gwells_ETL_table();
 	raise notice 'Finished setup_replicate() procedure.';
 END;
 $$ LANGUAGE plpgsql;
