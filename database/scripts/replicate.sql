753d3fc4ed85119240813b8741b4039d394871e9
Fieran Mason <fieranmason@gmail.com>
2017-11-01 16:57:40 -0700

    loading from dump which includes schema and FDW

--- database/scripts/replicate.sql
+++ database/scripts/replicate.sql
@@ -6,7 +6,7 @@ BEGIN
 	raise notice 'Starting replicate() procedure...';
 
 	raise notice '...replicating...';
-	PERFORM public.copy_remote_code_tables();
+	PERFORM public.create_xform_well_gwells_ETL_table();
 	PERFORM public.populate_xform_gwells_well();
 	PERFORM public.populate_gwells_from_xform();
 
