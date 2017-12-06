d1ca7dbd9554283679edef1c32fe6f0f97b93e86
Roland Stens <roland.stens@gmail.com>
2017-12-05 17:11:31 -0800

    Removal of NavUnit, we are now on BDDStack

--- navunit/src/test/groovy/pages/external/Accessibility.groovy
+++
@@ -1,9 +0,0 @@
-package pages.external
-
-import geb.Page
-
-class Accessibility extends Page {
-    static at = { title == "Web Accessibility - Province of British Columbia"}
-    static url = "https://www2.gov.bc.ca/gov/content/home/accessibility"
-}
-
--- navunit/src/test/groovy/pages/external/Privacy.groovy
+++
@@ -1,8 +0,0 @@
-package pages.external
-
-import geb.Page
-
-class Privacy extends Page {
-    static at = { title == "B.C. Government Website Privacy Statement - Province of British Columbia"}
-    static url = "https://www2.gov.bc.ca/gov/content/home/privacy"
-}
