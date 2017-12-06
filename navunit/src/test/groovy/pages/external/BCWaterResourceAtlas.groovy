72faa2c440a80db64e18ca1362566b247376f753
Roland Stens <roland.stens@gmail.com>
2017-12-05 17:11:31 -0800

    Removal of NavUnit, we are now on BDDStack

--- navunit/src/test/groovy/pages/external/BCWaterResourceAtlas.groovy
+++
@@ -1,9 +0,0 @@
-package pages.external
-
-import geb.Page
-
-class BCWaterResourceAtlas extends Page {
-    static at = { title == "DataBC"}
-    static url = "http://maps.gov.bc.ca/ess/sv/wrbc/"
-}
-
