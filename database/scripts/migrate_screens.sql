f25f64a2a3b5f82f4eb167f06c7973e92d5338a2
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2017-12-28 10:48:00 -0800

    Interim save - refactored to DB calls

--- database/scripts/migrate_screens.sql
+++ database/scripts/migrate_screens.sql
@@ -1,40 +1,49 @@
-\echo '...importing wells_screens data'
-INSERT INTO gwells_screen(
-  screen_guid                                   , -->PK
-  filing_number                                 , -->FK
-  well_tag_number                               , -->FK
-  screen_from                                   ,
-  screen_to                                     ,
-  internal_diameter                             ,
-  screen_assembly_type_guid                     , -->FK
-  slot_size                                     ,
-  when_created                                  ,
-  when_updated                                  ,
-  who_created                                   ,
-  who_updated)
-SELECT
-  gen_random_uuid()                             ,
-  null                                          ,
-  xform.well_tag_number                         ,
-  screens.screen_from                           ,
-  screens.screen_to                             ,
-  screens.screen_internal_diameter              ,
-  screen_assembly_type.screen_assembly_type_guid,
-  screens.screen_slot_size                      ,
-  screens.when_created                          ,
-  screens.when_updated                          ,
-  screens.who_created                           ,
-  screens.who_updated
-FROM wells.wells_screens screens
-     INNER JOIN xform_gwells_well xform ON xform.well_id=screens.well_id
-     LEFT OUTER JOIN gwells_screen_assembly_type screen_assembly_type ON
-     ( screens.screen_assembly_type_code=screen_assembly_type.screen_assembly_type_code OR
-       screens.screen_assembly_type_code='L' AND screen_assembly_type.screen_assembly_type_code='LEAD' OR
-       screens.screen_assembly_type_code='K  & Riser' AND screen_assembly_type.screen_assembly_type_code='K_RISER'
-     );
+DROP FUNCTION IF EXISTS gwells_migrate_screens();
 
-\echo 'wells_screens data imported'
+CREATE OR REPLACE FUNCTION gwells_migrate_screens() RETURNS void AS $$
+DECLARE
+  row_count integer;
+BEGIN
+  raise notice '...importing wells_screens data';
 
-\t
-SELECT count(*) || ' rows loaded into the gwells_screen table' from gwells_screen;
-\t
+  INSERT INTO gwells_screen(
+    screen_guid                                   , -->PK
+    filing_number                                 , -->FK
+    well_tag_number                               , -->FK
+    screen_from                                   ,
+    screen_to                                     ,
+    internal_diameter                             ,
+    screen_assembly_type_guid                     , -->FK
+    slot_size                                     ,
+    when_created                                  ,
+    when_updated                                  ,
+    who_created                                   ,
+    who_updated)
+  SELECT
+    gen_random_uuid()                             ,
+    null                                          ,
+    xform.well_tag_number                         ,
+    screens.screen_from                           ,
+    screens.screen_to                             ,
+    screens.screen_internal_diameter              ,
+    screen_assembly_type.screen_assembly_type_guid,
+    screens.screen_slot_size                      ,
+    screens.when_created                          ,
+    screens.when_updated                          ,
+    screens.who_created                           ,
+    screens.who_updated
+  FROM wells.wells_screens screens
+       INNER JOIN xform_gwells_well xform ON xform.well_id=screens.well_id
+       LEFT OUTER JOIN gwells_screen_assembly_type screen_assembly_type ON
+       ( screens.screen_assembly_type_code=screen_assembly_type.screen_assembly_type_code OR
+         screens.screen_assembly_type_code='L' AND screen_assembly_type.screen_assembly_type_code='LEAD' OR
+         screens.screen_assembly_type_code='K  & Riser' AND screen_assembly_type.screen_assembly_type_code='K_RISER'
+       );
+
+  raise notice 'wells_screens data imported';
+  SELECT count(*) from gwells_screen into row_count;
+  raise notice '... % rows loaded into the gwells_screen table',  row_count;
+END;
+$$ LANGUAGE plpgsql;
+
+COMMENT ON FUNCTION gwells_migrate_screens () IS 'Load Screen details numbers, only for the wells that have been replicated.'; 
