591a215562e838d55f8b29fbd79a31d7a2acd91f
Roland Stens <roland.stens@gmail.com>
2018-01-04 15:05:17 -0800

    Updates to simplify test script

--- functional-tests/src/test/groovy/Search/SearchResultsPagination.groovy
+++ functional-tests/src/test/groovy/Search/SearchResultsPagination.groovy
@@ -65,7 +65,6 @@ Given I have <count> search results
         		$("#results_paginate a[data-dt-idx=\"7\"]").click()	
         	}
         	else {
-        		// Last Page shown is always index 7
         		$("#results_paginate a[data-dt-idx=\"$TargetPage\"]").click()	
         	}
         	assert waitFor {(NumberEntryFrom().toInteger() == ("$TargetPage".toInteger() - 1) * 10 + 1 )}
@@ -84,7 +83,7 @@ Given I have <count> search results
         "Multiple matching results - Address 12 - 3 to 2"          | ""             	| "12"                     | ""                | ""                | 3    	  |2    	    | 362   
     } 
         @Unroll
-        def "Scenario 2: Next Page links: #TestDesc"() {
+        def "Scenario 2: Next Page links: #TestDesc#Testresult"() {
         given: "I have #NumberResult wells search result"
          to SearchPage
 
@@ -95,55 +94,27 @@ Given I have <count> search results
         when: "I view all search results from page <page>"
           assert (NumberOfEntriesFound() == "$NumberResult") //Total entries found
           assert waitFor {(CheckPageButton("$Page") == true)}
+          $("#results_paginate a[data-dt-idx=\"$Page\"]").click()
 
-          def CalcMax = "$NumberResult".toInteger() / "$Page".toInteger()
-
-          if ( CalcMax <= 10 ) {
-            $("#results_paginate a[data-dt-idx=\"7\"]").click()
-          }
-          else {
-            // Last Page shown is always index 7
-            $("#results_paginate a[data-dt-idx=\"$Page\"]").click()
-          }
-
-        then: "I should see a link \'Next\'"
+        then: "I should see a link \'Next\' is " + "$Testresult."
           assert waitFor {(next_button.displayed == true)}
           
-          // Test to see if we are at the end of the list....
-          if ( NumberOfEntriesFound() == NumberEntryTo() ) {
-            // Next should be disabled
-            assert waitFor {(next_button.classes().sort() == ["disabled", "next", "paginate_button"])}
-          }
-          else {
-            // Next is visible and enabled
+          if ( "$Testresult" == "enabled" )
+          {
             assert waitFor {(next_button.classes().sort() == ["next", "paginate_button"])}
           }
-
-       when: "I click \'Next\'"
-          $("#results_paginate a[data-dt-idx=\"7\"]").click()
-
-        then: "I should be on page #Page"
-          assert waitFor {(next_button.displayed == true)}
-          
-          // Test to see if we are at the end of the list....
-          if ( NumberOfEntriesFound() == NumberEntryTo() ) {
-            // Next should be disabled
+          else
+          {
             assert waitFor {(next_button.classes().sort() == ["disabled", "next", "paginate_button"])}
           }
-          else {
-            // Next is visible and enabled
-            assert waitFor {(next_button.classes().sort() == ["next", "paginate_button"])}
-            assert waitFor {(NumberEntryFrom().toInteger() == ("$Page".toInteger()) * 10 + 1 )}
-          }
-
         where:
-        TestDesc                                                      | WellId | Address | LegalId | Owner | Page | NumberResult
-        "Multiple matching results - Address 12 - Next is disabled"  | ""     | "12"   | ""      | ""    | 3    | 362    
-        "Multiple matching results - Address 12 - Next button shows" | ""     | "12"   | ""      | ""    | 1    | 362    
+        TestDesc                                                      | WellId | Address | LegalId | Owner | Page | NumberResult | Testresult
+        "Multiple matching results - Address 123 - Next is "  | ""     | "123"   | ""      | ""    | 3    | 29    | "disabled"
+        "Multiple matching results - Address 123 - Next is " | ""     | "123"   | ""      | ""    | 1    | 29    | "enabled"
     } 
 
     @Unroll
-    def "Scenario 3: Previous Page links: #TestDesc"() {
+    def "Scenario 3: Previous Page links: #TestDesc#TestResult"() {
         given: "I have NumberResult wells search result"
          to SearchPage
 
@@ -154,55 +125,21 @@ Given I have <count> search results
         when: "I view all search results from page <page>"
           assert (NumberOfEntriesFound() == "$NumberResult") //Total entries found
           assert waitFor {(CheckPageButton("$Page") == true)}
+          $("#results_paginate a[data-dt-idx=\"$Page\"]").click()
 
-        then: "I should see a link \'Previous\'"
-          assert waitFor {(previous_button.displayed == true)}
-
-          def CalcMax = "$NumberResult".toInteger() / "$Page".toInteger()
-
-          if ( CalcMax <= 10 ) {
-            $("#results_paginate a[data-dt-idx=\"7\"]").click()
-          }
-          else {
-            // Last Page shown is always index 7
-            $("#results_paginate a[data-dt-idx=\"$Page\"]").click()
-          }
-          
-          if ( "$Page" == "1") {
-            // Previous be disabled
-            println "Page should be 1: $Page"
-            println previous_button.classes().sort()
-            assert waitFor {(previous_button.classes().sort() == ["disabled", "paginate_button", "previous"])}
-            assert waitFor {(NumberEntryFrom().toInteger() == ("$Page".toInteger() - 1) * 10 + 1 )}
-          }
-          else {
-            // Previous is visible and enabled
-            println "Page should not be 1: $Page"
-            println previous_button.classes().sort()
+        then: "I should see \'Previous\' is " + "$Testresult."
+          assert waitFor {(previous_button.displayed == true)}  
+        if ( "$Testresult" == "enabled" )
+          {
             assert waitFor {(previous_button.classes().sort() == ["paginate_button", "previous"])}
           }
-
-       when: "I click \'Previous\'"
-           $("#results_paginate a[data-dt-idx=\"0\"]").click() //Always index 0
-           def tmpPage = "$Page".toInteger()
-           tmpPage = tmpPage - 1
-
-        then: "I should be on page #Page"
-          assert waitFor {(previous_button.displayed == true)}
-          
-          if ( tmpPage <= 1) {
-            // Previous should not be disabled
+          else
+          {
             assert waitFor {(previous_button.classes().sort() == ["disabled", "paginate_button", "previous"])}
           }
-          else {
-            // Previous is visible and enabled
-            assert waitFor {(previous_button.classes().sort() == ["paginate_button", "previous"])}
-            assert waitFor {(NumberEntryFrom().toInteger() == ("$Page".toInteger() - 2) * 10 + 1 )}
-          }
-
         where:
-        TestDesc                                                          | WellId | Address | LegalId | Owner | Page | NumberResult
-        "Multiple matching results - Address 123 - previous is disabled"  | ""     | "12"   | ""      | ""    | 1    | 362    
-        "Multiple matching results - Address 123 - Previous button shows" | ""     | "12"   | ""      | ""    | 3   | 362    
+        TestDesc                                                          | WellId | Address | LegalId | Owner | Page | NumberResult | Testresult
+        "Multiple matching results - Address 123 - Previous is "  | ""     | "123"   | ""      | ""    | 1    | 29           | "disabled"
+        "Multiple matching results - Address 123 - Previous is " | ""     | "123"   | ""      | ""    | 3   | 29            | "enabled"
     } 
 }
