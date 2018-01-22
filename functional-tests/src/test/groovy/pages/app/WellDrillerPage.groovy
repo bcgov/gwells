6683f5ccfd96032e6aab0f35bf3a96191c14044a
Fieran Mason <fieranmason@gmail.com>
2018-01-22 15:54:02 -0800

    Modified registry url in preparation for registry devex project.

--- functional-tests/src/test/groovy/pages/app/WellDrillerPage.groovy
+++ functional-tests/src/test/groovy/pages/app/WellDrillerPage.groovy
@@ -5,5 +5,5 @@ import geb.Page
 class WellDrillerPage extends Page {
     static at = { title == "Groundwater Wells Registry - Province of British Columbia" }
 
-    static url = "gwells/registry"
+    static url = "gwells/registry-legacy"
 }
