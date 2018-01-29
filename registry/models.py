0faccc32f411ca809d61966bcefd3c7c70a20174
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2018-01-28 20:37:44 -0800

    v2 of Registry app model, with code CSV files

--- registry/models.py
+++ registry/models.py
@@ -68,22 +68,39 @@ class ContactAt(AuditModel):
             ,self.person.surname
             ,self.org.name)
 
-
 class ActivityType(AuditModel):
     """
     Restricted Activity related to drilling wells and installing well pumps.
     """
     registry_activity_type_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     code = models.CharField(max_length=10)
-    subcode = models.CharField(max_length=10)
     description = models.CharField(max_length=100)
     is_hidden = models.BooleanField(default=False)
     sort_order = models.PositiveIntegerField()
 
     class Meta:
-        unique_together = ('code', 'subcode')
         db_table = 'registry_activity_type'
-        ordering = ['sort_order', 'code','description']
+        ordering = ['sort_order', 'description']
+        verbose_name_plural = 'Type of restricted activity'
+
+    def __str__(self):
+        return self.description
+
+class ActivitySubtype(AuditModel):
+    """
+    Restricted Activity Subtype related to drilling wells and installing well pumps.
+    """
+    registry_activity_subtype_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    registry_activity_type = models.ForeignKey(ActivityType, null=True, db_column='registry_activity_type_guid', on_delete=models.CASCADE, blank=True)
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=100)
+    is_hidden = models.BooleanField(default=False)
+    sort_order = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'registry_activity_subtype'
+        ordering = ['sort_order', 'description']
+        verbose_name_plural = 'Subtype of restricted activity, under a given Activity Type'
 
     def __str__(self):
         return self.description
@@ -94,7 +111,7 @@ class QualificationType(AuditModel):
     Type of well for which the activity is qualified.
     """
     registry_qualification_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    registry_activity_type = models.ForeignKey(ActivityType, null=True, db_column='registry_activity_type_guid', on_delete=models.CASCADE, blank=True)
+    registry_activity_subtype = models.ForeignKey(ActivitySubtype, null=True, db_column='registry_activity_subtype_guid', on_delete=models.CASCADE, blank=True)
     code = models.CharField(max_length=10)
     description = models.CharField(max_length=100)
     is_hidden = models.BooleanField(default=False)
@@ -103,15 +120,17 @@ class QualificationType(AuditModel):
     class Meta:
         db_table = 'registry_qualification'
         ordering = ['sort_order', 'description']
+        verbose_name_plural = 'Qualification under a given Activity Type and Subtype'
 
     def __str__(self):
-        return '%s (%s - %s)' % (self.description
-            ,self.registry_activity_type.code
-            ,self.registry_activity_type.description
+        return '%s (%s)' % (self.description
+            ,self.registry_activity_subtype.description
             )
 
 class RegistryApplication(AuditModel):
-
+    """
+    Application from a well driller or pump installer to be on the GWELLS Registry.
+    """
     application_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,
         verbose_name="Registry Application UUID, hidden from users")
     person = models.ForeignKey(Person, db_column='person_guid', on_delete=models.CASCADE, verbose_name="Person Reference")
@@ -130,13 +149,12 @@ class RegistryApplication(AuditModel):
             ,self.person.surname
             ,self.file_no)
 
-
-# TODO - GW for the one-off application where a secondary Certificate is attached
+# TODO - GW A related model/table for the one-off application where a secondary certificate(s) are attached
 class ClassificationAppliedFor(AuditModel):
     classification_applied_for_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,
         verbose_name="UUID of the Registry Classification being applied for, hidden from users")
     registry_application = models.ForeignKey(RegistryApplication, db_column='application_guid', on_delete=models.CASCADE, verbose_name="Application Reference")
-    registry_activity_type = models.ForeignKey(ActivityType, null=True, db_column='registry_activity_type_guid', on_delete=models.CASCADE)
+    registry_activity_subtype = models.ForeignKey(ActivitySubtype, null=True, db_column='registry_activity_subtype_guid', on_delete=models.CASCADE)
 
     primary_certificate_authority = models.ForeignKey(Organization, db_column='org_guid', on_delete=models.CASCADE, limit_choices_to={'certificate_authority': True}
         ,verbose_name="Certifying Organization")
@@ -148,10 +166,99 @@ class ClassificationAppliedFor(AuditModel):
 
     def __str__(self):
         return '%s : %s %s %s %s (%s)' % (self.classification_applied_for_guid
-            ,self.registry_application.application_guid
-            ,self.registry_application.person.surname            
+            ,self.registry_application.person.first_name            
             ,self.registry_application.person.surname
-            ,self.registry_activity_type.description
+            ,self.registry_activity_subtype.description
             ,self.registry_application.file_no)
 
+class RegistryStatusCode(AuditModel):
+    """
+    Status of the Registry Entry
+    """
+    registry_status_code_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=10, unique=True)
+    description = models.CharField(max_length=100)
+    is_hidden = models.BooleanField(default=False)
+    sort_order = models.PositiveIntegerField()
 
+    class Meta:
+        db_table = 'registry_status_code'
+        ordering = ['sort_order', 'description']
+        verbose_name_plural = 'Status Code of Registry Entries'
+
+    def __str__(self):
+        return self.description
+
+class Registry(AuditModel):
+
+    registry_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,
+        verbose_name="Registry UUID, hidden from users")
+    # TODO - GW constraint to ensure that DRILL/PUMP ActivityType of this entry is consistent with the Application
+    registry_activity_type = models.ForeignKey(ActivityType, db_column='registry_activity_type_guid', on_delete=models.CASCADE, blank=True)
+    registry_application = models.ForeignKey(RegistryApplication, db_column='application_guid', on_delete=models.CASCADE, blank=True, null=True, verbose_name="Application Reference")
+
+    registration_no = models.CharField(max_length=15,blank=True, null=True)    
+    registry_status = models.ForeignKey(RegistryStatusCode, db_column='registry_status_code_guid', on_delete=models.CASCADE, verbose_name="Registration Entry Status")
+    registration_date = models.DateField(blank=True, null=True)
+    registration_removal_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'registry'
+        verbose_name_plural = 'Registration of Drillers and Pump Installers'
+
+    def __str__(self):
+        return '%s : %s %s %s %s %s %s %s' % (self.registry_guid
+            ,self.registry_activity_type.description
+            ,self.registration_no
+            ,self.registry_application.file_no
+            ,self.registry_status
+            ,self.registration_date
+            ,self.registry_application.person.first_name
+            ,self.registry_application.person.surname
+            )
+
+
+class ApplicationStatusCode(AuditModel):
+    """
+    Status of Applications for the Well Driller and Pump Installer Registry
+    """
+    registry_application_status_code_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=10, unique=True)
+    description = models.CharField(max_length=100)
+    is_hidden = models.BooleanField(default=False)
+    sort_order = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'registry_application_status_code'
+        ordering = ['sort_order', 'description']
+        verbose_name_plural = 'Status of Applications'
+
+    def __str__(self):
+        return self.description
+
+class RegistryApplicationStatus(AuditModel):
+    application_status_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,
+        verbose_name="Registry Application Status UUID, hidden from users")
+    application = models.ForeignKey(RegistryApplication, db_column='application_guid', on_delete=models.CASCADE, verbose_name="Application Reference")
+    status = models.ForeignKey(RegistryStatusCode, db_column='registry_status_code_guid', on_delete=models.CASCADE, verbose_name="Status Code Reference")
+    notified_date = models.DateField(blank=True, null=True,default=datetime.date.today)
+
+    # TODO - GW Replace defaults with save() method, see
+    # ../../gwells/models/AuditModel.py and 
+    # https://stackoverflow.com/questions/1737017/django-auto-now-and-auto-now-add
+    effective_date = models.DateField(default=datetime.date.today)
+    expired_date = models.DateField(blank=True, null=True,default=datetime.date.today)
+
+
+    class Meta:
+        db_table = 'registry_application_status'
+        ordering = ['application', 'effective_date']        
+        verbose_name_plural = 'Status for a given Application'
+
+    def __str__(self):
+        return '%s : %s %s %s %s %s %s %s' % (self.application_status_guid
+            ,self.application.file_no
+            ,self.status.description
+            ,self.effective_date
+            ,self.expired_date
+            )
