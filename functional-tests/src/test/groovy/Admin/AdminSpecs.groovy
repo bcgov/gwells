cb38675c06f274c6273d09de55fbda544a2af42a
Roland Stens <roland.stens@gmail.com>
2018-01-31 16:14:08 -0800

    Test updates and ZAP run update

---
+++ functional-tests/src/test/groovy/Admin/AdminSpecs.groovy
@@ -0,0 +1,14 @@
+import geb.spock.GebReportingSpec
+import spock.lang.*
+
+@Stepwise
+@Title("Screen to allow App Admin to manage user roles")
+@Narrative("""<No Narritive yet>""")
+@See("https://trello.com/c/YV7QgoIl")
+class AdminSpecs extends GebReportingSpec {
+
+    @Unroll
+    @Ignore("TODO")
+    @Issue("https://trello.com/c/YV7QgoIl")
+    def "Scenario: 1 - Manage User Roles"(){ }
+}
