31ba2345c52b7fc8d74d6ba80bfd8150964957bc
Steve <stephenhillier@gmail.com>
2018-02-07 13:27:49 -0800

    Add create functionality (list/create endpoint)

--- registries/views.py
+++ registries/views.py
@@ -1,17 +1,27 @@
 from django.http import HttpResponse
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework.response import Response
 from registries.models import Organization
-from registries.serializers import DrillerListSerializer, DrillerDetailSerializer
+from registries.serializers import DrillerListSerializer, DrillerSerializer
 
-class APIDrillerListView(ListAPIView):
+# class APIDrillerListView(ListAPIView):
+#     queryset = Organization.objects.all().select_related('province_state')
+#     serializer_class = DrillerListSerializer
+
+class APIDrillerListCreateView(ListCreateAPIView):
     queryset = Organization.objects.all().select_related('province_state')
-    serializer_class = DrillerListSerializer
+    serializer_class = DrillerSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = DrillerListSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class APIDrillerDetailView(RetrieveAPIView):
     queryset = Organization.objects.all()
     lookup_field = "org_guid"
-    serializer_class = DrillerDetailSerializer
+    serializer_class = DrillerSerializer
 
 
 # Create your views here.
