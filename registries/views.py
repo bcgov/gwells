daf608063fbbc1efa6adf8accf99df93652e226f
Steve <stephenhillier@gmail.com>
2018-02-07 12:05:48 -0800

    Add basic detail view

--- registries/views.py
+++ registries/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from registries.models import Organization
-from registries.serializers import DrillerListSerializer
+from registries.serializers import DrillerListSerializer, DrillerDetailSerializer
 
 class APIDrillerListView(ListAPIView):
     queryset = Organization.objects.all().select_related('province_state')
     serializer_class = DrillerListSerializer
+
+
+class APIDrillerDetailView(RetrieveAPIView):
+    queryset = Organization.objects.all()
+    lookup_field = "org_guid"
+    serializer_class = DrillerDetailSerializer
+
 
 # Create your views here.
 def index(request):
