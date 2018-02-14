323f6c20006812853c76432baf2aa856a65a77ff
Steve <stephenhillier@gmail.com>
2018-02-14 13:55:08 -0800

    Change driller list view behavior (logged in users get full list, anon users only see active registered drillers)

--- gwells/settings.py
+++ gwells/settings.py
@@ -196,7 +196,7 @@ AUTH_USER_MODEL='gwells.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
