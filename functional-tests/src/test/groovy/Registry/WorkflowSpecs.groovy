b9d2fd94b62fcec8748ad5c63909f98536e7572d
Roland Stens <roland.stens@gmail.com>
2018-02-14 11:46:04 -0800

    Test Refactoring and Additions

---
+++ functional-tests/src/test/groovy/Registry/WorkflowSpecs.groovy
@@ -0,0 +1,14 @@
+import geb.spock.GebReportingSpec
+import spock.lang.*
+
+@Title("Workflow Between GW Staff and Comptroller")
+@Narrative("""""")
+@See("https://trello.com/c/cadeg3eb")
+class WorkflowSpecs extends GebReportingSpec {
+
+    @Unroll
+    @Ignore("TODO")
+    @Issue("https://trello.com/c/cadeg3eb")
+    def "Scenario: 1 - Workflow"(){
+     }
+}
