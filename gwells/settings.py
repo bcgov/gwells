8326d509c2dd467bbf945cdf759ef8d8c195418c
Steve <stephenhillier@gmail.com>
2018-02-14 09:44:23 -0800

    Add throttling with a default rate

--- gwells/settings.py
+++ gwells/settings.py
@@ -200,5 +200,13 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '200/hour',
+        'user': '1000/hour'
+    }
 }
