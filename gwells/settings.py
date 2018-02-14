f2b156a183e8326f1ab9f1816a3ca69262ba4a5b
Steve Hillier <stephenhillier@gmail.com>
2018-02-13 22:31:46 -0800

    Add basic JWT auth for testing

--- gwells/settings.py
+++ gwells/settings.py
@@ -193,3 +193,12 @@ LOGGING = {
 }
 
 AUTH_USER_MODEL='gwells.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    )
+}
