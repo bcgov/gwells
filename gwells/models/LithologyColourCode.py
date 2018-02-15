ae72e7ae59b9233309bc9651747d8e0b2429db76
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2018-02-14 23:43:29 -0800

    V3.1 of updating Model to match DA standards

--- gwells/models/LithologyColourCode.py
+++ gwells/models/LithologyColourCode.py
@@ -21,7 +21,7 @@ class LithologyColourCode(AuditModel):
     Colour of the lithology
     """
     lithology_colour_guid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    code = models.CharField(max_length=10, unique=True)
+    lithology_colour_code = models.CharField(max_length=10, unique=True)
     description = models.CharField(max_length=100)
     is_hidden = models.BooleanField(default=False)
     display_order = models.PositiveIntegerField()
