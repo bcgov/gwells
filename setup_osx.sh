8b18e92f50981d13b93a9b00b2d9b1007c5e5c66
Derek Roberts <derek.roberts@gmail.com>
2018-02-17 20:07:30 -0800

    +setup_osx.sh - start of setup for OS X

---
+++ setup_osx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+set -eux
+
+
+# Install Homebrew
+#
+which brew || \
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# Brew install packages
+#
+PACKAGES="git postgresql python3"
+for p in $PACKAGES
+do
+	which $p || brew install $p
+done
+
+
+# Configure git
+#
+git config --global --get push.default ||
+	git config --global push.default simple
+git config --global --get user.email &&
+	git config --global --get user.name ||
+	git config --global --edit
