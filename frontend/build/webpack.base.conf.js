83232d415bb0c2f6fadcac0d43918fc1b5ca8a47
Steve <stephenhillier@gmail.com>
2018-02-22 13:02:03 -0800

    Change directory structure to accomodate future apps

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -22,7 +22,7 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    registry: './src/registry/registry.js'
   },
   output: {
     path: config.build.assetsRoot,
@@ -35,7 +35,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
