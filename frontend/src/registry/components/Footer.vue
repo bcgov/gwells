3b58b7e0f70d4ca670f316149876b1e7647c2ccf
Steve <stephenhillier@gmail.com>
2018-02-22 14:36:04 -0800

    Testing VueJS and BCGov bootstrap compatibility

---
+++ frontend/src/registry/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+<div id="footer" role="contentinfo">
+    <img class="back-to-top" src="images/back-to-top.png" alt="Back to top" title="Back to top" style="display: inline;">
+    <div id="footerWrapper">
+        <div id="footerAdminSection">
+            <div id="footerAdminLinksContainer" class="container">
+                <div id="footerAdminLinks" class="row">
+                    <ul class="inline">
+                        <li>
+                            <a href="#" target="_self">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" target="_self">About this site</a>
+                        </li>
+                        <li>
+                            <a href="http://gov.bc.ca/disclaimer/" target="_self">Disclaimer</a>
+                        </li>
+                        <li>
+                            <a href="http://gov.bc.ca/privacy/" target="_self">Privacy</a>
+                        </li>
+                        <li>
+                            <a href="http://gov.bc.ca/webaccessibility/" target="_self">Accessibility</a>
+                        </li>
+                        <li>
+                            <a href="http://gov.bc.ca/copyright" target="_self">Copyright</a>
+                        </li>
+                        <li>
+                            <a href="#" target="_self">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/registry/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+    <div id="header" role="banner">
+        <div id="header-main" class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div id="header-main-row" class="row">
+                    <!-- GOV TITLE -->
+                    <div class="col-sm-3 col-md-2 col-lg-2 header-main-left">
+                        <div id="logo">
+                            <a href="http://gov.bc.ca"><img src="../../assets/images/gov3_bc_logo.png" alt="Province of British Columbia" title="Province of British Columbia logo" /></a>
+                        </div>
+                        <div id="access">
+                            <ul>
+                                <li aria-label="Keyboard Tab Skip">
+                                    <a accesskey="0" href="#main-content-anchor" aria-label="Skip to main content">Skip to main content</a>
+                                </li>
+                                <li aria-label="Keyboard Tab Skip">
+                                    <a accesskey="0" href="#navigation-anchor" aria-label="Skip to navigation">Skip to navigation</a>
+                                </li>
+                                <li aria-label="Keyboard Tab Skip">
+                                    <a accesskey="0" href="http://gov.bc.ca/webaccessibility/" aria-label="Accessibility Statement">Accessibility Statement</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <button type="button" class="navbar-toggle gov-button-custom collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="true" aria-label="Burger Navigation">
+                            <img src="images/menu-open-mobile.png" />
+                        </button>
+                    </div>
+                    <div class="col-sm-8 col-md-8 col-lg-6 hidden-xs">
+                        <div class="bcgov-title">
+                            <h1>Gov 2.0 Bootstrap Skeleton</h1>
+                        </div>
+                    </div>
+                    <!-- /GOV & ENV TITLE -->
+
+                    <!-- COLLAPSING NAVBAR -->
+                    <div id="navbar" class="collapse navbar-collapse" role="navigation">
+                        <a id="navigation-anchor"></a>
+                        <ul class="nav navbar-nav">
+                            <li><a href="#">Navigation Link 1</a></li>
+                            <li><a href="#">Navigation Link 2</a></li>
+                            <li><a href="#">Navigation Link 3</a></li>
+                            <li><a href="#">Navigation Link 4</a></li>
+                        </ul>
+                    </div>
+                    <!-- /COLLAPSING NAVBAR -->
+                </div>
+            </div>
+            <div class="navigationRibbon">
+                <div class="level2Navigation">
+                    <div class="container">
+                        <a href="#">Navigation Link 1</a>
+                        <a href="#">Navigation Link 2</a>
+                        <a href="#">Navigation Link 3</a>
+                        <a href="#">Navigation Link 4</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
