b04710eb1c49d6e7e895c5b4d088b6d885724b09
Steve <stephenhillier@gmail.com>
2018-02-23 12:48:51 -0800

    Add bootstrap-vue as dependency

--- frontend/src/registry/registry.js
+++ frontend/src/registry/registry.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
