8bb7b3f03e47f36d72e630a7d1963ea6615bef29
Steve <stephenhillier@gmail.com>
2018-02-22 16:19:15 -0800

    Add simple layout for list of companies with registered drillers

--- frontend/src/registry/router/index.js
+++ frontend/src/registry/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/registry/components/HelloWorld'
+import DrillerTable from '@/registry/components/DrillerTable'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'DrillerTable',
+      component: DrillerTable
     }
   ]
 })
