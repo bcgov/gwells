6619d099567c9e53fc960335684bebf81e6bc98c
Roland Stens <roland.stens@gmail.com>
2018-02-23 13:51:11 -0800

    Update runzapdev.sh

--- zap/runzapdev.sh
+++ zap/runzapdev.sh
@@ -1,1 +1,1 @@
-/zap/zap-baseline.py -r index.html -t https://dev.lowcarbonfuels.gov.bc.ca -z "-session TFRS.session"
+/zap/zap-baseline.py -r index.html -t https://dev.lowcarbonfuels.gov.bc.ca -z "-session /home/jenkins/workspace/moe-gwells-tools-gwells-pipeline-zap/zap/TFRS.session"
