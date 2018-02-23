d873f41eee47e100390957305d60d780dcd10410
Roland Stens <roland.stens@gmail.com>
2018-02-22 16:20:07 -0800

    Update runnewman.sh

--- api-tests/runnewman.sh
+++ api-tests/runnewman.sh
@@ -1,4 +1,3 @@
-npm install -g newman
 export GWELLS_API_BASE_URL="https://gwells-dev.pathfinder.gov.bc.ca/gwells/registries"
 export GWELLS_API_TEST_USER="testuser"
 export GWELLS_API_TEST_PASSWORD="testpassword"
