47e270f1d3acdf9599ec167c925290456f81596c
Roland Stens <roland.stens@gmail.com>
2018-02-23 09:31:20 -0800

    Update runnewman.sh

--- api-tests/runnewman.sh
+++ api-tests/runnewman.sh
@@ -1,4 +1,4 @@
 export GWELLS_API_BASE_URL="https://gwells-dev.pathfinder.gov.bc.ca/gwells/registries"
 export GWELLS_API_TEST_USER="testuser"
 export GWELLS_API_TEST_PASSWORD="testpassword"
-newman run registries_api_tests.json --global-var base_url="https://gwells-dev.pathfinder.gov.bc.ca/gwells/registries" --global-var test_user="testuser" --global-var test_password="testpassword" -r cli,junit,html
+newman run registries_api_tests.json --global-var base_url="https://gwells-dev.pathfinder.gov.bc.ca/gwells/registries" --global-var test_user="testuser" --global-var test_password="testpassword" -r cli,junit,html;
