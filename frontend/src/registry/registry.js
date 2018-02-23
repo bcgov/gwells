a00b31e380ca15810bf323e1c2d4bd27fedb667b
Steve <stephenhillier@gmail.com>
2018-02-23 15:44:45 -0800

    Add vuex to help manage state

--- frontend/src/registry/registry.js
+++ frontend/src/registry/registry.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 
+Vue.use(Vuex)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
