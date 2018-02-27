13fc78dc76e468ca434d256c152d18bbd964582a
Steve <stephenhillier@gmail.com>
2018-02-27 10:20:24 -0800

    Remove promise rejection handling from api helper functions

--- frontend/src/common/services/gwells.js
+++ frontend/src/common/services/gwells.js
@@ -9,24 +9,15 @@ const ApiService = {
   },
   query (resource, params) {
     return axios.get(resource, { params: params })
-      .catch((error) => {
-        throw new Error(`API query: ${error}`)
-      })
   },
   get (resource, uuid) {
     if (uuid.length && uuid.length === 36) {
       return axios.get(`${resource}/${uuid}`)
-        .catch((error) => {
-          throw new Error(`API get: ${error}`)
-        })
     }
     throw new Error(`API get: UUID required to fetch ${resource}`)
   },
   post (resource, params) {
     return axios.post(resource, params)
-      .catch((error) => {
-        throw new Error(`API post: ${error}`)
-      })
   }
 }
 
