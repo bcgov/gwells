b82cc48698f91fb721ef73a5d8357230ebe5f22d
Steve <stephenhillier@gmail.com>
2018-02-28 10:22:28 -0800

    Reorganize test config and app entry point to make for easier testing

--- frontend/test/unit/specs/RegisterHome.spec.js
+++ frontend/test/unit/specs/RegisterHome.spec.js
@@ -5,7 +5,7 @@ describe('RegisterHome.vue', () => {
   it('renders the correct title', () => {
     const Constructor = Vue.extend(RegisterHome)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('#registry-title h2').textContent)
+    expect(vm.$el.querySelector('#registry-title').textContent)
       .to.equal('Register of Well Drillers and Well Pump Installers')
   })
 })
