42e12ce1323dae2580e0e44c5b2e2f265bd19ded
Steve <stephenhillier@gmail.com>
2018-02-27 16:29:02 -0800

    Change error state variable name to be more specific

--- frontend/src/registry/store/mutations.types.js
+++ frontend/src/registry/store/mutations.types.js
@@ -1,4 +1,4 @@
 export const SET_LOADING = 'setLoading'
-export const SET_ERROR = 'setError'
+export const SET_LIST_ERROR = 'setListError'
 export const SET_DRILLER_LIST = 'setDrillerList'
 export const SET_DRILLER = 'setDriller'
