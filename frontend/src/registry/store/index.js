cea2be2a2a4468f59be89da9cd220da45c53b3b4
Steve <stephenhillier@gmail.com>
2018-02-28 15:27:09 -0800

    Add actions, state, mutations and getter for currentDriller

--- frontend/src/registry/store/index.js
+++ frontend/src/registry/store/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import ApiService from '@/common/services/gwells'
-import { FETCH_DRILLER_LIST } from './actions.types.js'
-import { SET_DRILLER_LIST, SET_LOADING, SET_LIST_ERROR } from './mutations.types.js'
+import { FETCH_DRILLER, FETCH_DRILLER_LIST } from './actions.types.js'
+import {
+  SET_DRILLER,
+  SET_DRILLER_LIST,
+  SET_LOADING,
+  SET_ERROR,
+  SET_LIST_ERROR
+} from './mutations.types.js'
 
 Vue.use(Vuex)
 
@@ -10,42 +16,46 @@ export const store = new Vuex.Store({
   state: {
     currentUser: 'anonymous',
     loading: false,
+    error: null,
     listError: null,
-    drillerList: []
+    drillerList: [],
+    currentDriller: {}
   },
   mutations: {
     [SET_LOADING] (state, payload) {
       state.loading = payload
     },
+    [SET_ERROR] (state, payload) {
+      state.error = payload
+    },
     [SET_LIST_ERROR] (state, payload) {
       state.listError = payload
+    },
+    [SET_DRILLER] (state, payload) {
+      state.currentDriller = payload
     },
     [SET_DRILLER_LIST] (state, payload) {
       state.drillerList = payload
     }
   },
   actions: {
+    [FETCH_DRILLER] ({commit}, guid) {
+      commit(SET_LOADING, true)
+      ApiService.get('drillers', guid)
+        .then((response) => {
+          commit(SET_LOADING, false)
+          commit(SET_ERROR, null)
+          commit(SET_DRILLER, response.data)
+        })
+        .catch((error) => {
+          commit(SET_LOADING, false)
+          commit(SET_ERROR, error.response)
+        })
+    },
     [FETCH_DRILLER_LIST] ({commit}, params) {
       commit(SET_LOADING, true)
       ApiService.query('drillers', params)
         .then((response) => {
-          // const drillers = []
-          // response.data.results.forEach((driller) => {
-          //   const company = driller.companies.length ? driller.companies[0] : {}
-          //   const companyName = company.organization_name || ''
-          //   // const address = company.length ? company.street_address
-          //   const tel = company.contact_tel || ''
-          //   const email = company.contact_email || ''
-          //   const status = driller.applications[0].registrations[0].status
-          //   const details = {
-          //     name: `${driller.first_name} ${driller.surname}`,
-          //     company: companyName,
-          //     tel: tel,
-          //     email: email,
-          //     status: status
-          //   }
-          //   drillers.push(details)
-          // })
           commit(SET_LOADING, false)
           commit(SET_LIST_ERROR, null)
           commit(SET_DRILLER_LIST, response.data)
@@ -65,6 +75,9 @@ export const store = new Vuex.Store({
     },
     drillers (state) {
       return state.drillerList
+    },
+    currentDriller (state) {
+      return state.currentDriller
     }
   }
 })
