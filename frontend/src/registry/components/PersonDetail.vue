aeb643892150365e34479488a3575fcc63a02642
Steve Hillier <stephenhillier@gmail.com>
2018-02-28 21:09:41 -0800

    Add formatting to driller detail page

--- frontend/src/registry/components/PersonDetail.vue
+++ frontend/src/registry/components/PersonDetail.vue
@@ -11,56 +11,56 @@
         <h2>Certification - {{ person.activityDescription }}</h2>
       </div>
     </div>
-    <fieldset>
+    <fieldset class="registry-section">
       <legend>Certification</legend>
       <div class="row">
-        <div class="col-xs-12 col-sm-4">
+        <div class="col-xs-12 col-sm-4 registry-item">
           Certificate number: {{ person.certNumber }}
         </div>
-        <div class="col-xs-12 col-sm-4">
+        <div class="col-xs-12 col-sm-4 registry-item">
           Issued by: {{ person.certAuthority }}
         </div>
       </div>
     </fieldset>
-    <fieldset>
+    <fieldset class="registry-section">
       <legend>Classification and Qualifications</legend>
       <div class="row">
         <h4>Qualification</h4>
       </div>
       <div class="row">
-        <div class="col-xs-12 col-sm-4">
+        <div class="col-xs-12 col-sm-4 registry-item">
           Issued by: {{ person.certAuthority }}
         </div>
-        <div class="col-xs-12 col-sm-4">
+        <div class="col-xs-12 col-sm-4 registry-item">
           Certificate number: {{ person.certNumber }}
         </div>
       </div>
     </fieldset>
-    <fieldset>
+    <fieldset class="registry-section">
       <legend>Adjudication</legend>
       <div class="row">
-        <div class="col-xs-12">
+        <div class="col-xs-12 registry-item">
           Date application received:
         </div>
       </div>
       <div class="row">
-        <div class="col-xs-12 col-sm-4">
+        <div class="col-xs-12 col-sm-4 registry-item">
           Approval outcome date:
         </div>
-        <div class="col-xs-12 col-sm-4">
+        <div class="col-xs-12 col-sm-4 registry-item">
           Approval outcome:
         </div>
-        <div class="col-xs-12 col-sm-4">
+        <div class="col-xs-12 col-sm-4 registry-item">
           Reason denied:
         </div>
       </div>
       <div class="row">
-        <div class="col-xs-12 col-sm-3">
+        <div class="col-xs-12 col-sm-3 registry-item">
           Register removal date:
         </div>
       </div>
       <div class="row">
-        <div class="col-xs-12">
+        <div class="col-xs-12 registry-item">
           As Deputy Comptroller, I confirm I have reviewed the application or action and approved this registry update.
         </div>
       </div>
@@ -105,8 +105,11 @@ export default {
 </script>
 
 <style>
-.panel-heading {
-  padding-bottom: 0px!important;
-  margin-bottom: 0px!important;
+.registry-section {
+  margin-top: 25px;
+  margin-bottom: 20px;
+}
+.registry-item {
+  margin-bottom: 20px;
 }
 </style>
