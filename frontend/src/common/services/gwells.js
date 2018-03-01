a548f5c4bc2dd0473085555d4c670627952cadbc
Steve <stephenhillier@gmail.com>
2018-03-01 10:49:26 -0800

    remove unnecessary Vue.use that causes error message

--- frontend/src/common/services/gwells.js
+++ frontend/src/common/services/gwells.js
@@ -1,10 +1,8 @@
-import Vue from 'vue'
 import axios from 'axios'
 const BASE_URL = 'http://localhost:8000/gwells/registries/'
 
 const ApiService = {
   init () {
-    Vue.use(axios)
     axios.defaults.baseURL = BASE_URL
   },
   query (resource, params) {
