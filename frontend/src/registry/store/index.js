ec4e26b00283c58c49c9df716acd22f5f4643a79
Steve <stephenhillier@gmail.com>
2018-03-02 16:30:20 -0800

    add simple logging

--- frontend/src/registry/store/index.js
+++ frontend/src/registry/store/index.js
@@ -16,6 +16,8 @@ import {
 
 Vue.use(Vuex)
 
+const logging = true
+
 export const store = new Vuex.Store({
   state: {
     user: null,
@@ -38,6 +40,7 @@ export const store = new Vuex.Store({
     },
     [SET_USER] (state, payload) {
       state.user = payload
+      logging && console.log('switched to user', payload && payload.username)
     },
     [SET_CITY_LIST] (state, payload) {
       state.cityList = payload
@@ -51,34 +54,42 @@ export const store = new Vuex.Store({
   },
   actions: {
     [FETCH_CITY_LIST] ({commit}, activity) {
+      logging && console.log('fetching city list...')
       ApiService.query('cities/' + activity + '/')
         .then((response) => {
           commit(SET_CITY_LIST, response.data)
+          logging && console.log('fetched city list:', response.data)
         })
         .catch((error) => {
           commit(SET_ERROR, error.response)
+          logging && console.log('error fetching cities:', error.response)
         })
     },
     [FETCH_DRILLER] ({commit}, guid) {
+      logging && console.log('fetching driller...')
       commit(SET_LOADING, true)
       ApiService.get('drillers', guid)
         .then((response) => {
           commit(SET_LOADING, false)
           commit(SET_ERROR, null)
           commit(SET_DRILLER, response.data)
+          logging && console.log('fetched driller:', response.data)
         })
         .catch((error) => {
           commit(SET_LOADING, false)
           commit(SET_ERROR, error.response)
+          logging && console.log('error fetching driller:', error.response)
         })
     },
     [FETCH_DRILLER_LIST] ({commit}, params) {
       commit(SET_LOADING, true)
+      logging && console.log('fetching driller list...')
       ApiService.query('drillers/', params)
         .then((response) => {
           commit(SET_LOADING, false)
           commit(SET_LIST_ERROR, null)
           commit(SET_DRILLER_LIST, response.data)
+          logging && console.log('fetched driller list:', response.data)
         })
         .catch((error) => {
           commit(SET_LOADING, false)
