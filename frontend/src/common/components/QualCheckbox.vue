83e748cb94aacb1cfd9450e22b7c4b5e45cf0d94
Steve Hillier <stephenhillier@gmail.com>
2018-03-04 11:55:50 -0800

    switch to truthy logic for displaying checkbox or not

--- frontend/src/common/components/QualCheckbox.vue
+++ frontend/src/common/components/QualCheckbox.vue
@@ -1,6 +1,6 @@
 <template>
   <span>
-    <i class="fa fa-check-square registry-checkbox" v-if="checked==='true'"></i>
+    <i class="fa fa-check-square registry-checkbox" v-if="checked"></i>
     <i class="fa fa-square-o registry-checkbox" v-else></i>
   </span>
 </template>
