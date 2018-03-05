6fc58e3c014f208a39f9c495095cd6f098144757
Steve Hillier <stephenhillier@gmail.com>
2018-03-04 21:14:50 -0800

    add legal text

--- frontend/src/registry/components/RegisterHome.vue
+++ frontend/src/registry/components/RegisterHome.vue
@@ -1,137 +1,143 @@
 <template>
   <div class="container-fluid">
-      <div class="row form-spacing">
-        <div class="col-xs-12 col-sm-7">
-          <a id="main-content-anchor"></a>
-          <h2 id="registry-title">Register of Well Drillers and Well Pump Installers</h2>
-            <p><a href="#">Learn more about registering as a well driller or well pump installer in B.C.</a></p>
-        </div>
-        <div class="col-xs-12 col-sm-5 text-center">
-          <button v-if="!user" type="button" class="btn btn-primary" @click="loginPanelToggle = !loginPanelToggle">Log in</button>
-          <button v-if="user" type="button" class="btn btn-secondary" @click="logout">Log out</button>
-          <button v-if="user" type="button" class="btn btn-primary" @click="adminPanelToggle=!adminPanelToggle">Options</button>
-        </div>
+    <div class="row form-spacing">
+      <div class="col-xs-12 col-sm-7">
+        <a id="main-content-anchor"></a>
+        <h2 id="registry-title">Register of Well Drillers and Well Pump Installers</h2>
+          <p><a target="_blank" href="https://www2.gov.bc.ca/gov/content/environment/air-land-water/water/groundwater-wells/information-for-well-drillers-well-pump-installers/what-you-need-to-practice-in-bc">Learn more about registering as a well driller or well pump installer in B.C.</a></p>
       </div>
-      <div class="row" v-if="!user && loginPanelToggle">
-        <div class="col-xs-12">
-            <div class="well well-sm">
-              <div class="container-fluid">
-                <form @submit.prevent="login">
-                  <div class="form-group">
-                    <div class="col-xs-12 col-sm-2">
-                      <label for="loginUser">Username</label>
-                      <input type="text" class="form-control" id="loginUser" placeholder="Search" v-model="credentials.username">
-                    </div>
-                    <div class="col-xs-12 col-sm-2">
-                      <label for="loginPassword">Password</label>
-                      <input type="password" class="form-control" id="loginPassword" placeholder="Password" v-model="credentials.password">
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="col-xs-12">
-                      <button type="submit" class="btn btn-primary">Login</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-        </div>
+      <div class="col-xs-12 col-sm-5 text-center">
+        <button v-if="!user" type="button" class="btn btn-primary" @click="loginPanelToggle = !loginPanelToggle">Log in</button>
+        <button v-if="user" type="button" class="btn btn-secondary" @click="logout">Log out</button>
+        <button v-if="user" type="button" class="btn btn-primary" @click="adminPanelToggle=!adminPanelToggle">Options</button>
       </div>
-      <div class="row" v-if="adminPanelToggle">
-        <div class="col-xs-12">
-            <div class="well well-sm">
-              <div v-if="user">
-                <div>Logged in as {{ user.username }}</div>
-                <div>
-                  <button type="button" class="btn btn-primary">Add new entry</button>
-                  <button type="button" class="btn btn-primary">Manage companies</button>
+    </div>
+    <div class="row" v-if="!user && loginPanelToggle">
+      <div class="col-xs-12">
+          <div class="well well-sm">
+            <div class="container-fluid">
+              <form @submit.prevent="login">
+                <div class="form-group">
+                  <div class="col-xs-12 col-sm-2">
+                    <label for="loginUser">Username</label>
+                    <input type="text" class="form-control" id="loginUser" placeholder="Search" v-model="credentials.username">
+                  </div>
+                  <div class="col-xs-12 col-sm-2">
+                    <label for="loginPassword">Password</label>
+                    <input type="password" class="form-control" id="loginPassword" placeholder="Password" v-model="credentials.password">
+                  </div>
                 </div>
+                <div class="form-group">
+                  <div class="col-xs-12">
+                    <button type="submit" class="btn btn-primary">Login</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class="row" v-if="adminPanelToggle">
+      <div class="col-xs-12">
+          <div class="well well-sm">
+            <div v-if="user">
+              <div>Logged in as {{ user.username }}</div>
+              <div>
+                <button type="button" class="btn btn-primary">Add new entry</button>
+                <button type="button" class="btn btn-primary">Manage companies</button>
               </div>
             </div>
-        </div>
+          </div>
       </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="panel">
-            <div class="panel-body">
-              <div class="container-fluid">
-                <h3>Search for a Well Driller or Well Installer</h3>
-                <form @submit.prevent="drillerSearch" @reset.prevent="drillerSearchReset">
-                  <div class="form-group">
-                    <div class="col-xs-12">
-                      <label>Choose professional type: &nbsp;</label>
-                    </div>
-                    <div class="col-xs-12 form-spacing">
-                      <label class="radio-inline">
-                        <input type="radio" name="activitySelector" id="activityDriller" v-model="searchParams.activity" value="DRILL" style="margin-top: 0px"> Well Driller
-                      </label>
-                      <label class="radio-inline">
-                        <input type="radio" name="activitySelector" id="activityInstaller" v-model="searchParams.activity" value="PUMP" style="margin-top: 0px"> Pump Installer
-                      </label>
-                    </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="panel">
+          <div class="panel-body">
+            <div class="container-fluid">
+              <h3>Search for a Well Driller or Well Installer</h3>
+              <form @submit.prevent="drillerSearch" @reset.prevent="drillerSearchReset">
+                <div class="form-group">
+                  <div class="col-xs-12">
+                    <label>Choose professional type: &nbsp;</label>
                   </div>
-                  <div class="row">
-                    <div class="form-group">
-                      <div class="col-xs-12 col-sm-6 form-spacing">
-                        <label>Community</label>
-                        <select class="form-control" v-model="searchParams.city">
-                          <option value="">All</option>
-                          <option v-for="city in cityList" :key="city.city + city.province" :value="city.city + ',' + city.province_state">{{city.city}}<span v-if="city.province_state">, {{city.province_state}}</span></option>
-                        </select>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <div class="col-xs-12 col-sm-6 form-spacing">
-                        <label>Registration status</label>
-                        <select v-model="searchParams.status" class="form-control">
-                          <option value="" disabled>All</option>
-                          <option value="PENDING" disabled>Pending</option>
-                          <option value="INACTIVE" disabled>Not registered</option>
-                          <option value="ACTIVE">Registered</option>
-                          <option value="REMOVED" disabled>Removed</option>
-                        </select>
-                      </div>
-                    </div>
+                  <div class="col-xs-12 form-spacing">
+                    <label class="radio-inline">
+                      <input type="radio" name="activitySelector" id="activityDriller" v-model="searchParams.activity" value="DRILL" style="margin-top: 0px"> Well Driller
+                    </label>
+                    <label class="radio-inline">
+                      <input type="radio" name="activitySelector" id="activityInstaller" v-model="searchParams.activity" value="PUMP" style="margin-top: 0px"> Pump Installer
+                    </label>
                   </div>
+                </div>
+                <div class="row">
                   <div class="form-group">
                     <div class="col-xs-12 col-sm-6 form-spacing">
-                      <label for="regTypeInput">Individual, company, or registration number</label>
-                      <input type="text" class="form-control" id="regTypeInput" placeholder="Search" v-model="searchParams.search">
+                      <label>Community</label>
+                      <select class="form-control" v-model="searchParams.city">
+                        <option value="">All</option>
+                        <option v-for="city in cityList" :key="city.city + city.province" :value="city.city + ',' + city.province_state">{{city.city}}<span v-if="city.province_state">, {{city.province_state}}</span></option>
+                      </select>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <div class="col-xs-12 form-inline form-spacing">
-                        <select class="form-control input-sm" v-model="searchParams.limit"><option>10</option><option>25</option></select> entries
+                  <div class="form-group" v-if="user">
+                    <div class="col-xs-12 col-sm-6 form-spacing">
+                      <label>Registration status</label>
+                      <select v-model="searchParams.status" class="form-control">
+                        <option value="" disabled>All</option>
+                        <option value="PENDING" disabled>Pending</option>
+                        <option value="INACTIVE" disabled>Not registered</option>
+                        <option value="ACTIVE">Registered</option>
+                        <option value="REMOVED" disabled>Removed</option>
+                      </select>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <div class="col-xs-12">
-                      <button type="submit" class="btn btn-primary">Submit</button>
-                      <button type="reset" class="btn btn-secondary">Reset</button>
-                    </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-xs-12 col-sm-6 form-spacing">
+                    <label for="regTypeInput">Individual, company, or registration number</label>
+                    <input type="text" class="form-control" id="regTypeInput" placeholder="Search" v-model="searchParams.search">
                   </div>
-                </form>
-              </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-xs-12 form-inline form-spacing">
+                      <select class="form-control input-sm" v-model="searchParams.limit"><option>10</option><option>25</option></select> entries
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-xs-12">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="reset" class="btn btn-secondary">Reset</button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-xs-12 col-sm-4">
-          <h3>Well Driller Results</h3>
-        </div>
-        <div v-if="listError" class="col-xs-12 col-sm-7">
-          <api-error :error="listError" resetter="setListError"></api-error>
-        </div>
-        <div class="col-xs-12">
-          <register-table/>
-        </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-4">
+        <h3>Well Driller Results</h3>
       </div>
+      <div v-if="listError" class="col-xs-12 col-sm-7">
+        <api-error :error="listError" resetter="setListError"></api-error>
+      </div>
+      <div class="col-xs-12">
+        <register-table/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <register-legal-text class="register-legal" :activity="lastSearchedActivity"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import RegisterTable from '@/registry/components/RegisterTable'
+import LegalText from '@/registry/components/text/Legal'
 import APIErrorMessage from '@/common/components/APIErrorMessage'
 import { mapGetters } from 'vuex'
 import { LOGIN, LOGOUT, FETCH_CITY_LIST, FETCH_DRILLER_LIST } from '@/registry/store/actions.types'
@@ -139,12 +145,14 @@ import { LOGIN, LOGOUT, FETCH_CITY_LIST, FETCH_DRILLER_LIST } from '@/registry/s
 export default {
   components: {
     'register-table': RegisterTable,
-    'api-error': APIErrorMessage
+    'api-error': APIErrorMessage,
+    'register-legal-text': LegalText
   },
   data () {
     return {
       adminPanelToggle: false,
       loginPanelToggle: false,
+      lastSearchedActivity: 'DRILL',
       regStatusOptions: [
         { value: '', text: 'All', public: false },
         { value: 'PENDING', text: 'Pending', public: false },
@@ -208,6 +216,8 @@ export default {
         limit: this.searchParams.limit,
         activity: this.searchParams.activity
       }
+      this.lastSearchedActivity = this.searchParams.activity || 'DRILL'
+      console.log(this.lastSearchedActivity)
       this.$store.dispatch(FETCH_DRILLER_LIST, params)
     },
     drillerSearchReset () {
@@ -243,5 +253,8 @@ export default {
 .registry-disabled-item {
   color: #808080;
   cursor: auto!important;
+}
+.register-legal {
+  margin-top: 25px;
 }
 </style>
