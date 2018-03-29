53c3dcd2ad9343c7b02f3ab311d114cdabfff977
Steve Hillier <stephenhillier@gmail.com>
2018-03-29 11:50:19 -0700

    put bootstrap base css back into html template

--- frontend/src/registry/App.vue
+++ frontend/src/registry/App.vue
@@ -17,7 +17,6 @@ export default {
 </script>
 
 <style>
-@import url('https://unpkg.com/bootstrap@3.3.7/dist/css/bootstrap.min.css');
 @import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
 @import '../../../gwells/static/gwells/css/ie10-viewport-bug-workaround.css';
 @import '../../../gwells/static/gwells/css/main.css';
