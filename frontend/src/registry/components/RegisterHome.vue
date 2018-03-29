bee4eb4506edddd235be912d4da361040ba6dc61
Steve Hillier <stephenhillier@gmail.com>
2018-03-29 11:29:35 -0700

    add custom styles back to buttons

--- frontend/src/registry/components/RegisterHome.vue
+++ frontend/src/registry/components/RegisterHome.vue
@@ -128,7 +128,7 @@
                   <div class="form-group">
                     <div class="col-xs-12">
                       <button type="submit" class="btn btn-primary" id="personSearchSubmit">Submit</button>
-                      <button type="reset" class="btn btn-secondary" id="personSearchReset">Reset</button>
+                      <button type="reset" class="btn btn-default" id="personSearchReset">Reset</button>
                     </div>
                   </div>
                 </div>
@@ -329,9 +329,6 @@ export default {
   margin-top: 0px;
   padding-top:0px;
 }
-.btn {
-  margin-top: 5px;
-}
 .form-spacing {
   margin-bottom: 15px;
 }
@@ -341,5 +338,146 @@ export default {
 }
 .register-legal {
   margin-top: 25px;
+}
+.btn {
+    display: inline-block;
+    padding: 7px 12px 5px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    font-weight: normal;
+    line-height: 1.42857143;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -ms-touch-action: manipulation;
+    touch-action: manipulation;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    background-image: none;
+    border: 1px solid;
+    border-radius: 0px;
+}
+.btn-primary {
+    color: #fff;
+    background-color: #38598a;
+    border-color: #003366;
+}
+
+    .btn-primary:focus,
+    .btn-primary.focus {
+        color: #fff;
+        background-color: #003366;
+        border-color: #003366;
+    }
+
+    .btn-primary:hover {
+        color: #fff;
+        background-color: #003366;
+        border-color: #003366;
+    }
+
+    .btn-primary:active,
+    .btn-primary.active,
+    .open > .dropdown-toggle.btn-primary {
+        color: #fff;
+        background-color: #003366;
+        border-color: #003366;
+        background-image: none;
+    }
+
+        .btn-primary:active:hover,
+        .btn-primary.active:hover,
+        .open > .dropdown-toggle.btn-primary:hover,
+        .btn-primary:active:focus,
+        .btn-primary.active:focus,
+        .open > .dropdown-toggle.btn-primary:focus,
+        .btn-primary:active.focus,
+        .btn-primary.active.focus,
+        .open > .dropdown-toggle.btn-primary.focus {
+            color: #fff;
+            background-color: #204d74;
+            border-color: #003366;
+        }
+
+    .btn-primary.disabled,
+    .btn-primary.disabled:hover,
+    .btn-primary[disabled]:hover,
+    fieldset[disabled] .btn-primary:hover,
+    .btn-primary.disabled:focus,
+    .btn-primary[disabled]:focus,
+    fieldset[disabled] .btn-primary:focus,
+    .btn-primary.disabled.focus,
+    .btn-primary[disabled].focus,
+    fieldset[disabled] .btn-primary.focus {
+        background-color: #337ab7;
+        border-color: #2e6da4;
+    }
+
+    .btn-primary .badge {
+        color: #337ab7;
+        background-color: #fff;
+}
+.btn-default {
+    color: #333;
+    background-color: #d1d1d1;
+    border-color: #afafaf;
+}
+
+    .btn-default:focus,
+    .btn-default.focus {
+        color: #333;
+        background-color: #afafaf;
+        border-color: #88929a;
+    }
+
+    .btn-default:hover {
+        color: #333;
+        background-color: #afafaf;
+        border-color: #88929a;
+    }
+
+    .btn-default:active,
+    .btn-default.active,
+    .open > .dropdown-toggle.btn-default {
+        color: #333;
+        background-color: #afafaf;
+        border-color: #88929a;
+        background-image: none;
+    }
+
+        .btn-default:active:hover,
+        .btn-default.active:hover,
+        .open > .dropdown-toggle.btn-default:hover,
+        .btn-default:active:focus,
+        .btn-default.active:focus,
+        .open > .dropdown-toggle.btn-default:focus,
+        .btn-default:active.focus,
+        .btn-default.active.focus,
+        .open > .dropdown-toggle.btn-default.focus {
+            color: #333;
+            background-color: #afafaf;
+            border-color: #88929a;
+        }
+
+    .btn-default.disabled,
+    .btn-default.disabled:hover,
+    .btn-default[disabled]:hover,
+    fieldset[disabled] .btn-default:hover,
+    .btn-default.disabled:focus,
+    .btn-default[disabled]:focus,
+    fieldset[disabled] .btn-default:focus,
+    .btn-default.disabled.focus,
+    .btn-default[disabled].focus,
+    fieldset[disabled] .btn-default.focus {
+        background-color: #fff;
+        border-color: #ccc;
+    }
+
+    .btn-default .badge {
+        color: #fff;
+        background-color: #333;
 }
 </style>
