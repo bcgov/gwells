321f7c29f95ea034350acfece7d4feda9a82ee46
Steve Hillier <stephenhillier@gmail.com>
2018-04-01 22:39:10 -0700

    add login form

--- frontend/src/common/components/Header.vue
+++ frontend/src/common/components/Header.vue
@@ -1,13 +1,20 @@
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <b-navbar type="dark" class="navbar-expand-lg" bg-variant="primary">
       <!-- Navbar content -->
-      <div class="container">
+      <b-container>
         <a class="navbar-brand" href="https://www2.gov.bc.ca">
-
-          <img class="img-fluid d-none d-sm-block" src="@/common/assets/images/17_gov3_bc_logo.svg" width="152" height="55" alt="B.C. Government Logo">
-
-          <img class="img-fluid d-sm-none" src="@/common/assets/images/01_gov3_bc_symbol.svg" width="61" height="43" alt="B.C. Government Logo">
+          <img
+              class="img-fluid d-none d-sm-block"
+              src="@/common/assets/images/17_gov3_bc_logo.svg"
+              width="152" height="55"
+              alt="B.C. Government Logo">
+          <img
+              class="img-fluid d-sm-none"
+              src="@/common/assets/images/01_gov3_bc_symbol.svg"
+              width="61"
+              height="43"
+              alt="B.C. Government Logo">
         </a>
         <b-navbar-nav>
           <span class="bc-nav-title d-none d-md-block">Groundwater Wells and Aquifers</span>
@@ -15,24 +22,110 @@
         <b-navbar-nav class="ml-auto">
           <b-navbar-toggle class="d-sm-none" target="nav_collapse"></b-navbar-toggle>
         </b-navbar-nav>
-      </div>
-    </nav>
+      </b-container>
+    </b-navbar>
     <b-navbar class="bc-nav-links" toggleable="sm" type="dark">
       <b-collapse class="py-2" is-nav id="nav_collapse">
-        <b-container>
-        <b-navbar-nav>
-          <b-nav-item class="navbar-link lvl2-link" href="#">Well Search</b-nav-item>
-          <b-nav-item class="navbar-link lvl2-link" href="#">Groundwater Information</b-nav-item>
-          <b-nav-item class="navbar-link lvl2-link" href="#">Registry</b-nav-item>
-        </b-navbar-nav></b-container>
+        <b-container id="navContainer">
+          <b-navbar-nav>
+            <b-nav-text class="d-sm-none text-light">Groundwater Wells and Aquifers</b-nav-text>
+            <b-nav-item class="navbar-link lvl2-link" href="#">Well Search</b-nav-item>
+            <b-nav-item class="navbar-link lvl2-link" href="#">Groundwater Information</b-nav-item>
+            <b-nav-item class="navbar-link lvl2-link" href="#">Registry</b-nav-item>
+            <b-nav-item
+                class="navbar-link lvl2-link"
+                v-if="!user"
+                @click="loginPopover = !loginPopover"
+                ref="loginButton">Log in</b-nav-item>
+            <b-nav-item class="navbar-link lvl2-link" v-if="user">Actions</b-nav-item>
+            <b-nav-item class="navbar-link lvl2-link" v-if="user" @click="logout()">Log out</b-nav-item>
+          </b-navbar-nav>
+        </b-container>
       </b-collapse>
     </b-navbar>
+    <b-modal
+        v-model="loginPopover"
+        @shown="onLoginShown"
+        title="Log in"
+        hide-footer>
+        <b-form-group
+            label="Username"
+            label-for="loginPopUsername"
+            :state="loginUsernameState" class="mb-3"
+            invalid-feedback="This field is required">
+          <b-form-input
+              ref="input1"
+              id="loginPopUsername"
+              size="sm"
+              v-model="credentials.username"/>
+        </b-form-group>
+        <b-form-group
+            label="Password"
+            label-for="loginPopPassword"
+            :state="loginPasswordState" class="mb-3"
+            invalid-feedback="This field is required">
+          <b-form-input size="sm" id="loginPopPassword" v-model="credentials.password"/>
+        </b-form-group>
+        <b-btn @click="login" size="sm" variant="primary" class="mr-2">Log in</b-btn>
+        <b-btn @click="loginPopover = false" size="sm" variant="light">Cancel</b-btn>
+    </b-modal>
   </header>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import { LOGIN, LOGOUT } from '@/registry/store/actions.types'
 export default {
-
+  data () {
+    return {
+      loginPopover: false,
+      loginUsernameState: null,
+      loginPasswordState: null,
+      credentials: {
+        username: null,
+        password: null
+      }
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'user'
+    ])
+  },
+  watch: {
+    'credentials.username' (val) {
+      if (val) {
+        this.loginUsernameState = true
+      }
+    },
+    'credentials.password' (val) {
+      if (val) {
+        this.loginPasswordState = true
+      }
+    }
+  },
+  methods: {
+    async login (evt) {
+      evt.preventDefault()
+      if (!this.credentials.username) { this.loginUsernameState = false }
+      if (!this.credentials.password) { this.loginPasswordState = false }
+      if (this.credentials.username && this.credentials.password) {
+        await this.$store.dispatch(LOGIN, this.credentials)
+        this.loginPopover = false
+      }
+    },
+    logout () {
+      this.$store.dispatch(LOGOUT)
+    },
+    onLoginShown () {
+      /* Transfer focus to the first input */
+      this.credentials.username = ''
+      this.credentials.password = ''
+      this.loginUsernameState = null
+      this.loginPasswordState = null
+      this.$refs.input1.focus()
+    }
+  }
 }
 </script>
 
@@ -57,14 +150,13 @@ export default {
   border-bottom: 0px;
   padding-top: 0px;
   padding-bottom: 0px;
-  margin-bottom: 20px;
   -webkit-box-shadow: 0px 3px 3px 1px rgba(51, 51, 51, 0.5);
   -moz-box-shadow: 0px 3px 3px 1px rgba(51, 51, 51, 0.5);
   box-shadow: 0px 3px 3px 1px rgba(51, 51, 51, 0.5);
 }
 header li + li {
   @include media-breakpoint-up(sm) {
-    border-left: 1px solid #fff;
+    border-left: 1px solid #607D8B;
   }
 }
 .lvl2-link a {
