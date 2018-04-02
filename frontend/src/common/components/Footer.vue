34ccbae9513c8ca891a7e50fe2b60f5f02fb4a43
Steve Hillier <stephenhillier@gmail.com>
2018-04-01 22:38:56 -0700

    convert to bootstrap-vue components

--- frontend/src/common/components/Footer.vue
+++ frontend/src/common/components/Footer.vue
@@ -1,27 +1,22 @@
 <template>
-  <footer class="footer" style="height: auto">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <footer class="footer">
+    <b-navbar type="dark" variant="primary" toggleable="sm">
+      <b-container>
       <!-- Navbar content -->
-      <div class="container">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="https://www2.gov.bc.ca/gov/content/home/disclaimer" target="_blank">Disclaimer</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www2.gov.bc.ca/gov/content/home/privacy" target="_blank">Privacy</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www2.gov.bc.ca/gov/content/home/accessibility" target="_blank">Accessibility</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www2.gov.bc.ca/gov/content/home/copyright" target="_blank">Copyright</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www2.gov.bc.ca/gov/content/home/contact-us" target="_blank">Contact Us</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <b-navbar-nav>
+            <b-nav-item href="https://www2.gov.bc.ca/gov/content/home/disclaimer">Disclaimer
+            </b-nav-item>
+            <b-nav-item href="https://www2.gov.bc.ca/gov/content/home/privacy">Privacy
+            </b-nav-item>
+            <b-nav-item href="https://www2.gov.bc.ca/gov/content/home/accessibility">Accessibility
+            </b-nav-item>
+            <b-nav-item href="https://www2.gov.bc.ca/gov/content/home/copyright">Copyright
+            </b-nav-item>
+            <b-nav-item href="https://www2.gov.bc.ca/gov/content/home/contact-us">Contact Us
+            </b-nav-item>
+        </b-navbar-nav>
+      </b-container>
+    </b-navbar>
   </footer>
 </template>
 
@@ -31,7 +26,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '~bootstrap/scss/_functions';
+@import '~bootstrap/scss/_variables';
+@import '~bootstrap/scss/mixins/_breakpoints';
+.footer {
+  @include media-breakpoint-down(xs) {
+    height: 8.2rem;
+  }
+  height: 45px;
+  position: absolute;
+  bottom: 0
+}
 .footer .nav-item {
   font-size: 13px;
 }
@@ -40,5 +46,13 @@ export default {
 }
 .footer nav {
   min-height: 45px;
+}
+.footer a {
+  color: #fff!important;
+}
+footer li+li  {
+  @include media-breakpoint-up(sm) {
+    border-left: 1px solid #4b5e73;
+  }
 }
 </style>
