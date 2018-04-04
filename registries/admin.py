dc9b3ca91c64a57387324c6d14132338ca373921
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2018-04-04 16:15:25 -0700

    Clean makemigrations/migrate/dataload-static

--- registries/admin.py
+++ registries/admin.py
@@ -1,34 +1,19 @@
 from django.contrib import admin
 from .models import (
-    Person,
-    Organization,
-    ContactInfo,
     ActivityCode,
-    SubactivityCode,
-    QualificationCode,
-    RegistriesApplication,
-    ClassificationAppliedFor,
-    RegistriesStatusCode,
-    RegistriesRemovalReason,
-    Register,
     ApplicationStatusCode,
-    RegistriesApplicationStatus
+    ContactInfo,
+    Organization,
+    Person,
+    Qualification,
+    Register,
+    RegistriesApplication,
+    RegistriesApplicationStatus,
+    RegistriesRemovalReason,
+    RegistriesStatusCode,
+    SubactivityCode,
+    WellClassCode,
 )
 from gwells.models.ProvinceStateCode import ProvinceStateCode
 
 # Register your models here.
-
-admin.site.register(Person)
-admin.site.register(Organization)
-admin.site.register(Register)
-admin.site.register(RegistriesApplication)
-admin.site.register(ClassificationAppliedFor)
-admin.site.register(RegistriesApplicationStatus)
-admin.site.register(ContactInfo)
-admin.site.register(ActivityCode)
-admin.site.register(SubactivityCode)
-admin.site.register(QualificationCode)
-admin.site.register(RegistriesStatusCode)
-admin.site.register(RegistriesRemovalReason)
-admin.site.register(ApplicationStatusCode)
-admin.site.register(ProvinceStateCode)
