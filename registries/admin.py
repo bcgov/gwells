3d57e75fc327f1418ca649420e64a339b35139e2
Steve Hillier <stephenhillier@gmail.com>
2018-04-05 14:17:46 -0700

    register new models

--- registries/admin.py
+++ registries/admin.py
@@ -1,19 +1,37 @@
 from django.contrib import admin
 from .models import (
-    ActivityCode,
-    ApplicationStatusCode,
+    Person,
     ContactInfo,
     Organization,
-    Person,
-    Qualification,
-    Register,
-    RegistriesApplication,
-    RegistriesApplicationStatus,
-    RegistriesRemovalReason,
-    RegistriesStatusCode,
+    ActivityCode,
+    AccreditedCertificateCode,
+    CertifyingAuthorityCode,
     SubactivityCode,
+    RegistriesApplication,
+    RegistriesStatusCode,
+    RegistriesRemovalReason,
+    Register,
+    ApplicationStatusCode,
+    RegistriesApplicationStatus,
     WellClassCode,
+    Qualification
+
 )
-from gwells.models.ProvinceStateCode import ProvinceStateCode
 
 # Register your models here.
+
+admin.site.register(AccreditedCertificateCode)
+admin.site.register(CertifyingAuthorityCode)
+admin.site.register(ContactInfo)
+admin.site.register(Person)
+admin.site.register(Organization)
+admin.site.register(ActivityCode)
+admin.site.register(SubactivityCode)
+admin.site.register(RegistriesApplication)
+admin.site.register(RegistriesStatusCode)
+admin.site.register(RegistriesRemovalReason)
+admin.site.register(Register)
+admin.site.register(ApplicationStatusCode)
+admin.site.register(RegistriesApplicationStatus)
+admin.site.register(WellClassCode)
+admin.site.register(Qualification)
