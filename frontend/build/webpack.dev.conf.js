d5558023b89cd755c78ab03245219b30cbef2259
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-04-09 09:00:47 -0700

    Added in BundleTracker

--- frontend/build/webpack.dev.conf.js
+++ frontend/build/webpack.dev.conf.js
@@ -9,6 +9,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+const BundleTracker = require('webpack-bundle-tracker')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -64,8 +65,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         to: config.dev.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
-    // new BundleTracker({})
+    ]),
+    new BundleTracker({filename: '../webpack-stats.json'})
   ]
 })
 
