d0fff956937f44d31a4c5d154a88e8fa03ee765f
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2018-04-09 16:52:44 -0700

    Added applications to data load and added new DB views.

--- database/scripts/registries/post-deploy.sql
+++ database/scripts/registries/post-deploy.sql
@@ -254,91 +254,36 @@ AND   reg.registries_status_code = 'ACTIVE'
 ;
 
 
-
-
-
---CREATE OR REPLACE VIEW registries_well_driller_register AS
---SELECT 
--- reg.registration_no
---,reg.registration_date
---,status.code   as status_code
---,reg.register_removal_date
---,removal.code  as register_removal_reason
---,act.code as activity_code
---,appl.file_no
---,appl.over19_ind
---,appl.registrar_notes 
---,appl.reason_denied 
---,per.first_name
---,per.surname
---,contact.contact_tel
---,contact.contact_email
---,org.name           AS org_name
---,org.street_address
---,org.city
---,org.province_state_code AS prov_state_code 
---,org.postal_code           
---,org.main_tel              
---,org.fax_tel               
---,org.website_url           
+-- Accredited Certificates 
+DROP VIEW IF EXISTS vw_accredited_certficate;
+CREATE OR REPLACE VIEW vw_accredited_certficate AS
+SELECT
+ auth.cert_auth_code AS certifying_auth_code
+,auth.description    AS certifying_auth_name   
+,cert.name                    AS  name                    
+,cert.description             AS  description             
 ---- Remainder are PK, FK, Audit columns (usually hidden from user)
---,reg.register_guid            
---,reg.create_user AS reg_create_user                 
---,reg.create_date AS reg_create_date
---,reg.update_user AS reg_update_user
---,reg.update_date AS reg_update_date
---,status.registries_status_guid
---,status.create_user AS status_create_user                 
---,status.create_date AS status_create_date
---,status.update_user AS status_update_user
---,status.update_date AS status_update_date
---,appl.application_guid
---,appl.create_user AS appl_create_user
---,appl.create_date AS appl_create_date
---,appl.update_user AS appl_update_user
---,appl.update_date AS appl_update_date
---,removal.registries_removal_reason_guid
---,removal.create_user AS removal_create_user
---,removal.create_date AS removal_create_date
---,removal.update_user AS removal_update_user
---,removal.update_date AS removal_update_date
---,act.registries_activity_guid 
---,act.create_user AS act_create_user
---,act.create_date AS act_create_date
---,act.update_user AS act_update_user
---,act.update_date AS act_update_date
---,per.person_guid
---,per.create_user AS per_create_user
---,per.create_date AS per_create_date
---,per.update_user AS per_update_user
---,per.update_date AS per_update_date
---,contact.contact_at_guid 
---,contact.effective_date AS contact_effective_date
---,contact.expired_date   AS contact_expired_date
---,contact.create_user AS contact_create_user
---,contact.create_date AS contact_create_date
-----,contact.update_user AS contact_update_user
-----,contact.update_date AS contact_update_date
-----,org.org_guid  
-----,org.create_user AS org_create_user          
-----,org.create_date AS org_create_date          
-----,org.update_user AS org_update_user          
-----,org.update_date AS org_update_date 
-----FROM registries_register reg
-----INNER JOIN registries_application appl
-----  ON appl.application_guid = reg.application_guid
-----INNER JOIN registries_status_code status
-----  ON reg.registries_status_guid = status.registries_status_guid
-----INNER JOIN registries_activity_code act 
-----  ON reg.registries_activity_guid = act.registries_activity_guid
-----LEFT JOIN registries_person per
---  ON per.person_guid = appl.person_guid
---INNER JOIN registries_contact_detail contact
---  ON per.person_guid = contact.person_guid
---INNER JOIN registries_organization org
---  ON org.org_guid = contact.org_guid
---LEFT JOIN registries_removal_reason_code removal
---  ON removal.registries_removal_reason_guid = reg.registries_removal_reason_guid
---WHERE act.code = 'DRILL';       
---
---COMMENT ON  VIEW registries_driller_register  IS 'Placeholder view comment.';
+,auth.create_user    AS auth_create_user   
+,auth.create_date    AS auth_create_date   
+,auth.update_user    AS auth_update_user   
+,auth.update_date    AS auth_update_date   
+,auth.effective_date AS auth_effective_date
+,auth.expired_date   AS auth_expired_date  
+,cert.create_user             AS  cert_create_user             
+,cert.create_date             AS  cert_create_date             
+,cert.update_user             AS  cert_update_user             
+,cert.update_date             AS  cert_update_date             
+,cert.acc_cert_guid           AS  cert_acc_cert_guid           
+,cert.effective_date          AS  cert_effective_date          
+,cert.expired_date            AS  cert_expired_date            
+,cert.cert_auth_code          AS  cert_cert_auth_code          
+,cert.registries_activity_code AS cert_registries_activity_code
+FROM registries_accredited_certificate_code cert
+INNER JOIN registries_certifying_authority_code auth
+ON cert.cert_auth_code = auth.cert_auth_code;
+
+
+
+
+
+
