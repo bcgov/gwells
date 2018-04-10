8a12e96f405f8e5bf16fd18aad1f439222e2e0a6
Steve Hillier <stephenhillier@gmail.com>
2018-04-10 16:15:39 -0700

    fix audience

--- gwells/settings.py
+++ gwells/settings.py
@@ -208,12 +208,11 @@ LOGGING = {
 }
 
 JWT_AUTH = {
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
     'JWT_PUBLIC_KEY': """-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjpPznS8NO5XNl395Xa/wJyhhMDMJUk8s2wrG/FQ9gZnRaCbm9YFYynZzeehkpTNbb+SsLBnh0Me5DKTSlt0Gm03ULXXW6FZzL3SCE1wTx6Trm+zQ1mx07aGDbv34OtK0HitToajZrnTsGQ0TloVbQladBM74S2K0ooveV7p2qIydFjtR+DTJGiOxSLvts+qsGn/Wr2l939SRpQa/10vpYJgCLsd6Bv/0v23DpmR8WbVkLh8e3rtI0XgsJ0ZFXR80DPt3fXX3gdrNdPRB+hpOR8IZMEUzhqGRg5VXP8Lp+bbaemFanTwlFD3aUfDlOcPekxYqQeEmS6ahA/6vCpjuGwIDAQAB
 -----END PUBLIC KEY-----""",
     'JWT_ALGORITHM': 'RS256',
-    'JWT_AUDIENCE': 'mozilla-django-oidc'
+    'JWT_AUDIENCE': 'webapp-dev-local'
 }
 
 DRF_RENDERERS = ['rest_framework.renderers.JSONRenderer',]
