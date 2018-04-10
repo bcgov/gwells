8a9f4b4313049cfc530107cfb15bc0b9dff51603
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-04-10 11:42:05 -0700

    Fixed asset path for dev

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/gwells/static/registries/',
     proxyTable: {},
 
     // Various Dev Server settings
