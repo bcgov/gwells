9eaec005f6ef448d325a3db0b2f7dcc320a6b0ad
Steve Hillier <stephenhillier@gmail.com>
2018-04-11 13:55:21 -0700

    moving keycloak init code out of components

--- frontend/src/common/components/Auth.vue
+++ frontend/src/common/components/Auth.vue
@@ -12,52 +12,32 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import Keycloak from 'keycloak-js'
 import ApiService from '@/common/services/ApiService.js'
-import { SET_KEYCLOAK } from '@/registry/store/mutations.types.js'
 
 export default {
   data () {
     return {
-      kc: null
     }
   },
   computed: {
-    ...mapGetters(['user', 'keycloak'])
+    ...mapGetters(['keycloak'])
   },
   methods: {
-    keyCloakInit () {
-      const kc = Keycloak({
-        'realm': 'gwells',
-        'url': 'https://dev-sso.pathfinder.gov.bc.ca/auth',
-        'clientId': 'webapp-dev-local'
-      })
-
-      kc.init({onLoad: 'check-sso'}).success(() => {
-        this.kc = kc
-        this.$store.commit(SET_KEYCLOAK, kc)
-        ApiService.authHeader('JWT', kc.token)
-      })
-    },
     keyCloakLogin () {
-      if (this.kc) {
-        this.kc.login({ idpHint: 'idir' }).success((authenticated) => {
+      if (this.keycloak) {
+        this.keycloak.login({ idpHint: 'idir' }).success((authenticated) => {
           if (authenticated) {
-            this.$store.commit(SET_KEYCLOAK, this.kc)
-            ApiService.authHeader('JWT', this.kc.token)
+            ApiService.authHeader('JWT', this.keycloak.token)
           }
         })
       }
     },
     keyCloakLogout () {
-      if (this.kc && this.kc.authenticated) {
-        this.kc.logout()
+      if (this.keycloak && this.keycloak.authenticated) {
+        this.keycloak.logout()
         this.$router.push({ name: 'SearchHome' })
       }
     }
-  },
-  created () {
-    this.keyCloakInit()
   }
 }
 </script>
