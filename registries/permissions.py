0f67f3ae001b4451fd75bc60928b646924088003
Steve Hillier <stephenhillier@gmail.com>
2018-04-12 09:55:05 -0700

    allow django admins permission in the IsGwellsAdmin permission class

--- registries/permissions.py
+++ registries/permissions.py
@@ -1,4 +1,9 @@
+"""
+Registries view permission classes
+"""
+
 from rest_framework.permissions import BasePermission, IsAdminUser, SAFE_METHODS
+
 
 class IsAdminOrReadOnly(IsAdminUser):
     """
@@ -16,4 +21,17 @@ class IsGwellsAdmin(BasePermission):
     """
 
     def has_permission(self, request, view):
-        return request.user and request.user.profile and request.user.profile.is_gwells_admin
+        # user is a gwells admin (comes from Keycloak role)
+        is_gwells_admin = (
+            request.user and
+            request.user.is_authenticated and
+            request.user.profile and
+            request.user.profile.is_gwells_admin)
+
+        # user is an admin (comes from Django's default user model is_staff)
+        is_system_admin = (
+            request.user and
+            request.user.is_authenticated and
+            request.user.is_staff)
+
+        return is_gwells_admin or is_system_admin
