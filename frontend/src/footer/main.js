ed667259b06c07be919ccc7dfdb1543cc1005b8c
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-04-16 15:04:09 -0700

    Back to top button now working as used to

---
+++ frontend/src/footer/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import BootstrapVue from 'bootstrap-vue'
+import Footer from '../common/components/Footer'
+import '@/common/assets/css/bootstrap-theme.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(Vuex)
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#footer',
+  components: {
+    Footer
+  },
+  template: '<Footer/>'
+})
+
+Vue.config.devtools = true
