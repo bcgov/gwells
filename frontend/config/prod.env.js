9d941ef155a188697df7dba09ff1903e52ff2cac
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-04-17 16:51:48 -0700

    Router base now feeds off configured value

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: JSON.stringify('production'),
+  ENABLE_DATA_ENTRY: JSON.stringify(false),
+  APPLICATION_ROOT: JSON.stringify('/gwells/registries')
 }
