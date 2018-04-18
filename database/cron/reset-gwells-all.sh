0c7719d2714ab6fcc41dc1d54a0057bed26a9710
Derek Roberts <derek.roberts@gmail.com>
2018-04-18 12:52:46 -0700

    reset-gwells-all.sh + comments, cleanup

--- database/cron/reset-gwells-all.sh
+++ database/cron/reset-gwells-all.sh
@@ -59,9 +59,14 @@ then
 fi
 
 
-oc project ${PROJECT}
-podname=$(oc get pods -n ${PROJECT} | grep postgresql-[0-9] | grep Running | head -n 1 | awk '{print $1}')
-oc exec ${podname} -n ${PROJECT} -- /bin/bash -c 'export PGPASSWORD=$POSTGRESQL_PASSWORD;psql -h $POSTGRESQL_SERVICE_HOST -d $POSTGRESQL_DATABASE -U $POSTGRESQL_USER  << EOF
+# Identify running GWells database pod under selected project
+#
+PODNAME=$(oc get pods -n ${PROJECT} | grep postgresql-[0-9] | grep Running | head -n 1 | awk '{print $1}')
+
+
+# Drop tables
+#
+oc exec ${PODNAME} -n ${PROJECT} -- /bin/bash -c 'export PGPASSWORD=$POSTGRESQL_PASSWORD;psql -h $POSTGRESQL_SERVICE_HOST -d $POSTGRESQL_DATABASE -U $POSTGRESQL_USER  << EOF
 drop table if exists activity_submission                             cascade;
 drop table if exists activity_submission_water_quality               cascade;
 drop table if exists aquifer_well                                    cascade;
