361386a79fc32f7d93781953953f9000edb824fa
Derek Roberts <derek.roberts@gmail.com>
2018-04-18 11:30:30 -0700

    setup-xfer.sh + use project parameters for copy and sql

--- database/xfer-registries-to-openshift/setup-xfer.sh
+++ database/xfer-registries-to-openshift/setup-xfer.sh
@@ -37,7 +37,7 @@ fi
 
 # Check login
 #
-if( ! oc whoami )
+if ( ! oc whoami )
 then
     echo
     echo "Please obtain an OpenShift API token.  A window will open shortly."
@@ -47,7 +47,7 @@ then
 fi
 
 
-# Check project
+# Check project availability
 #
 CHECK=$( oc projects | tr -d '*' | grep -v "Using project" | grep "${PROJECT}" | awk '{ print $1 }' || echo )
 if [ "${PROJECT}" != "${CHECK}" ]
@@ -59,24 +59,20 @@ then
 fi
 
 
-oc project moe-gwells-dev
-podname=$(oc get pods -n moe-gwells-dev | grep gwells-[0-9] | grep Running | head -n 1 | awk '{print $1}')
-oc rsync /Users/garywong/projects/registry-gwells-export/2018-APR-16.sanitized  $podname:/tmp
-oc exec ${podname} -n moe-gwells-dev -- /bin/bash -c 'cp --remove-destination /tmp/2018-APR-16.sanitized/*.sanitized.csv  /opt/app-root/src/database/code-tables/registries/'
-oc exec ${podname} -n moe-gwells-dev -- /bin/bash -c 'export PGPASSWORD=$DATABASE_PASSWORD;cd /opt/app-root/src/database/code-tables/registries/;psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER  << EOF
-\ir ../../scripts/registries/post-deploy.sql
-\i clear-tables.sql
-\ir ../../scripts/registries/initialize-xforms-registries.sql
-\i data-load-static-codes.sql
-\ir ../../scripts/registries/populate-registries-from-xform.sql
-EOF
-'
-# TEST
-oc project moe-gwells-test
-podname=$(oc get pods -n moe-gwells-test | grep gwells-[0-9] | grep Running | head -n 1 | awk '{print $1}')
-oc rsync /Users/garywong/projects/registry-gwells-export/2018-APR-16.sanitized  $podname:/tmp
-oc exec ${podname} -n moe-gwells-test -- /bin/bash -c 'cp --remove-destination /tmp/2018-APR-16.sanitized/*.sanitized.csv  /opt/app-root/src/database/code-tables/registries/'
-oc exec ${podname} -n moe-gwells-test -- /bin/bash -c 'export PGPASSWORD=$DATABASE_PASSWORD;cd /opt/app-root/src/database/code-tables/registries/;psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER  << EOF
+# Identify running GWells pod under selected project
+#
+PODNAME=$( oc get pods -n ${PROJECT} | grep gwells-[0-9] | grep Running | head -n 1 | awk '{print $1}' )
+
+
+# Use oc rsync to copy into the pod and move to the correct dir
+#
+oc rsync /Users/garywong/projects/registry-gwells-export/2018-APR-16.sanitized  $PODNAME:/tmp
+oc exec ${PODNAME} -n ${PROJECT} -- /bin/bash -c 'cp --remove-destination /tmp/2018-APR-16.sanitized/*.sanitized.csv  /opt/app-root/src/database/code-tables/registries/'
+
+
+# Run post-deploy and other scripts
+#
+oc exec ${PODNAME} -n ${PROJECT} -- /bin/bash -c 'export PGPASSWORD=$DATABASE_PASSWORD;cd /opt/app-root/src/database/code-tables/registries/;psql -h $DATABASE_SERVICE_NAME -d $DATABASE_NAME -U $DATABASE_USER  << EOF
 \ir ../../scripts/registries/post-deploy.sql
 \i clear-tables.sql
 \ir ../../scripts/registries/initialize-xforms-registries.sql
