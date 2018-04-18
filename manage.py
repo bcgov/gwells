2fd08e4d1078f5156fbc2f5be7c72d52ed7e42b9
Steve Hillier <stephenhillier@gmail.com>
2018-04-17 17:45:41 -0700

    add nose args when manage.py test called

--- manage.py
+++ manage.py
@@ -7,4 +7,18 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
 
+    is_testing = 'test' in sys.argv
+
+    if is_testing:
+        import coverage
+        cov = coverage.coverage(source=['gwells', 'registries'], omit=['*/tests/*', '*/*tests.py'])
+        cov.set_option('report:show_missing', True)
+        cov.erase()
+        cov.start()
+
     execute_from_command_line(sys.argv)
+
+    if is_testing:
+        cov.stop()
+        cov.save()
+        cov.report()
