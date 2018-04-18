cb31a583033b85b36a86aaf8ee3cc1d98ff89cd1
Derek Roberts <derek.roberts@gmail.com>
2018-04-18 10:48:06 -0700

    setup-xfer.sh + verify oc login

--- database/xfer-registries-to-openshift/setup-xfer.sh
+++ database/xfer-registries-to-openshift/setup-xfer.sh
@@ -35,6 +35,18 @@ then
 fi
 
 
+# Check login
+#
+if( ! oc whoami )
+then
+    echo
+    echo "Please obtain an OpenShift API token.  A window will open shortly."
+    sleep 3
+    open https://console.pathfinder.gov.bc.ca:8443/oauth/token/request
+    exit
+fi
+
+
 # Check project
 #
 CHECK=$( oc projects | tr -d '*' | grep -v "Using project" | grep "${PROJECT}" | awk '{ print $1 }' || echo )
