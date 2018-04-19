6cc1e57ed0442e1492bfbec1ded2532f7fa7a9a6
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-04-19 11:32:13 -0700

    Cleaning up

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -2,8 +2,6 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-console.log(process.env.APPLICATION_ROOT)
-
 module.exports = merge(prodEnv, {
   NODE_ENV: JSON.stringify('development'),
   APPLICATION_ROOT: process.env.APPLICATION_ROOT ? JSON.stringify(process.env.APPLICATION_ROOT) : JSON.stringify('/gwells/registries'),
