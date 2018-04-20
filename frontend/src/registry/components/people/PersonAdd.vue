d836634dfdba87fc719cacf14104d17e76f3a1f4
Steve Hillier <stephenhillier@gmail.com>
2018-04-19 17:52:39 -0700

    improve person add screen

--- frontend/src/registry/components/people/PersonAdd.vue
+++ frontend/src/registry/components/people/PersonAdd.vue
@@ -66,16 +66,25 @@
             </b-row>
             <b-row class="mt-3">
               <b-col md="5">
-                <!-- <b-form-group
+                <b-form-group
                   id="companyInputGroup"
                   label="Company:"
                   label-for="companyInput">
                   <v-select
                     v-model="drillerForm.person.organization"
                     :options="companies"
-                    placeholder="Begin typing a company name"/>
-                </b-form-group> -->
-                <b-button type="button" href="#" variant="light" size="sm" class="mb-3"><i class="fa fa-plus-square-o"></i> Add a company</b-button>
+                    placeholder="Begin typing a company name"
+                    label="name"
+                    />
+                </b-form-group>
+                <b-button
+                  type="button"
+                  v-b-modal.orgModal
+                  variant="light"
+                  size="sm"
+                  class="mb-3">
+                  <i class="fa fa-plus-square-o"></i> Add a company</b-button>
+                  <b-modal id="orgModal" title="Add an Organization"><organization-add></organization-add></b-modal>
               </b-col>
             </b-row>
             <b-row class="mt-3">
@@ -124,6 +133,28 @@
                 <b-button type="reset" variant="light">Reset</b-button>
               </b-col>
             </b-row>
+            <b-row class="mt-3">
+              <b-col>
+                <b-alert :show="submitSuccess"
+                        dismissible
+                        variant="success"
+                        @dismissed="submitSuccess=false">
+                  Successfully created a new person!
+                </b-alert>
+                <b-alert :show="!!submitError"
+                        dismissible
+                        variant="warning"
+                        @dismissed="submitError=false">
+                  Error creating a new person.
+                  <div v-for="(value, key, index) in submitError.data" :key="`submit error ${index}`">
+                    <span class="text-capitalize">{{ key }}</span>:
+                    <span
+                      v-for="(msg, msgIndex) in value"
+                      :key="`submit error msg ${index} ${msgIndex}`">{{ msg }} </span>
+                  </div>
+                </b-alert>
+              </b-col>
+            </b-row>
           </b-form>
       </div>
     </div>
@@ -134,11 +165,13 @@
 import APIErrorMessage from '@/common/components/APIErrorMessage'
 import { mapGetters } from 'vuex'
 import ApiService from '@/common/services/ApiService.js'
+import OrganizationAdd from '@/registry/components/people/OrganizationAdd.vue'
 
 export default {
   name: 'PersonDetailEdit',
   components: {
-    'api-error': APIErrorMessage
+    'api-error': APIErrorMessage,
+    'organization-add': OrganizationAdd
   },
   data () {
     return {
@@ -155,7 +188,8 @@ export default {
       drillerForm: {
         person: {
           surname: '',
-          first_name: ''
+          first_name: '',
+          organization: ''
         },
         regType: [],
         contact_info: {
@@ -176,9 +210,11 @@ export default {
         }
       },
       companies: [
-        { value: '123', label: 'Big Time Drilling Co.' },
-        { value: '124', label: 'Steve\'s Drilling Inc.' }
-      ]
+        { org_guid: '123', name: 'Big Time Drilling Co.' },
+        { org_guid: '124', name: 'Steve\'s Drilling Inc.' }
+      ],
+      submitSuccess: false,
+      submitError: false
     }
   },
   computed: {
@@ -192,6 +228,20 @@ export default {
       const registrations = []
       const contactInfo = []
       const personData = Object.assign({}, this.drillerForm.person)
+      this.submitSuccess = false
+      this.submitError = false
+
+      // strip empty strings
+      Object.keys(personData).forEach((key) => {
+        if (personData[key] === '' || personData[key] === null) {
+          delete personData[key]
+        }
+      })
+
+      // Set organization property to the GUID of the organization selected in the form
+      if (personData.organization) {
+        personData.organization = personData.organization.org_guid
+      }
 
       // add registration data for activities checked off on form
       this.drillerForm.regType.forEach((item) => {
@@ -204,13 +254,18 @@ export default {
       personData['registrations'] = registrations
       personData['contact_info'] = contactInfo
 
-      ApiService.post('drillers', personData)
+      ApiService.post('drillers', personData).then((response) => {
+        this.submitSuccess = true
+      }).catch((error) => {
+        this.submitError = error.response
+      })
     },
     onFormReset () {
       this.drillerForm = Object.assign({}, {
         person: {
           surname: '',
-          first_name: ''
+          first_name: '',
+          organization: ''
         },
         regType: [],
         contact_info: {
@@ -231,6 +286,13 @@ export default {
         }
       })
     }
+  },
+  created () {
+    ApiService.query('organizations/names/').then((response) => {
+      this.companies = response.data
+    }).catch(() => {
+      this.orgListError = 'Unable to retrieve organization list.'
+    })
   }
 }
 </script>
