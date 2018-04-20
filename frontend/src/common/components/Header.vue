7d3e9f9da6dd6d00d97cb085c5b9ae289978e37b
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-04-19 17:43:07 -0700

    Adding id to links, and changing url to match tests

--- frontend/src/common/components/Header.vue
+++ frontend/src/common/components/Header.vue
@@ -35,11 +35,11 @@
           <b-container id="navContainer">
             <b-navbar-nav>
               <b-nav-text class="d-sm-none text-light">Groundwater Wells and Aquifers</b-nav-text>
-              <b-nav-item class="navbar-link lvl2-link" href="/gwells/">Well Search</b-nav-item>
-              <b-nav-item class="navbar-link lvl2-link" href="/gwells/groundwater-information/">Groundwater Information</b-nav-item>
-              <b-nav-item class="navbar-link lvl2-link" href="/gwells/registries/">Registry</b-nav-item>
-              <b-nav-item class="navbar-link lvl2-link" v-if="show.dataEntry" href="/gwells/submission/create/">Submit Report</b-nav-item>
-              <b-nav-item id="ribbon-admin" class="navbar-link lvl2-link" v-if="show.admin" href="/gwells/admin/">Admin</b-nav-item>
+              <b-nav-item id="ribbon-search" class="navbar-link lvl2-link" href="/gwells">Well Search</b-nav-item>
+              <b-nav-item id="ribbon-groundwaterinfo" class="navbar-link lvl2-link" href="/gwells/groundwater-information">Groundwater Information</b-nav-item>
+              <b-nav-item class="navbar-link lvl2-link" href="/gwells/registries">Registry</b-nav-item>
+              <b-nav-item class="navbar-link lvl2-link" v-if="show.dataEntry" href="/gwells/submission/create">Submit Report</b-nav-item>
+              <b-nav-item id="ribbon-admin" class="navbar-link lvl2-link" v-if="show.admin" href="/gwells/admin">Admin</b-nav-item>
             </b-navbar-nav>
           </b-container>
         </b-collapse>
