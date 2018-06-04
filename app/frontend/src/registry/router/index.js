d2b86586451453b7512447761298a1e734cbf0a3
Steve Hillier <stephenhillier@gmail.com>
2018-06-04 14:59:55 -0700

    added scroll to top when changing pages

--- app/frontend/src/registry/router/index.js
+++ app/frontend/src/registry/router/index.js
@@ -79,5 +79,8 @@ export default new Router({
     }
   ],
   mode: 'history',
-  base: process.env.APPLICATION_ROOT
+  base: process.env.APPLICATION_ROOT,
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
