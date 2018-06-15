6bfd25209807ad5d3993c675cf1b0b0ba1a64d8a
Steve Hillier <stephenhillier@gmail.com>
2018-06-15 16:47:47 -0700

    fix test for person detail view

--- app/frontend/src/registry/components/people/PersonDetail.vue
+++ app/frontend/src/registry/components/people/PersonDetail.vue
@@ -371,6 +371,15 @@
 
         <!-- Notes -->
         <person-notes @updated="updateRecord"></person-notes>
+
+        <!-- Change history for this record -->
+        <change-history
+          ref="changeHistory"
+          class="my-3"
+          v-if="!!currentDriller"
+          resource="person"
+          :id="currentDriller.person_guid"></change-history>
+
       </div>
     </div>
   </div>
@@ -380,6 +389,7 @@
 import APIErrorMessage from '@/common/components/APIErrorMessage'
 import PersonEdit from '@/registry/components/people/PersonEdit.vue'
 import PersonNotes from '@/registry/components/people/PersonNotes.vue'
+import ChangeHistory from '@/registry/components/people/ChangeHistory.vue'
 import ApplicationAddEdit from '@/registry/components/people/ApplicationAddEdit.vue'
 import ApiService from '@/common/services/ApiService.js'
 import { mapGetters } from 'vuex'
@@ -392,6 +402,7 @@ export default {
     'api-error': APIErrorMessage,
     'person-edit': PersonEdit,
     'application-add': ApplicationAddEdit,
+    ChangeHistory,
     PersonNotes
   },
   data () {
@@ -543,6 +554,10 @@ export default {
     },
     updateRecord () {
       this.$store.dispatch(FETCH_DRILLER, this.$route.params.person_guid)
+      // update changeHistory when company is updated
+      if (this.currentDriller && this.$refs.changeHistory) {
+        this.$refs.changeHistory.update()
+      }
     },
     addApplication (registration) {
       const newClassification = {
