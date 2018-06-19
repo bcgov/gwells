0fe009639ace0e3f0576ade003fb7dc977a110f7
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-06-19 13:47:13 -0700

    Fixed vue unit test, fixed code to return correct subset of applications

--- app/frontend/src/registry/components/search/table-helpers/DrillerRegistrationStatus.vue
+++ app/frontend/src/registry/components/search/table-helpers/DrillerRegistrationStatus.vue
@@ -5,7 +5,7 @@
         :key="`reg status ${driller.person_guid} ${regIndex}`">
       <div v-for="(app, appIndex) in reg.applications" :key="`app ${driller.person_guid} ${regIndex} ${appIndex}`">
         <div v-if="!!app.removal_date">Removed</div>
-        <div v-else>{{reg.status}}</div>
+        <div v-else-if="!!app.current_status">{{app.current_status.description}}</div>
       </div>
     </div>
   </div>
