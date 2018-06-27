63ff33d97d05579191b22ab68fbf196352adba04
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-06-27 16:51:35 -0700

    Changed config

--- app/frontend/config/dev.env.js
+++ app/frontend/config/dev.env.js
@@ -6,5 +6,5 @@ module.exports = merge(prodEnv, {
   NODE_ENV: JSON.stringify('development'),
   APPLICATION_ROOT: process.env.APPLICATION_ROOT ? JSON.stringify(process.env.APPLICATION_ROOT) : JSON.stringify('/gwells/registries'),
   AXIOS_BASE_URL: process.env.AXIOS_BASE_URL ? JSON.stringify(process.env.AXIOS_BASE_URL) : JSON.stringify('/gwells/api/v1/'),
-  ENABLE_DATA_ENTRY: JSON.stringify(true)
+  ENABLE_DATA_ENTRY: process.env.ENABLE_DATA_ENTRY ? JSON.stringify(process.env.ENABLE_DATA_ENTRY) : JSON.stringify(false)
 })
--- app/frontend/config/prod.env.js
+++ app/frontend/config/prod.env.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   NODE_ENV: JSON.stringify('production'),
-  ENABLE_DATA_ENTRY: JSON.stringify(true),
+  ENABLE_DATA_ENTRY: JSON.stringify(false),
   APPLICATION_ROOT: JSON.stringify('/gwells/registries'),
   AXIOS_BASE_URL: JSON.stringify('/gwells/api/v1/')
 }
