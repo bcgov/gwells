82ba807b108ce9d5d912169e633912cac2409f2b
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-06-27 14:32:36 -0700

    Adding vue

---
+++ app/frontend/src/storage/main.js
@@ -0,0 +1,37 @@
+/*
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+import 'babel-polyfill'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { store } from '@/registry/store'
+import BootstrapVue from 'bootstrap-vue'
+import Storage from './Storage'
+import '@/common/assets/css/bootstrap-theme.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(Vuex)
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#storage',
+  store,
+  components: {
+    Storage
+  },
+  template: '<Storage/>'
+})
+
+Vue.config.devtools = true
