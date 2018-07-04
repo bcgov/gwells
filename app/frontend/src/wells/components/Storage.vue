c7937c05ae20d71135a4ee5a188706082ac13d5e
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-07-03 17:40:24 -0700

    Hooked up well tag number

--- app/frontend/src/wells/components/Storage.vue
+++ app/frontend/src/wells/components/Storage.vue
@@ -1,59 +1,45 @@
 <template>
-  <div class="row no-gutters">
-    <div class="col-md-12">
-      <div v-if="loading" class="fa-2x text-center">
-        <i class="fa fa-circle-o-notch fa-spin"></i>
-      </div>
-      <div v-else>
-        <!-- public documents -->
-        <table id="additional_documentation_table" class="table table-sm table-bordered table-striped">
-          <thead>
-              <th>Document Link</th>
-          </thead>
-          <tbody>
-            <tr v-if="error">
-              <td>{{error}}</td>
-            </tr>
-            <tr v-else-if="files && files.public && files.public.length" v-for="(file, index) in files.public" :key="index">
-              <td>
-                <a :href="file.url" target="_blank">{{file.name}}</a>
-              </td>
-            </tr>
-            <tr v-else>
-              <td>
-                <div class="row no-gutters hide-for-print show-for-screen">
-                  <div id="no_documentation_msg">No additional documentation currently available for this well.</div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <!-- private documents -->
-        <div v-if="userRoles.wellsView">
-          <table id="additional_documentation_table" class="table table-sm table-bordered table-striped">
-            <thead>
-                <th>Document Link (Internal documentation - authorized access only)</th>
-            </thead>
-            <tbody>
-              <tr v-if="error">
-                <td>{{error}}</td>
-              </tr>
-              <tr v-else-if="files && files.private && files.private.length" v-for="(file, index) in files.private" :key="index">
-                <td>
-                  <a :href="file.url" target="_blank">{{file.name}}</a>
-                </td>
-              </tr>
-              <tr v-else>
-                <td>
-                  <div class="row no-gutters hide-for-print show-for-screen">
-                    <div id="no_documentation_msg">No additional private documentation currently available for this well.</div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+  <div>
+    <div v-if="loading" class="row no-gutters">
+      <div class="col-md-12">
+        <div class="fa-2x text-center">
+          <i class="fa fa-circle-o-notch fa-spin"></i>
         </div>
       </div>
+    </div>
+    <div v-else>
+      <div class="row no-gutters mt-3">
+        <div class="col-md-12">
+          <!-- public documents -->
+          <div v-if="error">
+            {{error}}
+          </div>
+          <ul v-else-if="files && files.public && files.public.length">
+            <li v-for="(file, index) in files.public" :key="index">
+              <a :href="file.url" target="_blank">{{file.name}}</a>
+            </li>
+          </ul>
+          <div v-else>
+              No additional documentation currently available for this well.
+          </div>
+        </div>
+      </div>
+      <div class="row no-gutters" v-if="userRoles.wellsView">
+        <div class="col-md-12">
+          <h4>Internal documentation - authorized access only</h4>
+            <div v-if="error">
+              {{error}}
+            </div>
+            <ul v-else-if="files && files.private && files.private.length">
+              <li v-for="(file, index) in files.private" :key="index">
+                <a :href="file.url" target="_blank">{{file.name}}</a>
+              </li>
+            </ul>
+              <div v-else>
+                No additional private documentation currently available for this well.
+              </div>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -76,7 +62,8 @@ export default {
     ...mapGetters(['userRoles'])
   },
   created () {
-    ApiService.query('well/123/files').then((response) => {
+    let wellMeta = document.head.querySelector('meta[name="well.tag_number"]')
+    ApiService.query('wells/' + wellMeta.content + '/files').then((response) => {
       this.files = response.data
     }).catch((e) => {
       console.error(e)
