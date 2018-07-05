a42f7477058dfda484617b3608fd1f4d4e41246a
Steve Hillier <stephenhillier@gmail.com>
2018-07-05 13:37:39 -0700

    better error handling

--- app/wells/documents.py
+++ app/wells/documents.py
@@ -12,11 +12,14 @@
     limitations under the License.
 """
 import os
+import logging
 from datetime import timedelta
 from django.urls import reverse
 from urllib.parse import quote
 from minio import Minio
 from gwells.settings.base import get_env_variable
+
+logger = logging.getLogger(__name__)
 
 
 class MinioClient():
@@ -25,8 +28,8 @@ class MinioClient():
     Requires environment variables:
         S3_HOST: hostname for public document storage
         S3_ROOT_BUCKET: public document storage bucket
-        S3_PRIVATE_ACCESS_KEY: private storage account
-        S3_PRIVATE_SECRET_KEY: private storage secret
+        MINIO_ACCESS_KEY: private storage account
+        MINIO_SECRET_KEY: private storage secret
         S3_PRIVATE_HOST: private storage host (must be specified even if same as public storage)
         S3_PRIVATE_BUCKET: private storage bucket
 
@@ -43,10 +46,6 @@ class MinioClient():
     def __init__(self, request=None, disable_public=False, disable_private=False):
         self.request = request
 
-        # allow disabling public or private clients if not needed (e.g. if user not authorized anyway)
-        self.disable_private = disable_private
-        self.disable_public = disable_public
-
         if not disable_public:
             self.public_host = get_env_variable('S3_HOST', strict=True)
             self.public_bucket = get_env_variable(
@@ -55,6 +54,7 @@ class MinioClient():
                 'S3_PUBLIC_ACCESS_KEY', warn=False)
             self.public_secret_key = get_env_variable(
                 'S3_PUBLIC_SECRET_KEY', warn=False)
+
             self.public_client = Minio(
                 self.public_host,
                 access_key=self.public_access_key,
@@ -63,10 +63,11 @@ class MinioClient():
             )
 
         if not disable_private:
-            self.private_access_key = get_env_variable('S3_PRIVATE_ACCESS_KEY')
-            self.private_secret_key = get_env_variable('S3_PRIVATE_SECRET_KEY')
+            self.private_access_key = get_env_variable('MINIO_ACCESS_KEY')
+            self.private_secret_key = get_env_variable('MINIO_SECRET_KEY')
             self.private_host = get_env_variable('S3_PRIVATE_HOST')
             self.private_bucket = get_env_variable('S3_PRIVATE_BUCKET')
+
             self.private_client = Minio(
                 self.private_host,
                 access_key=self.private_access_key,
@@ -128,20 +129,34 @@ class MinioClient():
         prefix = str(str('{:0<6}'.format('{:0>2}'.format(well_tag_number//10000))) + '/WTN ' +
                      str(well_tag_number) + '_')
 
+        objects = {}
+
         # provide all requests with a "public" collection of documents
-        objects = {
-            "public": self.create_url_list(
-                self.public_client.list_objects(
-                    self.public_bucket, prefix=prefix, recursive=True),
-                self.public_host
-            )}
+        if self.public_client:
+            pub_objects = []
+            try:
+                pub_objects = self.create_url_list(
+                    self.public_client.list_objects(
+                        self.public_bucket, prefix=prefix, recursive=True),
+                    self.public_host)
+            except:
+                logger.error(
+                    "Could not retrieve files from public file server")
+
+            objects['public'] = pub_objects
 
         # authenticated requests also receive a "private" collection
-        if include_private and not self.disable_private:
-            priv_objects = self.create_url_list(
-                self.private_client.list_objects(
-                    self.private_bucket, prefix=prefix, recursive=True),
-                self.private_host, private=True)
+        if include_private and self.private_client:
+            priv_objects = []
+            try:
+                priv_objects = self.create_url_list(
+                    self.private_client.list_objects(
+                        self.private_bucket, prefix=prefix, recursive=True),
+                    self.private_host, private=True)
+            except:
+                logger.error(
+                    "Could not retrieve files from private file server")
+
             objects['private'] = priv_objects
 
         return objects
