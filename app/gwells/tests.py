2b2d02d21d07df2bc3b3741080b292f8204ba08f
Derek Roberts <derek.roberts@gmail.com>
2018-07-05 15:15:01 -0700

    Reverted secrests in backend.dc and removed unused manage commands

--- app/gwells/tests.py
+++ app/gwells/tests.py
@@ -15,30 +15,6 @@ from django.contrib.auth.models import User
 from gwells.models import Profile
 
 
-class CreateGroupsTest(TestCase):
-    """ tests manage.py creategroups command """
-
-    def test_groups_created_output(self):
-        out = StringIO()
-        call_command('creategroups', stdout=out)
-        self.assertIn('Group gwells_administrator created', out.getvalue())
-        self.assertIn(
-            'Group registries_statutory_authority created', out.getvalue())
-        self.assertIn('Group registries_adjudicator created', out.getvalue())
-        self.assertIn('Group registries_staff_viewer created', out.getvalue())
-
-    def test_groups_already_created(self):
-        out = StringIO()
-        call_command('creategroups', stdout=None)
-        # running a second time to simulate trying to create groups that already exist
-        call_command('creategroups', stdout=out)
-        self.assertIn('Group gwells_administrator exists', out.getvalue())
-        self.assertIn(
-            'Group registries_statutory_authority exists', out.getvalue())
-        self.assertIn('Group registries_adjudicator exists', out.getvalue())
-        self.assertIn('Group registries_staff_viewer exists', out.getvalue())
-
-
 class GwellsRoleGroupsTests(TestCase):
     """
     Tests GWELLS "SSO role"-based groups
