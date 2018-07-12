064dbb4fdda9214e7db495081ffa2bcbba1fa526
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-07-12 12:16:52 -0700

    Added comment for order reason

--- app/registries/views.py
+++ app/registries/views.py
@@ -343,6 +343,7 @@ class PersonListView(RevisionMixin, AuditCreateMixin, ListCreateAPIView):
                 current_status='A', removal_date__isnull=True)
 
         # generate applications queryset
+        # We need to order by something to get a consistent result.
         applications_qs = applications_qs \
             .select_related(
                 'current_status',
