aa6f746f21b11ddc381b42252c137e37d9fa67d7
Steve Hillier <stephenhillier@gmail.com>
2018-07-13 16:32:27 -0700

    simple form layout

---
+++ app/frontend/src/submissions/components/SubmissionForm/Step01Type.vue
@@ -0,0 +1,68 @@
+<template>
+    <fieldset>
+      <legend>Type of Work and Well Class</legend>
+      <b-row>
+        <b-col cols="12" md="6">
+          <b-form-group label="Type of Work*">
+            <b-form-radio-group v-model="formData.type_of_work"
+                                stacked
+                                name="submissionTypeRadio">
+              <b-form-radio value="CON">Construction</b-form-radio>
+              <b-form-radio value="ALT">Alteration</b-form-radio>
+              <b-form-radio value="DEC">Decommissioning</b-form-radio>
+            </b-form-radio-group>
+          </b-form-group>
+        </b-col>
+        <b-col cols="12" md="6">
+          <b-form-group label="Measurement units for data entry">
+            <b-form-radio-group v-model="units"
+                                stacked
+                                name="measurementUnitsRadio">
+              <b-form-radio value="metric">Metric</b-form-radio>
+              <b-form-radio value="imperial">Imperial</b-form-radio>
+            </b-form-radio-group>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="12" md="6">
+          <b-form-group label="Person Responsible for Drilling*">
+            <v-select></v-select>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="12" md="6">
+          <b-form-group label="Start Date of Work*">
+            <b-form-input type="date" v-model="formData.work_start_date"/>
+          </b-form-group>
+        </b-col>
+        <b-col cols="12" md="6">
+          <b-form-group label="End Date of Work*">
+            <b-form-input type="date" v-model="formData.work_end_date"/>
+          </b-form-group>
+        </b-col>
+      </b-row>
+    </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'Step01Type',
+  data () {
+    return {
+      units: 'metric',
+      // form data keys follow naming convention from API for consistency with request/response field names
+      formData: {
+        type_of_work: 'CON',
+        work_start_date: '',
+        work_end_date: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
