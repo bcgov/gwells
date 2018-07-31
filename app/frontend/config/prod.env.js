f6c5c20b06ce038ddacf3e2ed3c06238a647540b
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-07-30 18:21:08 -0700

    Change to load data entry from env

--- app/frontend/config/prod.env.js
+++ app/frontend/config/prod.env.js
@@ -4,7 +4,8 @@ module.exports = {
   NODE_ENV: JSON.stringify('production'),
   APPLICATION_ROOT: JSON.stringify('/gwells/registries'),
   AXIOS_BASE_URL: JSON.stringify('/gwells/api/v1/'),
-  ENABLE_DATA_ENTRY: JSON.stringify(false),
-  // Unless disabled, we always analytics in production.
+  // Unless explicitly enabled, we always disable data entry in production.
+  ENABLE_DATA_ENTRY: process.env.ENABLE_DATA_ENTRY ? JSON.stringify(process.env.ENABLE_DATA_ENTRY) : JSON.stringify(false),
+  // Unless explicitly disabled, we always enable analytics in production.
   ENABLE_GOOGLE_ANALYTICS: process.env.ENABLE_GOOGLE_ANALYTICS ? JSON.stringify(process.env.ENABLE_GOOGLE_ANALYTICS) : JSON.stringify(true)
 }
