89f22a710b3ab80c7ef81fd937a5103304cd2c1d
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-07-30 17:56:27 -0700

    By default, enable data entry on dev

--- app/frontend/config/dev.env.js
+++ app/frontend/config/dev.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: JSON.stringify('development'),
   APPLICATION_ROOT: process.env.APPLICATION_ROOT ? JSON.stringify(process.env.APPLICATION_ROOT) : JSON.stringify('/gwells/'),
   AXIOS_BASE_URL: process.env.AXIOS_BASE_URL ? JSON.stringify(process.env.AXIOS_BASE_URL) : JSON.stringify('/gwells/api/v1/'),
-  ENABLE_DATA_ENTRY: process.env.ENABLE_DATA_ENTRY ? JSON.stringify(process.env.ENABLE_DATA_ENTRY) : JSON.stringify(false),
+  ENABLE_DATA_ENTRY: process.env.ENABLE_DATA_ENTRY ? JSON.stringify(process.env.ENABLE_DATA_ENTRY) : JSON.stringify(true),
   // We never do analytics on dev!
   ENABLE_GOOGLE_ANALYTICS: JSON.stringify(false)
 })
