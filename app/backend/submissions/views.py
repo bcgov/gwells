776737620d7f257d126c8fa16aa010cb22952062
Derek Roberts <derek.roberts@gmail.com>
2018-09-05 13:01:48 -0700

    Added serializer to views.py

--- app/backend/submissions/views.py
+++ app/backend/submissions/views.py
@@ -69,7 +69,8 @@ from submissions.serializers import (
     GroundElevationMethodCodeSerializer,
     DrillingMethodCodeSerializer,
     YieldEstimationMethodCodeSerializer,
-    FilterPackMaterialCodeSerializer
+    FilterPackMaterialCodeSerializer,
+    FilterPackMaterialSizeCodeSerializer
 )
 
 
