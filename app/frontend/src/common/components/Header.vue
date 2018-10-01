1d7431287517e255761e46d391854ebef05caf4a
Ryan Barber <rfb@skyscraper.nu>
2018-10-01 11:53:16 -0700

    Updated aquifers link
    - hide the link based on data.show
    - correct id name

--- app/frontend/src/common/components/Header.vue
+++ app/frontend/src/common/components/Header.vue
@@ -41,7 +41,7 @@
             <b-nav-text class="d-sm-none text-light">Groundwater Wells and Aquifers</b-nav-text>
             <b-navbar-nav class="gwells-nav">
               <b-nav-item id="ribbon-search" class="navbar-link lvl2-link" href="/gwells">Well Search</b-nav-item>
-              <b-nav-item id="ribbon-registry" class="navbar-link lvl2-link" href="/gwells/aquifers">Aquifers</b-nav-item>
+              <b-nav-item id="ribbon-aquifers" v-if="show.aquifers" class="navbar-link lvl2-link" href="/gwells/aquifers">Aquifers</b-nav-item>
               <b-nav-item id="ribbon-groundwaterinfo" class="navbar-link lvl2-link" href="/gwells/groundwater-information">Groundwater Information</b-nav-item>
               <b-nav-item id="ribbon-registry" class="navbar-link lvl2-link" href="/gwells/registries">Registry</b-nav-item>
               <b-nav-item class="navbar-link lvl2-link" v-if="show.dataEntry" href="/gwells/submissions">Submit Report</b-nav-item>
@@ -72,7 +72,8 @@ export default {
       const adminMeta = document.head.querySelector('meta[name="show.admin"]')
       return {
         dataEntry: (!!this.config && this.config.enable_data_entry === true) && this.userRoles.submissions.edit === true,
-        admin: adminMeta ? adminMeta.content === 'true' : false
+        admin: adminMeta ? adminMeta.content === 'true' : false,
+        aquifers: false
       }
     },
     ...mapGetters(['userRoles', 'config'])
