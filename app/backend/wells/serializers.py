8662ceef8bbb2486f9661ff634002ff9daa9c191
Steve Hillier <stephenhillier@gmail.com>
2018-10-02 10:59:42 -0700

    fix up stacking of decommission reports

--- app/backend/wells/serializers.py
+++ app/backend/wells/serializers.py
@@ -74,14 +74,15 @@ class DecommissionDescriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = DecommissionDescription
         fields = (
-            'decommission_description_guid',
-            'activity_submission',
-            'well',
             'start',
             'end',
             'material',
             'observations',
         )
+        extra_kwargs = {
+            'start': {'required': True},
+            'end': {'required': True},
+        }
 
 
 class ScreenSerializer(serializers.ModelSerializer):
@@ -119,6 +120,7 @@ class WellStackerSerializer(AuditModelSerializer):
     casing_set = CasingSerializer(many=True)
     screen_set = ScreenSerializer(many=True)
     linerperforation_set = LinerPerforationSerializer(many=True)
+    decommission_description_set = DecommissionDescriptionSerializer(many=True)
 
     class Meta:
         model = Well
@@ -130,7 +132,7 @@ class WellStackerSerializer(AuditModelSerializer):
         # based on this update. Trying to match up individual records and updating them, dealing with
         # removed casing/screen/perforation records etc. etc. is not the responsibility of this section.
         # The composite section is responsible for that.
-        FOREIGN_KEYS = {'casing_set': Casing, 'screen_set': Screen, 'linerperforation_set': LinerPerforation}
+        FOREIGN_KEYS = {'casing_set': Casing, 'screen_set': Screen, 'linerperforation_set': LinerPerforation, 'decommission_description_set': DecommissionDescription}
         for key in FOREIGN_KEYS.keys():
             for record in getattr(instance, key).all():
                 record.delete()
