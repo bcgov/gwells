3168655bea5fbedfcd3d81a53252feb83fa2953e
Ryan Barber <rfb@skyscraper.nu>
2018-10-09 14:07:23 -0700

    quality_concert -> quality_concern

--- app/backend/aquifers/serializers.py
+++ app/backend/aquifers/serializers.py
@@ -23,6 +23,7 @@ class AquiferListSerializer(serializers.ModelSerializer):
     productivity_description = serializers.SlugRelatedField(source='productivity', read_only=True, slug_field='description')
     subtype_description = serializers.SlugRelatedField(source='subtype', read_only=True, slug_field='description')
     vulnerability_description = serializers.SlugRelatedField(source='vulnerability', read_only=True, slug_field='description')
+    quality_concern_description = serializers.SlugRelatedField(source='quality_concern', read_only=True, slug_field='description')
 
     class Meta:
         model = Aquifer
@@ -39,6 +40,8 @@ class AquiferListSerializer(serializers.ModelSerializer):
             'material',
             'productivity_description',
             'productivity',
+            'quality_concern_description',
+            'quality_concern',
             'subtype_description',
             'subtype',
             'vulnerability_description',
