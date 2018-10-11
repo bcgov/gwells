645235b82d374cea80cc11404e283a51f8d493a3
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-10-11 13:27:03 -0700

    Timeout tweaks

--- functional-tests/src/test/groovy/specs/traits/Utils.groovy
+++ functional-tests/src/test/groovy/specs/traits/Utils.groovy
@@ -32,4 +32,48 @@ trait Utils {
     resetBrowser()
     CachingDriverFactory.clearCacheAndQuitDriver()
   }
+
+  /**
+   * Throw an AssertionError with the given message.
+   *
+   * @param String exception message. (optional, default: '')
+   */
+  void fail(String message='') {
+    throw new AssertionError(message)
+  }
+
+  /**
+   * Asserts that the given selector is not present on the page.
+   *
+   * @param Navigator the geb selector to assert is not present. (required)
+   * @param int time in seconds to wait before assuming the selector is not present.
+   *   Necessary if the selector typically becomes present after a delay. (optional, default: 1)
+   * @return true if the selector is not found.
+   * @throws AssertionError if the selector is found.
+   */
+  Boolean expectNotPresent(Navigator selector, int wait=5) {
+    try {
+      waitFor(wait) { selector }
+      fail("Expected the [$selector] selector to not be present, but was found.");
+    } catch (WaitTimeoutException e) {
+      true
+    }
+  }
+
+  /**
+   * Asserts that the given selector is present on the page.
+   *
+   * @param Navigator the geb selector to assert is present. (required)
+   * @param int time in seconds to wait before agreeing the selector is not present.
+   *   Necessary if the selector typically becomes present after a delay. (optional, default: 1)
+   * @return true if the selector is found.
+   * @throws AssertionError if the selector is not found.
+   */
+  Boolean expectPresent(Navigator selector, int wait=5) {
+    try {
+      waitFor(wait) { selector }
+    } catch (WaitTimeoutException e) {
+      fail("Expected the [$selector] selector to be present, but was not found.");
+    }
+  }
 }
