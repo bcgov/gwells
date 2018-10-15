0c5eec17dd5992c1cca7b79fb132d164ffa5c6bf
Steve Hillier <stephenhillier@gmail.com>
2018-10-15 13:03:38 -0700

    better way to generate unique id for login button

--- app/frontend/src/common/components/Auth.vue
+++ app/frontend/src/common/components/Auth.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div v-if="!keycloak || (keycloak && !keycloak.authenticated)">
-      <button type="button" :id="`gwells-sso-login${fromMenu ? '-menu':''}`" class="btn btn-light btn-sm" @click="keyCloakLogin()">Log in</button>
+      <button type="button" :id="`${id}-login-button`" class="btn btn-light btn-sm" @click="keyCloakLogin()">Log in</button>
     </div>
     <div v-if="keycloak && keycloak.authenticated">
       <span class="userLoggedInText text-light">{{ keycloak.tokenParsed.name }}</span>
-      <button type="button" class="btn btn-light btn-sm" @click="keyCloakLogout()">Log out</button>
+      <button type="button" :id="`${id}-logout-button`" class="btn btn-light btn-sm" @click="keyCloakLogout()">Log out</button>
     </div>
   </div>
 </template>
@@ -17,9 +17,9 @@ import { SET_ERROR } from '@/registry/store/mutations.types.js'
 
 export default {
   props: {
-    fromMenu: {
-      type: Boolean,
-      default: false
+    id: {
+      type: String,
+      default: 'sso-auth'
     }
   },
   data () {
--- app/frontend/src/common/components/Header.vue
+++ app/frontend/src/common/components/Header.vue
@@ -46,7 +46,7 @@
               <b-nav-item id="ribbon-registry" class="navbar-link lvl2-link" href="/gwells/registries">Registry</b-nav-item>
               <b-nav-item class="navbar-link lvl2-link" v-if="show.dataEntry" href="/gwells/submissions">Submit Report</b-nav-item>
               <b-nav-item id="ribbon-admin" class="navbar-link lvl2-link" v-if="show.admin" href="/gwells/admin">Admin</b-nav-item>
-              <b-nav-item class="d-sm-none"><keycloak-auth v-if="auth !== 'hide'" id="keycloak-auth-xs" :from-menu="true"/></b-nav-item>
+              <b-nav-item class="d-sm-none"><keycloak-auth v-if="auth !== 'hide'" id="keycloak-auth-xs"/></b-nav-item>
             </b-navbar-nav>
           </b-container>
         </b-collapse>
