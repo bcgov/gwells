5f6abaf8435c14eb203a00cd291a2471a8709a0b
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-10-16 11:51:18 -0700

    Fixed unit test

--- app/frontend/src/submissions/views/SubmissionsHome.vue
+++ app/frontend/src/submissions/views/SubmissionsHome.vue
@@ -5,7 +5,7 @@
         <b-row>
           <b-col lg="12" >Well Activity Submission
             <b-form-radio-group button-variant="outline-primary" size="sm" buttons v-model="formIsFlat" label="Form layout" class="float-right">
-              <b-form-radio v-bind:value="true">Single page</b-form-radio>
+              <b-form-radio v-bind:value="true" id="flat">Single page</b-form-radio>
               <b-form-radio v-bind:value="false">Multi page</b-form-radio>
             </b-form-radio-group>
           </b-col>
