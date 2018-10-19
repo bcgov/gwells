73d6aaefd28a1bc463485238cc29328781daee1f
Steve Hillier <stephenhillier@gmail.com>
2018-10-19 12:20:24 -0700

    add a route for preview

--- app/frontend/src/submissions/router/index.js
+++ app/frontend/src/submissions/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import SubmissionsHome from '@/submissions/views/SubmissionsHome.vue'
+import SubmissionPreview from '@/submissions/components/SubmissionPreview/SubmissionPreview.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/preview',
+      name: 'SubmissionPreview',
+      component: SubmissionPreview
+    },
     {
       path: '/:id/edit',
       name: 'SubmissionsEdit',
