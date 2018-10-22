b7f31f19643fc9301cf0862ebf05df7b5c68dc72
Sybrand Strauss <sybrand.strauss@gov.bc.ca>
2018-10-22 12:13:12 -0700

    Busy experimenting with map

---
+++ app/frontend/src/submissions/components/SubmissionForm/CoordsMap.vue
@@ -0,0 +1,62 @@
+<template>
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      style="height: 80%"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate">
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"/>
+      <l-marker :lat-lng="marker">
+        <l-popup>
+          <div @click="popupClick">
+            I am a tooltip
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi. Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-popup>
+      </l-marker>
+    </l-map>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet'
+import L from 'leaflet'
+export default {
+  name: 'Example',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup
+  },
+  data () {
+    return {
+      zoom: 13,
+      center: L.latLng(47.413220, -1.219482),
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: L.latLng(47.413220, -1.219482),
+      currentZoom: 13,
+      currentCenter: L.latLng(47.413220, -1.219482),
+      showParagraph: false
+    }
+  },
+  methods: {
+    zoomUpdate (zoom) {
+      this.currentZoom = zoom
+    },
+    centerUpdate (center) {
+      this.currentCenter = center
+    },
+    showLongText () {
+      this.showParagraph = !this.showParagraph
+    },
+    popupClick () {
+      alert('Popup Click!')
+    }
+  }
+}
+</script>
