b0e6bc5921ad994c0fa5062c85f590811cf08cc3
Steve Hillier <stephenhillier@gmail.com>
2018-10-25 15:55:13 -0700

    add store into test that calls getters

--- app/frontend/test/unit/specs/submissions/SubmissionPreview.spec.js
+++ app/frontend/test/unit/specs/submissions/SubmissionPreview.spec.js
@@ -1,12 +1,26 @@
+import Vuex from 'vuex'
 import SubmissionPreview from '@/submissions/components/SubmissionPreview/SubmissionPreview.vue'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 
 const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('filterBlankRows', () => {
+  let store
+  let getters
+
+  beforeEach(() => {
+    getters = {
+      codes: () => ({})
+    }
+    store = new Vuex.Store({
+      getters
+    })
+  })
   it('Removes blank rows and objects containing only empty strings for tables', () => {
     const wrapper = shallowMount(SubmissionPreview, {
       localVue,
+      store,
       propsData: {
         form: {},
         sections: {}
@@ -25,6 +39,7 @@ describe('filterBlankRows', () => {
   it('Does not remove rows with values for tables', () => {
     const wrapper = shallowMount(SubmissionPreview, {
       localVue,
+      store,
       propsData: {
         form: {},
         sections: {}
