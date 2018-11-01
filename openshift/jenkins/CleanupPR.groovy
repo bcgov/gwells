2393c395caa5a92342a135299fde0edac1e0a946
Steve Hillier <stephenhillier@gmail.com>
2018-11-01 14:20:30 -0700

    clean up selectors

--- openshift/jenkins/CleanupPR.groovy
+++ openshift/jenkins/CleanupPR.groovy
@@ -14,25 +14,19 @@ def payload = jsonSlurper.parseText(build.buildVariableResolver.resolve("payload
 def prNum = payload['number']
 
 
-// this script is triggered on all events, but we are specifically interested in pull requests
-// pull requests come with actions like "opened", "closed"
+// this script is triggered on all events, but we are specifically interested in pull requests that are closed
+// pull requests come with actions like "opened", "closed".  Merged and closed are the same event (there is an
+// additional "merged: true" property)
 if (ghEventType == 'pull_request' && payload['action'] == 'closed' && prNum) {
 
     def sout = new StringBuilder(), serr = new StringBuilder()
 
     // delete all the objects in the DEV namespace labeled with this PR number
-    def deleteAllAppObjects = "oc delete all,pvc,secret,configmap -n ${DEV_PROJECT} -l app=gwells-dev-pr-${prNum}".execute()
+    // todo: there are several labels that need to be targeted and hardcoding them is fragile.
+    // a future task should focus on creating a label that applies to all resources associated with one pull request.
+    def deleteAllAppObjects = "oc delete all,pvc,secret,configmap -n ${DEV_PROJECT} -l app=gwells-dev-pr-${prNum} -l appver=gwells-dev-pr-${prNum} -l gluster.kubernetes.io/provisioned-for-pvc=gwells-pgsql-dev-pr-${prNum}".execute()
     deleteAllAppObjects.consumeProcessOutput(sout, serr)
     deleteAllAppObjects.waitForOrKill(5000)
-    println "out> $sout err> $serr"
-
-    // some objects aren't labeled with the "app" label - but we need to delete them too
-    // todo: currently these objects are labeled with appver (needs to be given a better name or labeled with "app")
-    sout = new StringBuilder()
-    serr = new StringBuilder()
-    def deleteAllCreatedObjects = "oc delete all,pvc,secret,configmap -n ${DEV_PROJECT} -l appver=gwells-dev-pr-${prNum}".execute()
-    deleteAllCreatedObjects.consumeProcessOutput(sout, serr)
-    deleteAllCreatedObjects.waitForOrKill(5000)
     println "out> $sout err> $serr"
 
     // delete the objects in the tools project (this is primarly the build configs,
