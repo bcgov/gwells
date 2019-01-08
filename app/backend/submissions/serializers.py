3c1ec5f84e9013b5e513c3a5b6e586b7e869ebea
Steve Hillier <stephenhillier@gmail.com>
2019-01-07 17:19:43 -0800

    remove odd double-quote in sea of single quotes

--- app/backend/submissions/serializers.py
+++ app/backend/submissions/serializers.py
@@ -467,7 +467,7 @@ class WellStaffEditSubmissionSerializer(WellSubmissionSerializerBase):
             'water_quality_colour',
             'water_quality_odour',
             'ems_id',
-            "aquifer",
+            'aquifer',
             'total_depth_drilled',
             'finished_well_depth',
             'decommission_reason',
