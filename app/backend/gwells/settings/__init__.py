ee4e26fbf15574593ec232e922681506d23d3182
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2019-01-25 23:12:17 -0800

    Fixed mis-spelling.

--- app/backend/gwells/settings/__init__.py
+++ app/backend/gwells/settings/__init__.py
@@ -65,10 +65,9 @@ FIXTURES_DIR = '/'.join([BASE_DIR, APP_CONTEXT_ROOT, 'fixtures'])
 # Fixtures dirs
 FIXTURES_DIRS = [FIXTURES_DIR]
 
-# GDAL Library Path
+# GeoDjango External Library Paths
 GDAL_LIBRARY_PATH = get_env_variable('GDAL_LIBRARY_PATH', '/usr/lib/libgdal.so')
-GEOS_LIBRARY_PATH = get_env_variable('GDAL_LIBRARY_PATH', '/usr/lib/libgeos_c.so')
-
+GEOS_LIBRARY_PATH = get_env_variable('GEOS_LIBRARY_PATH', '/usr/lib/libgeos_c.so')
 
 # django-settings-export lets us make these variables available in the templates.
 # This eleminate the need for setting the context for each and every view.
