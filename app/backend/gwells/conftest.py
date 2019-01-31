986e1dae4344d57660ed0f64e298c5e268d5bae3
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2019-01-30 21:34:32 -0800

    Moving test config file to main app folder.

--- app/backend/gwells/conftest.py
+++ app/backend/gwells/conftest.py
@@ -1,23 +1,14 @@
 import pytest
-import get_env_variable
-import settings
-
-engines = {
-    'sqlite': 'django.db.backends.sqlite3',
-    'postgresql': 'django.db.backends.postgresql',
-    'postgis': 'django.contrib.gis.db.backends.postgis',
-    'mysql': 'django.db.backends.mysql',
-}
+from gwells.settings.base import get_env_variable
 
 @pytest.fixture(scope='session')
 def django_db_setup():
     service_name = get_env_variable('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-    name = get_env_variable('DATABASE_NAME')
-    engine = engines['postgis']
 
-    settings.DATABASES['gwells'] = {
-        'ENGINE': engine,
-        'NAME': name,
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'HOST': get_env_variable('DATABASE_SERVICE_NAME'),        
+        'NAME': get_env_variable('DATABASE_NAME'),
         'USER': get_env_variable('DATABASE_USER'),
         'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
         'HOST': get_env_variable('{}_SERVICE_HOST'.format(service_name)),
