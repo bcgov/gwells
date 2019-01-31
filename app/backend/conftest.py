0fbf4cfe21f08c3a0697a69928e01146cba187b7
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2019-01-30 20:59:43 -0800

    Trying to get conftest.py recognized during nose tests.

--- app/backend/conftest.py
+++ app/backend/conftest.py
@@ -1,12 +1,25 @@
 import pytest
+import get_env_variable
+import settings
+
+engines = {
+    'sqlite': 'django.db.backends.sqlite3',
+    'postgresql': 'django.db.backends.postgresql',
+    'postgis': 'django.contrib.gis.db.backends.postgis',
+    'mysql': 'django.db.backends.mysql',
+}
 
 @pytest.fixture(scope='session')
 def django_db_setup():
+    service_name = get_env_variable('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
+    name = get_env_variable('DATABASE_NAME')
+    engine = engines['postgis']
+
     settings.DATABASES['gwells'] = {
-        'ENGINE': get_env_variable('DATABASE_ENGINE'),
-        'NAME': get_env_variable('DATABASE_NAME'),
+        'ENGINE': engine,
+        'NAME': name,
         'USER': get_env_variable('DATABASE_USER'),
         'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
-        'HOST': get_env_variable('DATABASE_SERVICE_NAME'),
-        'PORT': get_env_variable('POSTGRESQL_SERVICE_PORT'),
+        'HOST': get_env_variable('{}_SERVICE_HOST'.format(service_name)),
+        'PORT': get_env_variable('{}_SERVICE_PORT'.format(service_name)),
     }
