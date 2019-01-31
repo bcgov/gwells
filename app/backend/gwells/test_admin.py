8e1cc86f4f62d9dc98ccf1bce2105f2ac33c976e
Steve Hillier <stephenhillier@gmail.com>
2019-01-30 16:08:58 -0800

    remove tests that rely on frontend being built first

--- app/backend/gwells/test_admin.py
+++ app/backend/gwells/test_admin.py
@@ -27,23 +27,3 @@ class AdminTestCase(TestCase):
         # test
         response = self.client.get(reverse('site_admin'))
         self.assertNotEqual(response.status_code, HTTPStatus.OK)
-
-    def test_authorized_user_admin_view(self):
-        # setup
-        group_name = 'admin'
-        username = 'admin'
-        password = 'admin'
-        email = 'admin@admin.com'
-        self.user = User.objects.create_user(username=username, password=password, email=email)
-        admin_group = Group.objects.get(name=group_name)
-        admin_group.user_set.add(self.user)
-        self.client.login(username=username, password=password)
-
-        # test
-        response = self.client.get(reverse('site_admin'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-        # teardown
-        self.client.logout()
-        self.user.delete()
-        admin_group.user_set.remove(self.user)
