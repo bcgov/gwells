919f225c937ee03db1aa3a1c73416da495dcc970
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2019-01-30 19:03:17 -0800

    Continued backout of geometry changes in model, but leaving in pygdal related features.

--- app/backend/wells/migrations/0023_auto_change_pid_to_positive_integerfield_20180925_2040.py
+++ app/backend/wells/migrations/0023_auto_change_pid_to_positive_integerfield_20180925_2040.py
@@ -8,7 +8,7 @@ from django.conf import settings
 
 
 class Migration(migrations.Migration):
-    atomic = settings.DATABASES.get('default').get('engine') == 'django.contrib.gis.db.backends.postgis'
+    atomic = settings.DATABASES.get('default').get('engine') == 'django.db.backends.postgresql'
 
     dependencies = [
         ('wells', '0022_update_casing_material_code'),
