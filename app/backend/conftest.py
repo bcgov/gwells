07b2442a851428753dc5aae53c5fc99c9bab9eda
Gary Wong (BC Gov) <garywong@gov.bc.ca>
2019-01-30 19:17:52 -0800

    Trying to get nose tests to use conftest.py configuration during unit tests.

--- app/backend/conftest.py
+++ app/backend/conftest.py
@@ -2,15 +2,7 @@ import pytest
 
 @pytest.fixture(scope='session')
 def django_db_setup():
-"""
-
-DATABASE_SCHEMA=public
-DATABASE_USER=userGN0
-DATABASE_NAME=gwells
-DATABASE_ENGINE=postgresql
-DATABASE_SERVICE_NAME=gwells-pgsql-dev-pr-1116
-"""
-    settings.DATABASES['default'] = {
+    settings.DATABASES['gwells'] = {
         'ENGINE': get_env_variable('DATABASE_ENGINE'),
         'NAME': get_env_variable('DATABASE_NAME'),
         'USER': get_env_variable('DATABASE_USER'),
