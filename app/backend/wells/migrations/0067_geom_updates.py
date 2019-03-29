2be0a460d7a222cf6080692718bb5ae21417b276
AlexZorkin <alex@bigthink.solutions>
2019-03-29 11:52:52 -0700

    added logging

--- app/backend/wells/migrations/0067_geom_updates.py
+++ app/backend/wells/migrations/0067_geom_updates.py
@@ -2,6 +2,8 @@
 
 from django.db import migrations
 from django.core.exceptions import FieldDoesNotExist
+import logging
+logger = logging.getLogger(__name__)
 
 
 def update_fields(apps, schema_editor):
@@ -10,6 +12,7 @@ def update_fields(apps, schema_editor):
         activitySubmission._meta.get_field('latitude')
         schema_editor.execute("update activity_submission set geom = ST_SetSrid(ST_MakePoint(longitude, latitude), 4326);")
     except FieldDoesNotExist:
+        logger.error('Field does not exist.')
         pass
 
     well = apps.get_model('wells', 'well')
@@ -17,6 +20,7 @@ def update_fields(apps, schema_editor):
         well._meta.get_field('latitude')
         schema_editor.execute("update well set geom = ST_SetSrid(ST_MakePoint(longitude, latitude), 4326);")
     except FieldDoesNotExist:
+        logger.error('Field does not exist.')
         pass
 
 
