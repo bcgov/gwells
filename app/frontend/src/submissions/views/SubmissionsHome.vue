987282190a1cd3ed1cbd3739cff8f8c958ee99f6
Steve Hillier <stephenhillier@gmail.com>
2019-04-01 17:00:07 -0700

    splitting out staff edit and well summary views

--- app/frontend/src/submissions/views/SubmissionsHome.vue
+++ app/frontend/src/submissions/views/SubmissionsHome.vue
@@ -499,7 +499,7 @@ export default {
 
         this.loading = true
 
-        ApiService.query(`wells/${this.$route.params.id}`).then((res) => {
+        ApiService.query(`wells/${this.$route.params.id}?codes=true`).then((res) => {
           Object.keys(res.data).forEach((key) => {
             if (key in this.form) {
               this.form[key] = res.data[key]
