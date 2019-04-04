e1a6e22eb10ebcc4aab55152bed41acf20982100
Steve Hillier <stephenhillier@gmail.com>
2019-04-04 14:09:02 -0700

    add outbound link to obs well

--- app/frontend/src/wells/views/WellDetail.vue
+++ app/frontend/src/wells/views/WellDetail.vue
@@ -21,7 +21,15 @@
         <b-row>
           <b-col cols="12" md="4"><span class="font-weight-bold">Well Tag Number:</span> {{ well.well_tag_number }}</b-col>
           <b-col cols="12" md="4"><span class="font-weight-bold">Well Status:</span> {{ well.well_status }}</b-col>
-          <b-col cols="12" md="4"><span class="font-weight-bold">Observation Well Number:</span> {{ well.observation_well_number }}</b-col>
+          <b-col cols="12" md="4"><span class="font-weight-bold">Observation Well Number:</span>
+            <a
+              v-if="well.observation_well_number !== undefined"
+              id="obs_well_number"
+              :href="`https://governmentofbc.maps.arcgis.com/apps/webappviewer/index.html?id=b53cb0bf3f6848e79d66ffd09b74f00d&find=OBS%20WELL%20${well.observation_well_number}`"
+            >
+              {{ well.observation_well_number }}
+            </a>
+          </b-col>
         </b-row>
         <b-row>
           <b-col cols="12" md="4"><span class="font-weight-bold">Well Identification Plate Number:</span> {{ well.identification_plate_number }}</b-col>
