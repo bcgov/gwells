27d30245a587740e1e1de3696a5eba2193f78537
AlexZorkin <alex@bigthink.solutions>
2019-04-10 16:37:52 -0700

    added unit test checking for altered array between parent and child

--- app/frontend/test/unit/specs/submissions/ActivitySubmissionForm.spec.js
+++ app/frontend/test/unit/specs/submissions/ActivitySubmissionForm.spec.js
@@ -2,6 +2,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import ActivitySubmissionForm from '@/submissions/components/SubmissionForm/ActivitySubmissionForm'
+import Lithology from '@/submissions/components/SubmissionForm/Lithology'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -53,5 +54,53 @@ describe('ActivitySubmissionForm.vue', () => {
       expect(wrapper.vm.showSection('wellYield')).toBe(false)
       done()
     })
+  })
+  it('updates lithologydescription_set when lithology table is altered on Lithology child component', () => {
+    let getters = {
+      codes: () => ({})
+    }
+    let store = new Vuex.Store({
+      getters
+    })
+
+    const wrapper = mount(ActivitySubmissionForm, {
+      localVue,
+      router,
+      store,
+      propsData: {
+        sections: { lithology: true },
+        currentStep: 'lithology',
+        activityType: 'STAFF_EDIT',
+        formSteps: {
+          STAFF_EDIT: ['lithology']
+        },
+        isStaffEdit: true,
+        formIsFlat: true,
+        formSubmitLoading: false,
+        formValueChanged: false,
+        form: {
+          lithologydescription_set: [{
+            lithology_raw_data: '34',
+            lithology_colour: 'black',
+            lithology_hardness: 'hard',
+            lithology_moisture: 'Dry',
+            lithology_observation: 'test',
+            lithology_description: 'basic'
+          }]
+        }
+      }
+    })
+
+    wrapper.find(Lithology).vm.lithologyData.push({
+      lithology_raw_data: '1',
+      lithology_colour: 'white',
+      lithology_hardness: 'test1322',
+      lithology_moisture: 'Wet',
+      lithology_observation: 'non',
+      lithology_description: 'Solid'
+    })
+
+    expect(wrapper.vm.form.lithologydescription_set.length).toBe(2)
+    expect(wrapper.vm.form.lithologydescription_set[1].lithology_hardness).toBe('test1322')
   })
 })
