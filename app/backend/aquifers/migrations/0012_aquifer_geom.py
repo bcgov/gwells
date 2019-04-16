f696fdbbc41c1a04555a0cb7a4dc894a3036d6cd
Clark Van Oyen <clark@countable.ca>
2019-04-16 13:01:28 -0700

    Don't add shapefiles in migration.

--- app/backend/aquifers/migrations/0012_aquifer_geom.py
+++ app/backend/aquifers/migrations/0012_aquifer_geom.py
@@ -5,21 +5,6 @@ from django.db import migrations
 from aquifers.models import Aquifer as NewAquifer
 
 
-def add_shapefile(apps, schema_editor):
-    Aquifer = apps.get_model('aquifers', 'Aquifer')
-    aquifers = Aquifer.objects.all()
-    aquifer = aquifers[0]
-    NewAquifer.load_shapefile(aquifer, "aquifers/fixtures/shp/aquifer1193.zip")
-    aquifer.save()
-    aquifer = aquifers[1]
-    NewAquifer.load_shapefile(aquifer, "aquifers/fixtures/shp/aquiferagate.zip")
-    aquifer.save()
-
-
-def remove_shapefile(apps, schema_editor):
-    pass  # column is deleted, don't worry about it.
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -33,8 +18,4 @@ class Migration(migrations.Migration):
             field=django.contrib.gis.db.models.fields.PolygonField(
                 null=True, srid=3005),
         ),
-        # migrations.RunPython(
-        #     add_shapefile,
-        #     reverse_code=remove_shapefile
-        # ),
     ]
