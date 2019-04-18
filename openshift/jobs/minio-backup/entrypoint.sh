2b92ee1e4c57b83c98e7ae0026892b1f61c128a9
Steve Hillier <stephenhillier@gmail.com>
2019-04-17 17:12:47 -0700

    add a basic template

--- openshift/jobs/minio-backup/entrypoint.sh
+++ openshift/jobs/minio-backup/entrypoint.sh
@@ -3,18 +3,18 @@ set -e
 
 # Check if repository is initialized.  If not, initialize it.
 # RESTIC_PASSWORD is required.
-if ! restic -r /backup/gwells-documents snapshots > /dev/null 2>&1; then
-    restic -r /backup/gwells-documents init ; fi
+if ! restic -r /mnt/dest/gwells-documents snapshots > /dev/null 2>&1; then
+    restic -r /mnt/dest/gwells-documents init ; fi
 
 # Backup files using delta (de-duplicate) and encryption
-restic -r /backup/gwells-documents backup /docs
+restic -r /mnt/dest/gwells-documents backup /mnt/src/data
 
 # Clean up old snapshots.
 # As an example, the following arguments:
 # --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 2
 # will keep the most recent 7 daily snapshots, 5 weekly, 12 monthly, and 2 yearly snapshots.
 # The rest will be pruned.
-restic -r /backup/gwells-documents forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 10 --prune
+restic -r /mnt/dest/gwells-documents forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 10 --prune
 
 # check repository integrity before exiting
-restic -r /backup/gwells-documents check
+restic -r /mnt/dest/gwells-documents check
