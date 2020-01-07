e56dc96d3105b6015fc00ad2aa29029ebab2e989
Séamus O'Connor <seamus.oconnor@pneumaticweb.com>
2020-01-07 10:55:00 -0800

    Changes outdated browser message

--- app/frontend/src/common/helpers/browserUpdate.js
+++ app/frontend/src/common/helpers/browserUpdate.js
@@ -5,8 +5,10 @@ const config = {
   insecure: true,
   unsupported: true,
   reminder: 0,
+  // reminderClosed: 0,
   no_permanent_hide: true,
-  api: 2010.01
+  api: 2010.01,
+  text: '{brow_name} is not supported by the Groundwater Wells and Aquifers application. We recommend using: Chrome, Firefox or Safari.<br><a{up_but}>Read more</a> or <a{ignore_but}>ignore</a>'
 }
 
 browserUpdate(config)
