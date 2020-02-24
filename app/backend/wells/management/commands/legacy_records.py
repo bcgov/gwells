ee17ebe42a3a15d7646759524b227bbd46ec0ab6
Séamus O'Connor <seamus.oconnor@pneumaticweb.com>
2020-02-24 13:39:32 -0800

    Adds --next switch to legacy_records command

--- app/backend/wells/management/commands/legacy_records.py
+++ app/backend/wells/management/commands/legacy_records.py
@@ -33,15 +33,21 @@ class Command(BaseCommand):
         # Arguments added for debugging purposes.
         parser.add_argument('--start', type=int, nargs='?', help='Well to start at', default=1)
         parser.add_argument('--end', type=int, nargs='?', help='Well to end at', default=50)
+        parser.add_argument('--next', type=int, nargs='?', help='Process n wells', default=50)
 
     def handle(self, *args, **options):
         start = options['start']
         end = options['end']
-        print(f'Creating legacy records from well_tag_number {start} to {end}')
+        to_do_amount = options['next']
 
-        wells = self.find_wells_without_legacy_records(start, end)
+        num_wells = 0
+        if to_do_amount:
+            wells = self.find_next_n_wells_without_legacy_records(start, to_do_amount)
+        else:
+            wells = self.find_wells_without_legacy_records(start, end)
+
         num_wells = len(wells)
-        print(f'Found {num_wells} wells to update')
+        print(f'Creating {num_wells} legacy records from well_tag_number {wells[0].well_tag_number} to {wells[len(wells) - 1].well_tag_number}')
 
         start = timer()
         for well in wells:
@@ -61,6 +67,13 @@ class Command(BaseCommand):
                     well_tag_number__lte=end,
                     activitysubmission__isnull=True) \
             .order_by('well_tag_number')
+        return wells
+
+    def find_next_n_wells_without_legacy_records(self, start, num):
+        wells = Well.objects \
+            .filter(well_tag_number__gte=start, activitysubmission__isnull=True) \
+            .order_by('well_tag_number') \
+            [0:num]
         return wells
 
     @transaction.atomic
