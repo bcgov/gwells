89b8c4b8da65ad48802e7fb1b00eb9dd6babd561
Séamus O'Connor <seamus.oconnor@pneumaticweb.com>
2020-03-04 12:28:06 -0800

    Initializes Sentry only when app is loaded at prod URL

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -34,7 +34,7 @@ import { FETCH_CONFIG } from '@/common/store/config.js'
 // GWELLS js API library (helper methods for working with API)
 import ApiService from '@/common/services/ApiService.js'
 
-if (process.env.NODE_ENV === 'production') {
+if (/^https:\/\/apps\.nrs\.gov\.bc\.ca\/gwells/.test(window.location.href)) {
   Sentry.init({
     dsn: 'https://a83809da8c9b4f39b3d7cd683b803859@sentry.io/1802823',
     integrations: [new Integrations.Vue({ Vue, attachProps: true, logError: true })]
