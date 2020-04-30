9525707533fb925c4cc3e3c853929265c473a4ca
Séamus O'Connor <seamus.oconnor@pneumaticweb.com>
2020-04-30 11:50:16 -0700

    Changes vector tile server URL path to be /tiles/

--- app/frontend/src/common/mapbox/layers.js
+++ app/frontend/src/common/mapbox/layers.js
@@ -1,6 +1,6 @@
 import { defaultsDeep } from 'lodash'
 
-const VECTOR_TILE_SERVER = process.env.VUE_APP_VECTOR_TILE_SERVER
+const VECTOR_TILE_SERVER = `${window.location.protocol}${window.location.host}${process.env.VUE_APP_VECTOR_TILE_BASE_URL}`
 
 export const WELLS_LAYER_SOURCE = 'postgis_ftw.well_view'
 export const AQUIFERS_LAYER_SOURCE = 'postgis_ftw.aquifer_view'
