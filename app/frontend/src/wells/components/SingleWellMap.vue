dfa67b282ce2927ea0692619c38979dc20613c04
Séamus O'Connor <seamus.oconnor@pneumaticweb.com>
2020-05-12 16:12:50 -0700

    Replaces Leaflet with Mapbox GL JS Well Summary screen

--- app/frontend/src/wells/components/SingleWellMap.vue
+++ app/frontend/src/wells/components/SingleWellMap.vue
@@ -12,13 +12,24 @@ Licensed under the Apache License, Version 2.0 (the "License");
     limitations under the License.
 */
 <template>
-  <div id="single-well-map" class="map"/>
+  <div id="single-well-map" class="map">
+    <p id="unsupported-browser" v-if="browserUnsupported">Your browser is unable to view the map</p>
+  </div>
 </template>
 
 <script>
-import L from 'leaflet'
-import 'leaflet-gesture-handling'
-import { tiledMapLayer } from 'esri-leaflet'
+import mapboxgl from 'mapbox-gl'
+import GestureHandling from '@geolonia/mbgl-gesture-handling'
+
+import {
+  DATABC_ROADS_SOURCE,
+  DATABC_CADASTREL_SOURCE,
+  DATABC_ROADS_SOURCE_ID,
+  DATABC_CADASTREL_SOURCE_ID,
+  DATABC_ROADS_LAYER,
+  DATABC_CADASTREL_LAYER
+} from '../../common/mapbox/layers'
+import { buildLeafletStyleMarker } from '../../common/mapbox/images'
 
 export default {
   name: 'SingleWellMap',
@@ -28,118 +39,87 @@ export default {
     },
     longitude: {
       type: Number
-    },
-    draggable: {
-      type: Boolean,
-      default: false
-    },
-    insideBC: {
-      type: Function,
-      default: () => {}
     }
   },
   data () {
     return {
-      map: null
+      map: null,
+      browserUnsupported: false
     }
   },
   mounted () {
-    // There seems to be an issue loading leaflet immediately on mount, we use nextTick to ensure
-    // that the view has been rendered at least once before injecting the map.
-    this.$nextTick(function () {
-      this.initLeaflet()
-      this.initMap()
-    })
+    this.$emit('mapLoading')
+
+    this.initMapBox()
   },
   destroyed () {
     this.map.remove()
-  },
-  watch: {
-    latitude () {
-      this.updateCoords()
-    },
-    longitude () {
-      this.updateCoords()
-    }
+    this.map = null
   },
   methods: {
-    initLeaflet () {
-      // There is a known issue using leaflet with webpack, this is a workaround
-      // Fix courtesy of: https://github.com/PaulLeCam/react-leaflet/issues/255
-      delete L.Icon.Default.prototype._getIconUrl
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-        iconUrl: require('leaflet/dist/images/marker-icon.png'),
-        shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-      })
-    },
-    initMap () {
-      // Create map, with default centered and zoomed to show entire BC.
-      this.map = L.map(this.$el, {
-        gestureHandling: true
-      }).setView([this.latitude ? this.latitude : 54.5, this.getLongitude() ? this.getLongitude() : -126.5], 5)
-      L.control.scale().addTo(this.map)
+    initMapBox () {
+      if (!mapboxgl.supported()) {
+        this.browserUnsupported = true
+        return
+      }
 
-      // Add map layers.
-      tiledMapLayer({ url: 'https://maps.gov.bc.ca/arcserver/rest/services/Province/roads_wm/MapServer' }).addTo(this.map)
-      L.tileLayer.wms('https://openmaps.gov.bc.ca/geo/pub/WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW/ows?', {
-        format: 'image/png',
-        layers: 'pub:WHSE_CADASTRE.PMBC_PARCEL_FABRIC_POLY_SVW',
-        styles: 'PMBC_Parcel_Fabric_Cadastre_Outlined',
-        transparent: true
-      }).addTo(this.map)
-      this.createMarker()
-    },
-    createMarker () {
-      if (this.map && this.latitude !== null && this.getLongitude() !== null && !isNaN(this.latitude) && !isNaN(this.getLongitude())) {
-        const latlng = L.latLng(this.latitude, this.getLongitude())
-        this.marker = L.marker(latlng, { draggable: this.draggable, autoPan: true })
-        this.marker.addTo(this.map)
-        this.setMarkerPopup(this.latitude, this.longitude)
-        // The 1st time we create a marker, we jump to a different zoom level.
-        this.map.setView(latlng, 13)
-      }
-    },
-    updateMarkerLatLong (latlng) {
-      if (this.marker) {
-        this.setMarkerPopup(latlng.lat, latlng.lng)
-        this.marker.setLatLng(latlng)
-      }
-    },
-    setMarkerPopup (latitude, longitude) {
-      this.marker.bindPopup('Latitude: ' + latitude + ', Longitude: ' + longitude)
-    },
-    updateCoords () {
-      if (this.latitude && this.longitude) {
-        const latlng = L.latLng(this.latitude, this.getLongitude())
-        if (this.marker) {
-          this.updateMarkerLatLong(latlng)
-          this.map.panTo(latlng)
-        } else {
-          this.createMarker()
+      var mapConfig = {
+        container: this.$el,
+        zoom: 6,
+        minZoom: 4,
+        maxPitch: 0,
+        dragRotate: false,
+        center: [this.longitude || -126.5, this.latitude || 54.5],
+        style: {
+          version: 8,
+          sources: {
+            [DATABC_ROADS_SOURCE_ID]: DATABC_ROADS_SOURCE,
+            [DATABC_CADASTREL_SOURCE_ID]: DATABC_CADASTREL_SOURCE
+          },
+          layers: [
+            DATABC_ROADS_LAYER,
+            DATABC_CADASTREL_LAYER
+          ]
         }
       }
-    },
-    getLongitude () {
-      // In B.C. users are used to omitting the minus sign on longitude, it's always negative. So we're
-      // very forgiving, and just always make sure longitude is negative.
-      return this.longitude > 0 ? this.longitude * -1 : this.longitude
+
+      this.map = new mapboxgl.Map(mapConfig)
+      new GestureHandling({ modifierKey: 'ctrl' }).addTo(this.map)
+
+      /* Add controls */
+
+      this.map.addControl(new mapboxgl.NavigationControl({ showCompass: false }), 'top-left')
+      this.map.addControl(new mapboxgl.ScaleControl({
+        maxWidth: 80,
+        unit: 'imperial'
+      }))
+      this.map.addControl(new mapboxgl.ScaleControl({
+        maxWidth: 80,
+        unit: 'metric'
+      }))
+      this.map.addControl(new mapboxgl.AttributionControl({
+        customAttribution: 'MapBox | Government of British Columbia, DataBC, GeoBC '
+      }))
+
+      this.map.on('load', () => {
+        if (this.longitude && this.latitude) {
+          buildLeafletStyleMarker(this.longitude, this.latitude).addTo(this.map)
+
+          this.map.setZoom(12)
+        }
+
+        this.$emit('mapLoaded')
+      })
     }
   }
 }
 </script>
-<style>
-@import "~leaflet/dist/leaflet.css";
-@import "~leaflet-gesture-handling/dist/leaflet-gesture-handling.css";
+<style lang="scss">
+@import "~mapbox-gl/dist/mapbox-gl.css";
 
 #single-well-map {
-  width: 100%;
   height: 500px;
-}
 
-@media print {
-  .leaflet-control-zoom {
-    visibility: hidden;
-  }
+  @import "@/common/mapbox.scss";
 }
 </style>
