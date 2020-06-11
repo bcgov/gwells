f1212b38f70816d328651d2ec6f2699ffe9f0d4c
Séamus O'Connor <seamus.oconnor@pneumaticweb.com>
2020-06-11 08:32:19 -0700

    Changes ecocat to vector layer and adds map ecocat tooltips

--- app/frontend/src/common/mapbox/popup.js
+++ app/frontend/src/common/mapbox/popup.js
@@ -1,17 +1,94 @@
+import mapboxgl from 'mapbox-gl'
+
+import { getLngLatOfPointFeature } from './geometry'
+
 // Add onclick handler to use vue router to route to the link for all anchors in a popup
 export function wireUpAnchors (el, $router, routes) {
   function onClick (e) {
     if (!e.ctrlKey) {
       e.preventDefault()
       const routeName = e.currentTarget.getAttribute('data-route-name')
-      $router.push(routes[routeName])
+      if (!routeName) {
+        throw new Error('Anchor without a data-route-name')
+      }
+      const route = routes[routeName] || null
+      if (!route) {
+        throw new Error(`Route named "${routeName}" not found in routes`)
+      }
+      $router.push(route)
     }
   }
 
-  const anchors = el.querySelectorAll('a')
+  const anchors = el.querySelectorAll('a:not([target="_blank"])')
   for (let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener('click', onClick)
   }
 
   return el
+}
+
+export function setupFeatureTooltips (map, layers, options = {}) {
+  const popup = new mapboxgl.Popup()
+
+  const tooltip = new mapboxgl.Popup({ className: 'mapboxgl-popup-no-interaction' })
+  let currentFeature = null
+
+  const layerIds = Object.keys(layers)
+  layerIds.reverse()
+
+  map.on('mousemove', (e) => {
+    const features = map.queryRenderedFeatures(e.point, { layers: layerIds })
+
+    if (features.length > 0) {
+      const feature = features[0]
+
+      const layer = layers[feature.layer.id]
+
+      if (feature !== currentFeature) {
+        const contentDiv = layer.createTooltipContent(features, { canInteract: false })
+        tooltip.setDOMContent(contentDiv)
+        currentFeature = feature
+
+        // Only show tooltip when stuck popup is not open
+        if (!tooltip.isOpen() && !popup.isOpen()) {
+          map.getCanvas().style.cursor = 'pointer'
+
+          tooltip.addTo(map)
+        }
+      }
+
+      const lngLat = layer.snapToCenter ? getLngLatOfPointFeature(feature) : e.lngLat
+
+      tooltip.setLngLat(lngLat)
+    } else {
+      if (currentFeature !== null) {
+        currentFeature = null
+        tooltip.remove()
+        map.getCanvas().style.cursor = ''
+      }
+    }
+  })
+
+  map.on('click', (e) => {
+    const features = map.queryRenderedFeatures(e.point, { layers: layerIds })
+
+    if (features.length > 0) {
+      const feature = features[0]
+      const layerId = feature.layer.id
+      const layer = layers[layerId]
+
+      const contentDiv = layer.createTooltipContent(features, { canInteract: true })
+
+      popup
+        .setDOMContent(contentDiv)
+        .setLngLat(tooltip.getLngLat())
+        .addTo(map)
+      tooltip.remove()
+    }
+  })
+
+  map.on('reset', () => {
+    popup.remove()
+    tooltip.remove()
+  })
 }
--- app/frontend/src/wells/popup.js
+++ app/frontend/src/wells/popup.js
@@ -1,41 +1,19 @@
-import mapboxgl from 'mapbox-gl'
-
-import { getLngLatOfPointFeature } from '../common/mapbox/geometry'
-import { WELLS_BASE_AND_ARTESIAN_LAYER_ID, SEARCHED_WELLS_LAYER_ID, FOCUSED_WELLS_LAYER_ID } from '../common/mapbox/layers'
+import { WELLS_BASE_AND_ARTESIAN_LAYER_ID } from '../common/mapbox/layers'
 import { wireUpAnchors } from '../common/mapbox/popup'
 
-export const WELL_FEATURE_PROPERTIES_FOR_POPUP = [
-  'well_tag_number',
-  'identification_plate_number',
-  'street_address'
-]
+export function createWellPopupElement (features, map, $router, options = {}) {
+  const canInteract = Boolean(options.canInteract)
+  const wellLayerIds = options.wellLayerIds || [ WELLS_BASE_AND_ARTESIAN_LAYER_ID ]
 
-export function createWellTooltipElement (wellFeatureProperties) {
+  // Filter the features to only the well layers we care about
+  const feature = features.filter((feature) => wellLayerIds.indexOf(feature.layer.id) !== -1)[0]
+
   const {
     well_tag_number: wellTagNumber,
     identification_plate_number: identificationPlateNumber,
     street_address: streetAddress,
     is_published: isPublished
-  } = wellFeatureProperties
-
-  const container = document.createElement('div')
-  container.className = 'mapbox-tooltip-well'
-  container.innerHTML = `
-    Well Tag Number: ${wellTagNumber}<br>
-    Identification Plate Number: ${identificationPlateNumber || '—'}<br>
-    Address: ${streetAddress || '—'}<br>
-    ${!isPublished ? '<b>unpublished</b>' : ''}
-  `
-  return container
-}
-
-export function createWellPopupElement ($router, wellFeatureProperties) {
-  const {
-    well_tag_number: wellTagNumber,
-    identification_plate_number: identificationPlateNumber,
-    street_address: streetAddress,
-    is_published: isPublished
-  } = wellFeatureProperties
+  } = feature.properties
 
   const routes = {
     wellDetail: { name: 'wells-detail', params: { id: wellTagNumber } }
@@ -45,63 +23,13 @@ export function createWellPopupElement ($router, wellFeatureProperties) {
 
   const container = document.createElement('div')
   container.className = 'mapbox-popup-well'
-  container.innerHTML = `
-    Well Tag Number: <a href="${url.href}" data-route-name="wellDetail">${wellTagNumber}</a><br>
-    Identification Plate Number: ${identificationPlateNumber || '—'}<br>
-    Address: ${streetAddress || '—'}<br>
-    ${!isPublished ? '<b>unpublished</b>' : ''}
-  `
-  return wireUpAnchors(container, $router, routes)
-}
-
-// Adds mouse event listeners to the map which will show the tooltip on hover and popup for the clicked wells
-export function setupMapPopups (map, $router, options = {}) {
-  const wellsLayerIds = options.wellsLayerIds || [ WELLS_BASE_AND_ARTESIAN_LAYER_ID, SEARCHED_WELLS_LAYER_ID, FOCUSED_WELLS_LAYER_ID ]
-
-  const tooltip = new mapboxgl.Popup()
-  const popup = new mapboxgl.Popup()
-
-  wellsLayerIds.forEach((wellsLayerId) => {
-    map.on('mouseenter', wellsLayerId, (e) => {
-      if (popup.isOpen()) { return } // don't show tooltip when popup is open
-
-      map.getCanvas().style.cursor = 'pointer'
-
-      const feature = e.features[0]
-      const contentDiv = createWellTooltipElement(feature.properties)
-      tooltip
-        .setLngLat(getLngLatOfPointFeature(feature))
-        .setDOMContent(contentDiv)
-        .addTo(map)
-    })
-
-    map.on('mousemove', wellsLayerId, (e) => {
-      if (popup.isOpen()) { return } // don't show tooltip when popup is open
-
-      const feature = e.features[0]
-      const contentDiv = createWellTooltipElement(feature.properties)
-      tooltip
-        .setLngLat(getLngLatOfPointFeature(feature))
-        .setDOMContent(contentDiv)
-    })
-
-    map.on('mouseleave', wellsLayerId, () => {
-      map.getCanvas().style.cursor = ''
-      tooltip.remove()
-    })
-
-    map.on('click', wellsLayerId, (e) => {
-      const feature = e.features[0]
-      const contentDiv = createWellPopupElement($router, feature.properties)
-      popup
-        .setLngLat(getLngLatOfPointFeature(feature))
-        .setDOMContent(contentDiv)
-        .addTo(map)
-    })
-
-    map.on('reset', () => {
-      tooltip.remove()
-      popup.remove()
-    })
-  })
+  container.innerHTML = [
+    canInteract
+      ? `Well Tag Number: <a href="${url.href}" data-route-name="wellDetail">${wellTagNumber}</a>`
+      : `Well Tag Number: ${wellTagNumber}`,
+    `Identification Plate Number: ${identificationPlateNumber || '—'}`,
+    `Address: ${streetAddress || '—'}`,
+    isPublished === false ? '<b>unpublished</b>' : null
+  ].filter(Boolean).join('<br>')
+  return canInteract ? wireUpAnchors(container, $router, routes) : container
 }
