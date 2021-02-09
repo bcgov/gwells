6e07941b9234ca31a83dc1669cd0e2bca74e4113
dainetrinidad <daine.trinidad@gov.bc.ca>
2021-02-08 16:43:04 -0800

    Change variable name

--- openshift/ocp4/migration/scripts/migrate_database.sh
+++ openshift/ocp4/migration/scripts/migrate_database.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-read -r -p 'Namespace [test/prod] :' NAMESPACE
-. ./dump_and_copy.sh "$NAMESPACE"
+read -r -p 'Namespace [test/prod] :' ENVIRONMENT
+. ./dump_and_copy.sh "$ENVIRONMENT"
 ls -alh /tmp/backup
 echo "------------------------------------------------------------------------------"
 echo "Copy from ocp3 successful. Copying to the db pod and restoring the database..."
 echo "------------------------------------------------------------------------------"
-. ./copy_and_load_db.sh "$NAMESPACE"
+. ./copy_and_load_db.sh "$ENVIRONMENT"
