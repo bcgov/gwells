b417a37ba05a8529a0af25d7cc6b3aa6b777d30d
Steve Hillier <stephenhillier@gmail.com>
2021-02-10 14:49:12 -0800

    add revert options

--- openshift/ocp4/migration/scripts/activate-proxy.sh
+++ openshift/ocp4/migration/scripts/activate-proxy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Usage:  ./activate-proxy.sh <test|prod>
+# add --revert to end (after test/prod argument) to switch the routes back to OCP3 services.
+
+
+set -euo pipefail
 
 NAMESPACE="moe-gwells-$1"
 ENV_NAME="staging"
@@ -27,11 +31,54 @@ ROUTE_PATCH=$(cat <<-EOF
 EOF
 )
 
+REVERT_ROUTE_PATCH_GWELLS=$(cat <<-EOF
+{
+	"spec": {
+		"port": {
+			"targetPort": "web"
+		},
+		"to": {
+			"kind": "Service",
+			"name": "gwells-${ENV_NAME}"
+		}
+	}
+}
+EOF
+)
+
+REVERT_ROUTE_PATCH_TILESERV=$(cat <<-EOF
+{
+	"spec": {
+		"port": {
+			"targetPort": 8080
+		},
+		"to": {
+			"kind": "Service",
+			"name": "pgtileserv-${ENV_NAME}"
+		}
+	}
+}
+EOF
+)
+
+if echo $* | grep -e "--revert" -q
+then
+	# use revert script 
+	oc --kubeconfig=/tmp/KUBECONFIG -n "$NAMESPACE" patch "route/gwells-$ENV_NAME" -p "$REVERT_ROUTE_PATCH_GWELLS"
+	oc --kubeconfig=/tmp/KUBECONFIG -n "$NAMESPACE" patch "route/pgtileserv-$ENV_NAME" -p "$REVERT_ROUTE_PATCH_TILESERV"
+	echo
+	echo "route/gwells-$ENV_NAME patched to direct traffic to OCP3 services (proxy to ocp4 disabled)"
+	echo
+else
+	oc --kubeconfig=/tmp/KUBECONFIG -n "$NAMESPACE" patch "route/gwells-$ENV_NAME" -p "$ROUTE_PATCH"
+	oc --kubeconfig=/tmp/KUBECONFIG -n "$NAMESPACE" patch "route/pgtileserv-$ENV_NAME" -p "$ROUTE_PATCH"
+
+	echo
+	echo "route/gwells-$ENV_NAME patched to direct traffic to OCP4 proxy"
+	echo
+fi
+
+
 # switch to service gwells-maintenance-staging / gwells-maintenance-production ; target port: 2015-tcp
 
-oc --kubeconfig=/tmp/KUBECONFIG -n "$NAMESPACE" patch "route/gwells-$ENV_NAME" -p "$ROUTE_PATCH"
-oc --kubeconfig=/tmp/KUBECONFIG -n "$NAMESPACE" patch "route/pgtileserv-$ENV_NAME" -p "$ROUTE_PATCH"
 
-echo
-echo "route/gwells-$ENV_NAME patched to direct traffic to OCP4 proxy"
-echo
--- openshift/ocp4/migration/scripts/scale-up.sh
+++ openshift/ocp4/migration/scripts/scale-up.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 NAMESPACE="26e83e-$1"
 ENV_NAME="staging"
@@ -7,17 +8,25 @@ if [ "$1" == 'prod' ]; then
     ENV_NAME='production'
 fi
 
-echo
-echo "Scaling up on Silver ($NAMESPACE ; env: $ENV_NAME)"
-echo
+if echo $* | grep -e "--revert" -q
+then
+	# revert to 2 replicas
+    echo "Scaling down to 0 replicas on Silver ($NAMESPACE ; env: $ENV_NAME)"
 
-oc --kubeconfig=/tmp/KUBECONFIGSILVER -n "$NAMESPACE" scale "dc/gwells-$ENV_NAME" --replicas=2
+    oc --kubeconfig=/tmp/KUBECONFIGSILVER -n "$NAMESPACE" scale "dc/gwells-$ENV_NAME" --replicas=0
+    echo "Scaled to 0 replicas"
 
-REPLICAS=$(oc --kubeconfig=/tmp/KUBECONFIGSILVER -n "$NAMESPACE" get dc "gwells-$ENV_NAME" -o go-template="{{.status.readyReplicas}}")
-while [ "$REPLICAS" != 2 ]
-do
-    echo "Waiting for GWELLS to scale up..."
-    sleep 3
+else
+    echo "Scaling up on Silver ($NAMESPACE ; env: $ENV_NAME)"
+
+    oc --kubeconfig=/tmp/KUBECONFIGSILVER -n "$NAMESPACE" scale "dc/gwells-$ENV_NAME" --replicas=2
+
     REPLICAS=$(oc --kubeconfig=/tmp/KUBECONFIGSILVER -n "$NAMESPACE" get dc "gwells-$ENV_NAME" -o go-template="{{.status.readyReplicas}}")
-done
-echo "Successfully scaled up (current replicas: ${REPLICAS})"
+    while [ "$REPLICAS" != 2 ]
+    do
+        echo "Waiting for GWELLS to scale up..."
+        sleep 3
+        REPLICAS=$(oc --kubeconfig=/tmp/KUBECONFIGSILVER -n "$NAMESPACE" get dc "gwells-$ENV_NAME" -o go-template="{{.status.readyReplicas}}")
+    done
+    echo "Successfully scaled up (current replicas: ${REPLICAS})"
+fi
