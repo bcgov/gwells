ef9c9a529583a3ee2f9c870487846d4d987958b8
Daine Trinidad <daine.trinidad@gov.bc.ca>
2021-02-10 16:59:48 -0800

    Reset some parts of the migration

---
+++ openshift/ocp4/migration/scripts/reset_migration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "WARNING! This will not reset the database. This script will only reverse the proxy and pod scaling."
+. ./activate_proxy "$1" --revert
+. ./scale_up.sh "$1" --revert
+. ./scale_down.sh "$1" --revert
