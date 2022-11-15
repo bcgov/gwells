5f98e08dd5961ae55945be849fa2189aebbe845b
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2022-11-14 16:57:45 -0800

    Handle Gold JWT differences

--- app/backend/gwells/authentication.py
+++ app/backend/gwells/authentication.py
@@ -20,6 +20,7 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from gwells.models import Profile
 from gwells.roles import roles_to_groups
 
+KEYCLOAK_GOLD_REALM_URL = 'loginproxy.gov.bc.ca/auth/realms/standard'
 
 class JwtOidcAuthentication(JSONWebTokenAuthentication):
     """
@@ -46,10 +47,18 @@ class JwtOidcAuthentication(JSONWebTokenAuthentication):
             'email': 'email',
             'family_name': 'last_name'
         }
-        payload_profile_mapping = {
-            'preferred_username': 'username',
-            'name': 'name'
-        }
+
+        if self.is_gold_shared_realm(payload):
+            payload_profile_mapping = {
+                'preferred_username': 'username',
+                'name': 'name'
+            }
+        else:
+            payload_profile_mapping = {
+                'sub': 'silver_keycloak_id',
+                'preferred_username': 'username',
+                'name': 'name'
+            }
         # We map auth_time to user.last_login ; this is true depending on your point of view. It's the
         # last time the user logged into sso, which may not co-incide with the last time the user
         # logged into gwells.
@@ -57,6 +66,13 @@ class JwtOidcAuthentication(JSONWebTokenAuthentication):
         if auth_time:
             auth_time = datetime.fromtimestamp(auth_time, tz=timezone.utc)
 
+        # NOTE: We assume that as part of the user migration process (yet to be provided/announced by the SSO team),
+        #         we'll be overwriting the User.username field (in the auth_user table) with the GUID.
+        #       The Silver Keycloak ID is preserved under Profile.silver_keycloak_id.
+        #       If we've switched over to Gold but haven't done this,
+        #         we'll end up with duplicate users whenever we call get_or_create().
+        #       (one with the GUID as the username, the other with the Keycloak ID)
+        #
         # Get or create a user with the keycloak ID.
         try:
             user, update = User.objects.get_or_create(username=username)
@@ -94,6 +110,14 @@ class JwtOidcAuthentication(JSONWebTokenAuthentication):
                 if source == 'preferred_username':
                     value = value.upper()  # Uppercase to match existing data
                 setattr(profile, target, value)
+        # Manually combining IDP/username allows us to preserve the existing table structure meant for Silver
+        if self.is_gold_shared_realm(payload):
+            identity_provider = payload.get('identity_provider')
+            if identity_provider == 'idir':
+                username = payload.get('idir_username')
+            else:
+                username = payload.get('bceid_username')
+            setattr(profile, 'username', f'{username}@{identity_provider}'.upper())
         if not profile.name and profile.username:
             # When the name of the user isn't available, fallback to the username
             profile.name = profile.username
@@ -103,7 +127,10 @@ class JwtOidcAuthentication(JSONWebTokenAuthentication):
 
         # Get the roles supplied by Keycloak for this user.
         try:
-            roles = payload.get('realm_access').get('roles')
+            if self.is_gold_shared_realm(payload):
+                roles = payload.get('client_roles')
+            else:
+                roles = payload.get('realm_access').get('roles')
         except:
             raise exceptions.AuthenticationFailed('Failed to retrieve roles')
 
@@ -113,7 +140,17 @@ class JwtOidcAuthentication(JSONWebTokenAuthentication):
         return user
 
     @staticmethod
+    def is_gold_shared_realm(payload):
+        return payload.get('iss').endswith(KEYCLOAK_GOLD_REALM_URL)
+
+    @staticmethod
     def known_sso_authority(payload):
         preferred_username = payload.get('preferred_username')
-        return 'idir\\' in preferred_username or 'bceid\\' in preferred_username\
-            or preferred_username == 'testuser'
+
+        if payload.get('iss').endswith(KEYCLOAK_GOLD_REALM_URL):
+            identity_provider = payload.get('identity_provider')
+            return 'idir' in identity_provider or 'bceid' in identity_provider\
+                or preferred_username == 'testuser'
+        else:
+            return 'idir\\' in preferred_username or 'bceid\\' in preferred_username\
+                or preferred_username == 'testuser'
