ba9957d09584d61df9d4003fdad0baa3de1f06a5
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2023-03-16 16:42:23 -0700

    Frontend: check client roles instead, realm roles no longer exist in Gold

--- app/frontend/src/common/store/auth.js
+++ app/frontend/src/common/store/auth.js
@@ -20,33 +20,39 @@ const auth = {
         // e.g. We don't need to understand what a Statutory Authority is here, we should
         // only be concerned if the right to edit/approve is set. It's up to keycloak to associate some
         // group called Statutory Authority with the edit/approve roles.
-        return {
+
+        // NOTE: keycloak.js comes with hasResourceRole(role, resource) for checking if the user has a
+        // particular role, but it doesn't seem to look at the correct JWT so using it will return false
+        // even if the user does have that role.
+        // Instead, we have to look at the "raw" list of roles contained inside the keycloak instance.
+        const clientRoles = state.keycloak.idTokenParsed['client_roles']
+        return {          
           registry: {
-            view: state.keycloak.hasRealmRole('registries_viewer'),
-            edit: state.keycloak.hasRealmRole('registries_edit'),
-            approve: state.keycloak.hasRealmRole('registries_approve')
+            view: clientRoles.includes('registries_viewer'),
+            edit: clientRoles.includes('registries_edit'),
+            approve: clientRoles.includes('registries_approve')
           },
           wells: {
-            view: state.keycloak.hasRealmRole('wells_viewer'),
-            edit: state.keycloak.hasRealmRole('wells_edit'),
-            approve: state.keycloak.hasRealmRole('wells_approve')
+            view: clientRoles.includes('wells_viewer'),
+            edit: clientRoles.includes('wells_edit'),
+            approve: clientRoles.includes('wells_approve')
           },
           submissions: {
-            view: state.keycloak.hasRealmRole('wells_submission_viewer'),
-            edit: state.keycloak.hasRealmRole('wells_submission'),
-            approve: state.keycloak.hasRealmRole('wells_approve')
+            view: clientRoles.includes('wells_submission_viewer'),
+            edit: clientRoles.includes('wells_submission'),
+            approve: clientRoles.includes('wells_approve')
           },
           aquifers: {
-            edit: state.keycloak.hasRealmRole('aquifers_edit')
+            edit: clientRoles.includes('aquifers_edit')
           },
           surveys: {
-            edit: state.keycloak.hasRealmRole('surveys_edit')
+            edit: clientRoles.includes('surveys_edit')
           },
           bulk: {
-            wellAquiferCorrelation: state.keycloak.hasRealmRole('bulk_well_aquifer_correlation_upload'),
-            wellDocuments: state.keycloak.hasRealmRole('bulk_well_documents_upload'),
-            aquiferDocuments: state.keycloak.hasRealmRole('bulk_aquifer_documents_upload'),
-            verticalAquiferExtents: state.keycloak.hasRealmRole('bulk_vertical_aquifer_extents_upload')
+            wellAquiferCorrelation: clientRoles.includes('bulk_well_aquifer_correlation_upload'),
+            wellDocuments: clientRoles.includes('bulk_well_documents_upload'),
+            aquiferDocuments: clientRoles.includes('bulk_aquifer_documents_upload'),
+            verticalAquiferExtents: clientRoles.includes('bulk_vertical_aquifer_extents_upload')
           }
         }
       } else {
