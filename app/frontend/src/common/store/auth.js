d19f553cc535dcda90c7d89be1dd5823a50a44cc
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2023-03-16 17:05:17 -0700

    Update inline comments

--- app/frontend/src/common/store/auth.js
+++ app/frontend/src/common/store/auth.js
@@ -16,7 +16,8 @@ const auth = {
     userRoles (state) {
       if (state.keycloak && state.keycloak.authenticated) {
         // map SSO roles to web app permissions
-        // IMPORTANT: One should be relying on SSO groups to assign the appropriate roles.
+        // IMPORTANT: One should be relying on SSO composite roles (which can be found alongside all of the 
+        // granular roles on Common Hosted SSO) to assign the appropriate roles.
         // e.g. We don't need to understand what a Statutory Authority is here, we should
         // only be concerned if the right to edit/approve is set. It's up to keycloak to associate some
         // group called Statutory Authority with the edit/approve roles.
