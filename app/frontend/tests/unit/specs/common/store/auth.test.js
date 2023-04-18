c842148676fbe038982e440b1a0ea8b93b0f0935
Norris Ng <103449568+norrisng-bc@users.noreply.github.com>
2023-04-17 17:34:01 -0700

    API tests: check Keycloak client roles instead of realm roles

--- app/frontend/tests/unit/specs/common/store/auth.test.js
+++ app/frontend/tests/unit/specs/common/store/auth.test.js
@@ -3,43 +3,43 @@ import auth from '@/common/store/auth.js'
 describe('auth', () => {
   it('If user has role registries_viewer, then registry.view === true', () => {
     const state = { keycloak: { authenticated: true,
-      hasRealmRole (key) {
-        return key === 'registries_viewer'
+      idTokenParsed: {
+        client_roles: ['registries_viewer']
       } } }
     expect(auth.getters.userRoles(state).registry.view).toBe(true)
   })
   it('If keycloak has role as wells_viewer, then wells.view === true', () => {
     const state = { keycloak: { authenticated: true,
-      hasRealmRole (key) {
-        return key === 'wells_viewer'
+      idTokenParsed: {
+        client_roles: ['wells_viewer']
       } } }
     expect(auth.getters.userRoles(state).wells.view).toBe(true)
   })
   it('If keycloak has no roles, then wells.view === false', () => {
     const state = { keycloak: { authenticated: true,
-      hasRealmRole (key) {
-        return false
+      idTokenParsed: {
+        client_roles: []
       } } }
     expect(auth.getters.userRoles(state).wells.view).toBe(false)
   })
   it('If keycloak has role as wells_edit, then wells.edit === true', () => {
     const state = { keycloak: { authenticated: true,
-      hasRealmRole (key) {
-        return key === 'wells_edit'
+      idTokenParsed: {
+        client_roles: ['wells_edit']
       } } }
     expect(auth.getters.userRoles(state).wells.edit).toBe(true)
   })
   it('If keycloak has role as wells_submission_viewer, then submissions.view === true', () => {
     const state = { keycloak: { authenticated: true,
-      hasRealmRole (key) {
-        return key === 'wells_submission_viewer'
+      idTokenParsed: {
+        client_roles: ['wells_submission_viewer']
       } } }
     expect(auth.getters.userRoles(state).submissions.view).toBe(true)
   })
   it('If keycloak has role as wells_submission, then submissions.edit === true', () => {
     const state = { keycloak: { authenticated: true,
-      hasRealmRole (key) {
-        return key === 'wells_submission'
+      idTokenParsed: {
+        client_roles: ['wells_submission']
       } } }
     expect(auth.getters.userRoles(state).submissions.edit).toBe(true)
   })
