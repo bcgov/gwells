bf819e687c85474a15059c921d1808110eb48830
acatchpole <adamantiumite@gmail.com>
2024-01-16 14:55:07 -0800

    added doc comments to new methods in location.vue

--- app/frontend/src/submissions/components/SubmissionForm/Location.vue
+++ app/frontend/src/submissions/components/SubmissionForm/Location.vue
@@ -304,13 +304,18 @@ export default {
     }
   },
   methods: {
+      /**
+     * Asynchronously fetches address suggestions based on the owner's address input.
+     * If no input is provided, it clears the current suggestions.
+     * On success, it maps the received data to full addresses and updates the addressSuggestions state.
+     * On failure, it logs the error and clears the current suggestions.
+     * Finally, sets the loading state to false.
+     */
     async fetchAddressSuggestions() {
-
       if (!this.streetAddressInput) {
         this.addressSuggestions = [];
         return;
       }
-
       this.isLoadingSuggestions = true;
       const params = {
         minScore: 50,
@@ -338,6 +343,13 @@ export default {
         this.isLoadingSuggestions = false;
       }
     },
+
+    /**
+     * Processes the selected address suggestion.
+     * Splits the suggestion into components and updates the owner's province, city, and address inputs accordingly.
+     * Clears the address suggestions afterward.
+     * @param {string} suggestion - The selected address suggestion. ("1234 Street Rd, Name of City, BC")
+     */
     selectAddressSuggestion(suggestion) {
       const wellAddressArray = suggestion.split(',');
 
@@ -356,14 +368,26 @@ export default {
         }
       }
     },
+
+    /**
+     * Clears the current list of address suggestions.
+     */
     clearAddressSuggestions () {
       this.addressSuggestions = [];
     },
+    
+    /**
+     * Shows the address suggestions list in the UI.
+     */
     showList() {
       if(document.getElementById('address-suggestions-list')){
         document.getElementById('address-suggestions-list').style.display = 'block';
       }     
     },
+    
+    /**
+     * Hides the address suggestions list in the UI.
+     */
     hideList() {
       if(document.getElementById('address-suggestions-list')){
         document.getElementById('address-suggestions-list').style.display = 'none';
