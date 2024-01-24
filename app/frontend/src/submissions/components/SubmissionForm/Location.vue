e545ef60b240aac6452b83ce6c8e70fd1bf9d1eb
Sanjay Babu <sanjaytkbabu@gmail.com>
2024-01-24 13:52:16 -0800

    address autofill, api call moved to backend, api url moved to env

--- app/frontend/src/submissions/components/SubmissionForm/Location.vue
+++ app/frontend/src/submissions/components/SubmissionForm/Location.vue
@@ -38,10 +38,23 @@ Licensed under the Apache License, Version 2.0 (the "License");
             id="wellStreetAddress"
             type="text"
             label="Street address"
+            @input="fetchAddressSuggestions"
+            v-on:focus="showList(true)"
+            v-on:blur="showList(false)"
             :errors="errors['street_address']"
             :loaded="fieldsLoaded['street_address']"
             :disabled="sameAsOwnerAddress"
           ></form-input>
+           <!-- Display the address suggestions -->
+        <div v-if="addressSuggestions.length > 0" class="address-suggestions list-group list-group-flush border" id="location-address-suggestions-list">
+          <div v-for="(suggestion, index) in addressSuggestions" :key="index">
+            <button @mousedown="selectAddressSuggestion(suggestion)" class="list-group-item list-group-item-action border-0">{{ suggestion }}</button>
+          </div>
+        </div>
+        <!-- Display a loading indicator while fetching suggestions -->
+        <div v-if="isLoadingSuggestions" class="loading-indicator">
+          Loading...
+        </div>
         </b-col>
       </b-row>
       <b-row>
@@ -202,10 +215,9 @@ Licensed under the Apache License, Version 2.0 (the "License");
 </template>
 <script>
 import { mapGetters } from 'vuex'
-
 import inputBindingsMixin from '@/common/inputBindingsMixin.js'
-
 import BackToTopLink from '@/common/components/BackToTopLink.vue'
+import ApiService from '../../../common/services/ApiService'
 
 export default {
   name: 'Location',
@@ -255,7 +267,9 @@ export default {
   data () {
     return {
       wellAddressHints: [],
-      sameAsOwnerAddress: false
+      sameAsOwnerAddress: false,
+      addressSuggestions: [],
+      isLoadingSuggestions: false
     }
   },
   computed: {
@@ -285,6 +299,83 @@ export default {
       this.streetAddressInput = String(this.ownerMailingAddress)
       this.cityInput = String(this.ownerCity)
     }
+  },
+  methods: {
+      /**
+     * @desc Asynchronously fetches address suggestions based on the owner's address input.
+     * If no input is provided, it clears the current suggestions.
+     * On success, it maps the received data to full addresses and updates the addressSuggestions state.
+     * On failure, it logs the error and clears the current suggestions.
+     * Finally, sets the loading state to false.
+     */
+    async fetchAddressSuggestions() {
+      const MIN_QUERY_LENGTH = 3;
+      if (!this.streetAddressInput || this.streetAddressInput.length < MIN_QUERY_LENGTH) {
+        this.addressSuggestions = [];
+        return;
+      }
+      this.isLoadingSuggestions = true;
+      const params = {
+        minScore: 50,
+        maxResults: 5,
+        echo: 'false',
+        brief: true,
+        autoComplete: true,
+        matchPrecision: 'CIVIC_NUMBER', //forced minimum level of specificity for return values. will only return addresses that contain at least contain a street number
+        addressString: this.streetAddressInput
+      };
+
+      const querystring = require('querystring');
+      const searchParams = querystring.stringify(params);
+      try {
+        ApiService.getAddresses(searchParams).then((response) => {
+        if (response.data) {
+          const data = response.data;
+          if (data && data.features) {
+            this.addressSuggestions = data.features.map(item => item.properties.fullAddress);
+          } else {
+            this.addressSuggestions = [];
+          }
+        }
+      })
+      } catch (error) {
+        console.error(error);
+        this.addressSuggestions = [];
+      } finally {
+        this.isLoadingSuggestions = false;
+      }
+    },
+
+    /**
+     * @desc Processes the selected address suggestion.
+     * Splits the suggestion into components and updates the owner's province, city, and address inputs accordingly.
+     * Clears the address suggestions afterward.
+     * @param {string} suggestion - The selected address suggestion. ("1234 Street Rd, Name of City, BC")
+     */
+    selectAddressSuggestion(suggestion) {
+      const CITY_ARRAY_INDEX = 1;
+      const STREET_ARRAY_INDEX = 0;
+      const wellAddressArray = suggestion.split(',');
+      this.streetAddressInput = wellAddressArray[STREET_ARRAY_INDEX];
+      this.cityInput = wellAddressArray[CITY_ARRAY_INDEX].trim();
+    },
+
+    /**
+     * @desc Clears the current list of address suggestions.
+     */
+    clearAddressSuggestions () {
+      this.addressSuggestions = [];
+    },
+    
+    /**
+     * @desc Shows or hides the address suggestions list in the UI.
+     * @param {boolean} show - a boolean which indicates whether to show or hide the element
+     */
+     showList(show) {
+      if(document.getElementById('location-address-suggestions-list')){
+        document.getElementById('location-address-suggestions-list').style.display =  show? 'block' : 'none';
+      }
+    }        
   }
 }
 </script>
@@ -293,4 +384,9 @@ export default {
 .dropdown-error-border {
   border-radius: 5px;
 }
+.address-suggestions {
+    list-style-type: none;
+    position: absolute;
+    z-index: 10;
+  }
 </style>
