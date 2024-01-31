27d26d9caef3961b460774bf1aa76445f8e23053
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-01-31 11:32:10 -0800

    Add background to Coordinate tip

--- app/frontend/src/submissions/components/SubmissionForm/Coords.vue
+++ app/frontend/src/submissions/components/SubmissionForm/Coords.vue
@@ -26,7 +26,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
         </b-col>
       </b-row>
       <p>To determine coordinates using a Global Positioning System (GPS), set the datum to North America Datum of 1983 (NAD 83), the current ministry standard for mapping.</p>
-      <p>After the GPS coordinates are entered, the map pin can be moved by clicking and dragging it on the map. The GPS coordinates will be updated automatically.</p>
+      <p class="bg-warning p-2">After the GPS coordinates are entered, the map pin can be moved by clicking and dragging it on the map. The GPS coordinates will be updated automatically.</p>
       <b-row>
         <b-col sm="12" md="6">
           <b-card no-body class="p-3 m-1 m-md-1">
