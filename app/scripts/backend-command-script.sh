f87d0dae6cfed90fa51d2769c5ff2a259478472f
David <62899351+davidclaveau@users.noreply.github.com>
2024-03-15 11:57:51 -0700

    point to staging if opted; update command, env, gitignore

---
+++ app/scripts/backend-command-script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ "$ENVIRONMENT" = "local" ]; then
+  echo "Running in local environment" &&
+  sleep 3 &&
+  set -x &&
+  cd /app/backend &&
+  mkdir -p .pip &&
+  python3 -m pip install --upgrade pip &&
+  python3 -m pip install ptvsd &&
+  python3 -m pip install --cache-dir=.pip -r requirements.txt &&
+  python3 manage.py migrate --noinput &&
+  ./load_fixtures.sh all &&
+  python3 manage.py createinitialrevisions &&
+  python3 manage.py collectstatic --noinput &&
+  python3 manage.py export --cleanup=1 --upload=1 &&
+  python3 manage.py runserver 0.0.0.0:8000
+elif [ "$ENVIRONMENT" = "test" ]; then
+  echo "Running in test environment" &&
+  sleep 3 &&
+  set -x &&
+  cd /app/backend &&
+  mkdir -p .pip &&
+  python3 -m pip install --upgrade pip &&
+  python3 -m pip install ptvsd &&
+  python3 -m pip install --cache-dir=.pip -r requirements.txt &&
+  python3 manage.py collectstatic --noinput &&
+  python3 manage.py runserver 0.0.0.0:8000
+fi
