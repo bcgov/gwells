1d5e9098e2ebd3fe875d03c61b2232654efb8217
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-03-13 16:27:39 -0700

    Updated babel config to target specific version of JS

--- app/frontend/babel.config.js
+++ app/frontend/babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    [
+      '@babel/preset-env',
+      {
+        targets: 'es2020', // Specifies targeting ECMAScript 2020
+        useBuiltIns: 'usage', // Only include polyfills for features actually used in your code
+        corejs: 3 // Specify the version of core-js to use (recommended to use core-js 3)
+      }
+    ]
   ]
 }
