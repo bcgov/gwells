4f09dd7c907fc8493eb3b7a8bae3c358ae5929f0
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-03-13 16:30:54 -0700

    Reverted babel.config

--- app/frontend/babel.config.js
+++ app/frontend/babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: 'es2020', // Specifies targeting ECMAScript 2020
-        useBuiltIns: 'usage', // Only include polyfills for features actually used in your code
-        corejs: 3 // Specify the version of core-js to use (recommended to use core-js 3)
-      }
-    ]
+    '@vue/app'
   ]
 }
