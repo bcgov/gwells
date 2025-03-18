3f842137cc3dd2f5671edad8d3aaf22945d01bed
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-03-18 13:57:57 -0700

    Test commit

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -30,17 +30,17 @@ import VueMoment from 'vue-moment'
 import FormInput from '@/common/components/FormInput.vue'
 import { FETCH_CONFIG } from '@/common/store/config.js'
 import filters from '@/common/filters'
-
+// Test
 // GWELLS js API library (helper methods for working with API)
 import ApiService from '@/common/services/ApiService.js'
 
-const PRODUCTION_GWELLS_URL = 'https://apps.nrs.gov.bc.ca/gwells';
-const STAGING_GWELLS_URLS = ['testapps.nrs.gov.bc.ca', 'gwells-staging.apps.silver.devops.gov.bc.ca'];
-const BASE_PATH = '/gwells/';
-const PRODUCTION_MATOMO_HOST = 'https://water-matomo.apps.silver.devops.gov.bc.ca/';
-const TEST_MATOMO_HOST = 'https://water-matomo-staging.apps.silver.devops.gov.bc.ca/';
+const PRODUCTION_GWELLS_URL = 'https://apps.nrs.gov.bc.ca/gwells'
+const STAGING_GWELLS_URLS = ['testapps.nrs.gov.bc.ca', 'gwells-staging.apps.silver.devops.gov.bc.ca']
+const BASE_PATH = '/gwells/'
+const PRODUCTION_MATOMO_HOST = 'https://water-matomo.apps.silver.devops.gov.bc.ca/'
+const TEST_MATOMO_HOST = 'https://water-matomo-staging.apps.silver.devops.gov.bc.ca/'
 
-const isProduction = () => (window.location.href.includes(PRODUCTION_GWELLS_URL))//Does not return true for the production site @https://gwells.apps.silver.devops.gov.bc.ca/gwells
+const isProduction = () => (window.location.href.includes(PRODUCTION_GWELLS_URL)) // Does not return true for the production site @https://gwells.apps.silver.devops.gov.bc.ca/gwells
 const isStaging = () => (
   window.location.pathname.includes(BASE_PATH) && STAGING_GWELLS_URLS.includes(window.location.hostname)
 )
@@ -79,15 +79,14 @@ if (isProduction()) {
     router: router,
     domains: 'apps.nrs.gov.bc.ca'
   })
-}
-else if (isStaging()) {
+} else if (isStaging()) {
   Vue.use(VueMatomo, {
     host: TEST_MATOMO_HOST,
     siteId: 1,
     router: router,
     domains: STAGING_GWELLS_URLS
   })
-} else { //Local & DEV and anything else
+} else { // Local & DEV and anything else
   Vue.use(VueMatomo, {
     host: TEST_MATOMO_HOST,
     siteId: 3,
@@ -113,6 +112,6 @@ new Vue({
   },
   created () {
     this.FETCH_CONFIG()
-    window._paq.push(['trackPageView']); //To track pageview - Matomo
+    window._paq.push(['trackPageView']) // To track pageview - Matomo
   }
 })
