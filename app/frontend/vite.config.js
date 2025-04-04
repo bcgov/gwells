cdf93a1f23dcc2d3398fec94d776b40e06d15c80
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-04-04 10:19:01 -0700

    removed https

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -14,9 +14,9 @@ export default defineConfig({
     port: 8080,
     strictPort: true,
     host: true,
-    allowedHosts: ['https://gwells-frontend-26e83e-dev.apps.silver.devops.gov.bc.ca', 'https://gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca', 'localhost'],
+    allowedHosts: ['gwells-frontend-26e83e-dev.apps.silver.devops.gov.bc.ca', 'gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca', 'localhost'],
     cors: {
-      origin: 'https://gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca',
+      origin: 'gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca',
       credentials: true,
     },
   },
@@ -24,9 +24,9 @@ export default defineConfig({
     port: 8080,
     strictPort: true,
     host: true,
-    allowedHosts: ['https://gwells-frontend-26e83e-dev.apps.silver.devops.gov.bc.ca', 'https://gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca', 'localhost'],
+    allowedHosts: ['gwells-frontend-26e83e-dev.apps.silver.devops.gov.bc.ca', 'gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca', 'localhost'],
     cors: {
-      origin: 'https://gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca',
+      origin: 'gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca',
       credentials: true,
     },
   },
