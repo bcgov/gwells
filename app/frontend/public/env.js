7c476e36843561c59dc823db13e230f11488a111
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-05-06 13:50:36 -0700

    Frontend fix

--- app/frontend/public/env.js
+++ app/frontend/public/env.js
@@ -2,4 +2,5 @@
     window.env = window.env || {};
     window.env.BACKEND_URL = "{{.Env.BACKEND_URL}}";
     window.env.VITE_BACKEND_URL = "{{.Env.VITE_BACKEND_URL}}";
+    window.env.VITE_AXIOS_BASE_URL = "{{.Env.VITE_AXIOS_BASE_URL}}";
 })(window);
