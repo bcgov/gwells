35e53f450fe18d6744d1436c269b4d20c93a4d01
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-05-13 16:14:25 -0700

    Removed match

--- app/frontend/public/env.js
+++ app/frontend/public/env.js
@@ -1,6 +1,6 @@
 (function (window) {
     window.env = window.env || {};
-    window.env.DEBUG = "Template system is: {{.Env.PATH}}"; // PATH should always exist
-    window.env.AXIOS_BASE_URL = "{{.Env.AXIOS_BASE_URL}}";
-    window.env.API_BASE_URL = "{{.Env.API_BASE_URL}}";
+    window.env.DEBUG = "{$PATH}"; // Use Caddy's env var syntax
+    window.env.AXIOS_BASE_URL = "{$AXIOS_BASE_URL}";
+    window.env.API_BASE_URL = "{$API_BASE_URL}";
 })(window);
