b2bd32b0cb7e8beff3559d67d4edf2965d5fc2bf
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-05-13 16:30:16 -0700

    WIP

--- app/frontend/public/env.js
+++ app/frontend/public/env.js
@@ -2,5 +2,5 @@
     window.env = window.env || {};
     window.env.DEBUG = "{$PATH}"; // Use Caddy's env var syntax
     window.env.AXIOS_BASE_URL = "{$AXIOS_BASE_URL}";
-    window.env.API_BASE_URL = "{$API_BASE_URL}";
+    window.env.API_BASE_URL = "{$API_BASE_TEST}";
 })(window);
