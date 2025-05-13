f7a2b75e40748603cb23d5e301c4b9a9bad4d8bf
Dallas Richmond <dallas.richmond@gov.bc.ca>
2025-05-13 16:04:37 -0700

    WIP

--- app/frontend/public/env.js
+++ app/frontend/public/env.js
@@ -1,5 +1,6 @@
 (function (window) {
     window.env = window.env || {};
+    window.env.DEBUG = "Template system is: {{.Env.PATH}}"; // PATH should always exist
     window.env.AXIOS_BASE_URL = "{{.Env.AXIOS_BASE_URL}}";
     window.env.API_BASE_URL = "{{.Env.API_BASE_URL}}";
 })(window);
