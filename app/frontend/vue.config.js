69bb529b6664f85e3ba1279fc7fb4e484f006385
Wilson Wong <wilwong89@gmail.com>
2026-01-28 17:09:35 -0800

    Adjust env variables, update docker-compose with local image

--- app/frontend/vue.config.js
+++ app/frontend/vue.config.js
@@ -1,33 +1,31 @@
 module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/gwells/' : '/',
+  publicPath: import.meta.env.NODE_ENV === "production" ? "/gwells/" : "/",
   configureWebpack: {
     resolve: {
       alias: {
-        moment: 'moment/src/moment',
-        lodash: 'lodash-es'
-      }
+        moment: "moment/src/moment",
+        lodash: "lodash-es",
+      },
     },
     devServer: {
       watchOptions: {
         ignored: /node_modules/,
-        poll: 1000
-      }
-    }
+        poll: 1000,
+      },
+    },
   },
-  transpileDependencies: [
-    '@geolonia/mbgl-gesture-handling'
-  ],
+  transpileDependencies: ["@geolonia/mbgl-gesture-handling"],
   devServer: {
     proxy: {
-      '^/tiles/': {
-        target: process.env.VECTOR_TILE_SERVER || 'http://localhost:7800/',
+      "^/tiles/": {
+        target: import.meta.env.VECTOR_TILE_SERVER || "http://localhost:7800/",
         pathRewrite: {
-          '^/tiles/': '/'
+          "^/tiles/": "/",
         },
-        changeOrigin: true
-      }
-    }
-  }
-}
+        changeOrigin: true,
+      },
+    },
+  },
+};
