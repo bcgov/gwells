37d8a10cd3e98f142d549a5c70d40dfd62cb0f54
raarielgrace <raarielgrace@gmail.com>
2026-02-25 14:57:10 -0800

    Restore __init__.py to original settings

--- app/backend/gwells/settings/__init__.py
+++ app/backend/gwells/settings/__init__.py
@@ -80,7 +80,8 @@ SETTINGS_EXPORT = [
     'FIXTURES_DIRS'
 ]
 
-ALLOWED_HOSTS = ['https://gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca']
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['https://gwells-backend-26e83e-dev.apps.silver.devops.gov.bc.ca']
 # CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOWED_ORIGINS = ['https://gwells-frontend-26e83e-dev.apps.silver.devops.gov.bc.ca']
 CORS_ALLOW_ALL_ORIGINS = True
