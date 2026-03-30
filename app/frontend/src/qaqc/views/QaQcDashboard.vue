ee2d6bf56497de04cc2c2f99b373f9bbd12d8cfb
raarielgrace <raarielgrace@gmail.com>
2026-03-30 10:29:48 -0700

    ci: [GWELLS-2592] QAQC Pinia migration (#2605)

--- app/frontend/src/qaqc/views/QaQcDashboard.vue
+++ app/frontend/src/qaqc/views/QaQcDashboard.vue
@@ -2,7 +2,7 @@
   <div id="qaqcDashboard">
     <h1>QA/QC Dashboard</h1>
     <b-tabs v-model="currentTab" content-class="mt-3">
-      <b-tab title="Record Compliance"></b-tab>
+      <b-tab title="Record Compliance" active></b-tab>
       <b-tab title="Mislocated Wells"></b-tab>
       <b-tab title="Cross Referencing"></b-tab>
     </b-tabs>
@@ -14,6 +14,7 @@
 // Import your table components
 import QaQcTable from '../components/QaQcTable.vue'
 import { FETCH_CODES } from '@/submissions/store/actions.types.js'
+import { useQAQCStore } from '@/stores/qaqc'
 
 export default {
   name: 'QaQcDashboard',
@@ -21,12 +22,13 @@ export default {
     QaQcTable
   },
   computed: {
+    qaqcStore () { return useQAQCStore() },
     currentTab: {
       get () {
-        return this.$store.state.selectedTab
+        return this.qaqcStore.selectedTab
       },
       set (value) {
-        this.$store.dispatch('SET_QAQC_SELECTED_TAB_ACTION', value)
+        this.qaqcStore.setSelectedTabAction(value)
       }
     }
   },
