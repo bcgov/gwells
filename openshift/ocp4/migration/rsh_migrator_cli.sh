99c4c7753fa5b078096e1ba92b6bc73e2ea2bddf
raarielgrace <raarielgrace@gmail.com>
2026-06-29 16:33:37 -0700

    Removed lots of deprecated files

--- openshift/ocp4/migration/rsh_migrator_cli.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-MIGRATOR_POD=$(oc get pods -n "$1" | grep "migrator-cli" | grep Running | head -1 | awk '{print $1}')
-echo "oc rsh -n $1 $MIGRATOR_POD /bin/bash"
-oc rsh -n "$1" "$MIGRATOR_POD" /bin/bash
